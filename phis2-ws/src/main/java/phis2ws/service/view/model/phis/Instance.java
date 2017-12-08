de6320de8888a7837ee0eab2c59798c0d7dad9e5
Eloan LAGIER <lagier@mistea-adonis.supagro.inra.lan>
2017-12-08 10:18:46 +0100

    creation de la class du modele instance

---
+++ phis2-ws/src/main/java/phis2ws/service/view/model/phis/Instance.java
@@ -0,0 +1,18 @@
+//**********************************************************************************************
+//                                       Instance.java 
+//
+// Author(s): Eloan LAGIER
+// PHIS-SILEX version 1.0
+// Copyright © - INRA - 2017
+// Creation date: Decembre, 8 2017
+// Contact: eloan.lagier@inra.fr morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+// Last modification date: Decembre, 8 2017
+// Subject: A class of Instance for the service call
+//***********************************************************************************************
+package phis2ws.service.view.model.phis;
+
+
+public class Instance {
+    String uri;
+    String type;
+}

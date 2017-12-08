a96f1805487c87ac388863262cd5a5acbac97b9b
Eloan LAGIER <lagier@mistea-adonis.supagro.inra.lan>
2017-12-08 10:07:20 +0100

    modification du bandeau / oublie

--- phis2-ws/src/main/java/phis2ws/service/view/model/phis/Concept.java
+++ phis2-ws/src/main/java/phis2ws/service/view/model/phis/Concept.java
@@ -1,13 +1,13 @@
 //**********************************************************************************************
-//                                       XXX.java 
+//                                       Concept.java 
 //
-// Author(s): Morgane VIDAL
+// Author(s): Eloan LAGIER
 // PHIS-SILEX version 1.0
 // Copyright © - INRA - 2017
-// Creation date: XX 2017
-// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
-// Last modification date:  XX, 2017
-// Subject: XX
+// Creation date: Decembre, 8 2017
+// Contact: eloan.lagier@inra.fr morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+// Last modification date: Decembre, 8 2017
+// Subject: A class of concept for the service call
 //***********************************************************************************************
 package phis2ws.service.view.model.phis;
 

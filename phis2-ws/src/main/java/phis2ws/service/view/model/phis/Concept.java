7d31ae610391d1f3894e2c3ae5b333b6a52ee1f3
Eloan LAGIER <lagier@mistea-adonis.supagro.inra.lan>
2017-12-08 10:02:41 +0100

    creation du modèle de concept

---
+++ phis2-ws/src/main/java/phis2ws/service/view/model/phis/Concept.java
@@ -0,0 +1,21 @@
+//**********************************************************************************************
+//                                       XXX.java 
+//
+// Author(s): Morgane VIDAL
+// PHIS-SILEX version 1.0
+// Copyright © - INRA - 2017
+// Creation date: XX 2017
+// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+// Last modification date:  XX, 2017
+// Subject: XX
+//***********************************************************************************************
+package phis2ws.service.view.model.phis;
+
+/**
+ *
+ * @author lagier
+ */
+public class Concept {
+    String ConceptUri;
+
+}

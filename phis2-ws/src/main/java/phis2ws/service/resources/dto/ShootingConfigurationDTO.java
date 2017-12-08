73214ed2595d23bbfd030c1ea0e776bb8a05ecf3
vidalmor <morgane.vidal@wanadoo.fr>
2017-12-08 15:56:07 +0100

    Images service : creation of the shooting configuration DTO

---
+++ phis2-ws/src/main/java/phis2ws/service/resources/dto/ShootingConfigurationDTO.java
@@ -0,0 +1,66 @@
+//**********************************************************************************************
+//                                       ShootingConfigurationDTO.java 
+//
+// Author(s): Morgane VIDAL
+// PHIS-SILEX version 1.0
+// Copyright © - INRA - 2017
+// Creation date: December, 8 2017
+// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+// Last modification date:  December, 8 2017
+// Subject: Represents the JSON submitted for the images shooting configuration 
+//***********************************************************************************************
+package phis2ws.service.resources.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import java.util.HashMap;
+import java.util.Map;
+import phis2ws.service.documentation.DocumentationAnnotation;
+import phis2ws.service.resources.dto.manager.AbstractVerifiedClass;
+
+public class ShootingConfigurationDTO extends AbstractVerifiedClass {
+
+    private String date;
+    private String timestamp;
+    private String position;
+    
+    @Override
+    public Map rules() {
+        Map<String, Boolean> rules = new HashMap<>();
+        rules.put("date", Boolean.TRUE);
+        rules.put("timestamp", Boolean.TRUE);
+        rules.put("position", Boolean.FALSE);
+        return rules;
+    }
+
+    @Override
+    public Object createObjectFromDTO() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @ApiModelProperty(example = DocumentationAnnotation.EXAMPLE_SHOOTING_CONFIGURATION_DATE)
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+    
+    @ApiModelProperty(example = DocumentationAnnotation.EXAMPLE_SHOOTING_CONFIGURATION_TIMESTAMP)
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @ApiModelProperty(example = DocumentationAnnotation.EXAMPLE_SHOOTING_CONFIGURATION_POSITION)
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}

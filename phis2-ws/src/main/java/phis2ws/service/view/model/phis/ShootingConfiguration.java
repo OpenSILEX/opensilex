c9a5f9c58709b8b261c9b0deb6ec5b1cb04cbb5c
vidalmor <morgane.vidal@wanadoo.fr>
2017-12-11 11:28:13 +0100

    model ShootingConfiguration

---
+++ phis2-ws/src/main/java/phis2ws/service/view/model/phis/ShootingConfiguration.java
@@ -0,0 +1,45 @@
+//**********************************************************************************************
+//                                       ShootingConfiguration.java 
+//
+// Author(s): Morgane VIDAL
+// PHIS-SILEX version 1.0
+// Copyright © - INRA - 2017
+// Creation date: December, 11 2017
+// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+// Last modification date:  December, 11 2017
+// Subject: Represents the shooting configuration view
+//***********************************************************************************************
+package phis2ws.service.view.model.phis;
+
+public class ShootingConfiguration {
+    private String date;
+    private String timestamp;
+    private String position;
+
+    public ShootingConfiguration() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}

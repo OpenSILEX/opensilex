9070fed8e4f7ae2bc2c2cb80a019595584604e66
vidalmor <morgane.vidal@wanadoo.fr>
2017-12-12 09:21:32 +0100

    DAO ImageMetadataDaoSesame

---
+++ phis2-ws/src/main/java/phis2ws/service/dao/sesame/ImageMetadataDaoSesame.java
@@ -0,0 +1,42 @@
+//**********************************************************************************************
+//                                       ImageMetadataDaoSesame.java 
+//
+// Author(s): Morgane VIDAL
+// PHIS-SILEX version 1.0
+// Copyright © - INRA - 2017
+// Creation date: December, 11 2017
+// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+// Last modification date:  December, 11 2017
+// Subject: A Dao sepecific to images metadata
+//***********************************************************************************************
+package phis2ws.service.dao.sesame;
+
+import org.eclipse.rdf4j.query.MalformedQueryException;
+import org.eclipse.rdf4j.query.QueryEvaluationException;
+import org.eclipse.rdf4j.repository.RepositoryException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import phis2ws.service.dao.manager.DAOSesame;
+import phis2ws.service.utils.sparql.SPARQLQueryBuilder;
+import phis2ws.service.view.model.phis.ImageMetadata;
+
+public class ImageMetadataDaoSesame extends DAOSesame<ImageMetadata> {
+    
+    final static Logger LOGGER = LoggerFactory.getLogger(ImageMetadataDaoSesame.class);
+    
+    public String rdfType;
+
+    public ImageMetadataDaoSesame() {
+    }
+
+    @Override
+    protected SPARQLQueryBuilder prepareSearchQuery() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Integer count() throws RepositoryException, MalformedQueryException, QueryEvaluationException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

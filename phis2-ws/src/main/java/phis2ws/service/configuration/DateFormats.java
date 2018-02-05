5b9b5d11c2528acd624d1d34552a2482a8f15386
vidalmor <morgane.vidal@wanadoo.fr>
2018-02-05 15:20:51 +0100

    add dates packages which regroups the dates methods

--- phis2-ws/src/main/java/phis2ws/service/configuration/DateFormats.java
+++ phis2-ws/src/main/java/phis2ws/service/configuration/DateFormats.java
@@ -6,7 +6,7 @@
 // Copyright © - INRA - 2016
 // Creation date: august 2016
 // Contact:arnaud.charleroy@supagro.inra.fr, anne.tireau@supagro.inra.fr, pascal.neveu@supagro.inra.fr
-// Last modification date:  October, 2016
+// Last modification date:  February, 05 2018 - add constants 
 // Subject: Dates formats used in the WS
 //***********************************************************************************************
 package phis2ws.service.configuration;
@@ -15,8 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Regroupe les formats de date utilisées dans tout le webService
- * @date 08/16
+ * contains the date formats used in the web service
  * @author A. CHARLEROY
  */
 public final class DateFormats {
@@ -29,9 +28,14 @@ public final class DateFormats {
     public final static List<String> DATETIME_MONGO_FORMAT = Arrays.asList("yyyy-MM-dd HH:mm:ss.SSSZZ", "yyyy-MM-dd HH:mm:ss.SSSSSS", "yyyy-MM-dd HH:mm:ss.SSSSSSZZ", "yyyy-MM-dd HH:mm:ssZZ");
 
     public final static List<String> AUTHORIZED_DATE_FORMATS = Arrays.asList("yyyy-MM-dd HH:mm:ssZ", "yyyy-MM-dd'T'HH:mm:ss.SSSZ", "yyyy-MM-dd'T'HH:mm:ssZ", "yyyy-MM-dd'T'HH:mm:ssZZ", "yyyy-MM-dd");
-    public final static List<String> AUTHORIZED_USER_METEO_DATE_FORMATS = Arrays.asList("yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ssZZ");
-    public final static List<String> AUTHORIZED_USER_SPARQL_DATE_FORMATS = Arrays.asList("yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ssZZ");
+    public final static List<String> AUTHORIZED_USER_ENVIRONMENT_DATE_FORMATS = Arrays.asList("yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ssZZ");
+    public final static List<String> AUTHORIZED_USER_TRIPLESTORE_DATE_FORMATS = Arrays.asList("yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ssZZ");
     public final static List<String> AUTHORIZED_USER_MONGO_DATE_FORMATS = Arrays.asList("yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss.SSS", "yyyy-MM-dd HH:mm:ssZZ", "yyyy-MM-dd HH:mm:ss.SSSZZ");
 
-    public final static String DATETIME_YMD_FORMAT = "yyyy-MM-dd";
+    //Dates formats
+    public final static String YMD_FORMAT = "yyyy-MM-dd";
+    public final static String YMDHMSZ_FORMAT = "yyyy-MM-dd HH:mm:ssZ";
+    
+    //Timezones
+    public final static String TIMEZONE_EU_PARIS = "Europe/paris";
 }

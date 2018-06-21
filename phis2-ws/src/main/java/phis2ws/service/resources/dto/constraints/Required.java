bdaaae39a30475712e6521606fae253ea362a9e5
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2018-06-21 23:27:41 +0200

    Change Required validation annotation

--- phis2-ws/src/main/java/phis2ws/service/resources/dto/constraints/Required.java
+++ phis2-ws/src/main/java/phis2ws/service/resources/dto/constraints/Required.java
@@ -15,6 +15,14 @@ package phis2ws.service.resources.dto.constraints;
  * Multiple annotations which provides required annotation
  * @author Arnaud Charleroy<arnaud.charleroy@inra.fr>
  */
+import java.lang.annotation.Documented;
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
 import javax.validation.Payload;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
@@ -23,6 +31,10 @@ import javax.validation.constraints.NotNull;
 @NotNull
 @NotBlank
 @NotEmpty
+@Target( { METHOD, FIELD, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = {})
+@Documented
 public @interface Required {
 
     String message() default "This field is required";

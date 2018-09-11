977c9fc7acebfc0c3d6441984f036174bbf202bf
vidalmor <morgane.vidal@wanadoo.fr>
2018-09-11 15:54:36 +0200

    vocabulary constants

---
+++ phis2-ws/src/main/java/phis2ws/service/ontologies/Vocabulary.java
@@ -0,0 +1,17 @@
+//******************************************************************************
+//                                       Vocabulary.java
+// SILEX-PHIS
+// Copyright © INRA 2018
+// Creation date: 11 sept. 2018
+// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+//******************************************************************************
+package phis2ws.service.ontologies;
+
+/**
+ * Used to get the vocabulary of SILEX-PHIS. 
+ * Contains the ontology (oepo here) relations and context.
+ * @author Morgane Vidal <morgane.vidal@inra.fr>
+ */
+public enum Vocabulary {
+    
+}

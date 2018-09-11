72243b4df47f2bb92b9e1a2b97f2b409a704db7f
vidalmor <morgane.vidal@wanadoo.fr>
2018-09-11 16:08:08 +0200

    rdf constants

---
+++ phis2-ws/src/main/java/phis2ws/service/ontologies/Rdf.java
@@ -0,0 +1,40 @@
+//******************************************************************************
+//                                       Rdf.java
+// SILEX-PHIS
+// Copyright © INRA 2018
+// Creation date: 11 sept. 2018
+// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+//******************************************************************************
+package phis2ws.service.ontologies;
+
+/**
+ * The elements of the ontology rdf used.
+ * @see https://www.w3.org/1999/02/22-rdf-syntax-ns
+ * @author Morgane Vidal <morgane.vidal@inra.fr>
+ */
+public enum Rdf {
+    NAMESPACE {
+        @Override
+        public String toString() {
+            return "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+        }
+    },
+    RELATION_TYPE {
+        @Override
+        public String toString() {
+            return NAMESPACE.toString() + "type";
+        }
+    },
+    RELATION_REST {
+        @Override
+        public String toString() {
+            return NAMESPACE.toString() + "rest";
+        }
+    },
+    RELATION_FIRST {
+        @Override
+        public String toString() {
+            return NAMESPACE.toString() + "first";
+        }
+    }
+}

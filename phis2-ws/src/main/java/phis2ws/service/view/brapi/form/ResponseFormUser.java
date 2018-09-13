db60e1a2a8583d9254d0fd35d0a467d6521bec44
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2018-09-13 16:49:35 +0200

    Fix pagination issue

--- phis2-ws/src/main/java/phis2ws/service/view/brapi/form/ResponseFormUser.java
+++ phis2-ws/src/main/java/phis2ws/service/view/brapi/form/ResponseFormUser.java
@@ -1,14 +1,11 @@
-//**********************************************************************************************
-//                                       ResponseFormUser.java 
-//
-// Author(s): Morgane Vidal
-// PHIS-SILEX version 1.0
-// Copyright © - INRA - 2017
-// Creation date: April 2017
-// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
-// Last modification date:  April, 2017
-// Subject: Allows the formating of the results of the queries about groups
-//***********************************************************************************************
+//******************************************************************************
+//                            ResponseFormUser.java
+// SILEX-PHIS
+// Copyright © INRA 2017
+// Creation date: Apr, 2017
+// Contact: morgane.vidal@inra.fr,arnaud.charleroy@inra.fr, anne.tireau@inra.fr, 
+//          pascal.neveu@inra.fr
+//******************************************************************************
 package phis2ws.service.view.brapi.form;
 
 import java.util.ArrayList;
@@ -17,21 +14,43 @@ import phis2ws.service.view.brapi.Metadata;
 import phis2ws.service.view.brapi.results.ResultUser;
 import phis2ws.service.view.manager.ResultForm;
 
+/**
+ * Allows the formating of the results of the queries about users.
+ * @author Morgane Vidal <morgane.vidal@inra.fr>, Arnaud Charleroy <arnaud.charleroy@inra.fr>
+ * @update [Arnaud Charleroy] 13 Sept, 2018 : add paginated results constructor
+ */
 public class ResponseFormUser extends ResultForm<User> {
     
     /**
-     * Initialise les champs metadata et result
-     * @param pageSize nombre de résultats par page
-     * @param currentPage page demandée
-     * @param groups liste des resultats
-     * @param paginate 
+     * Initiate metadata and result response fields
+     * @param pageSize results per page
+     * @param currentPage current page
+     * @param users users results list
+     * @param paginate need to be paginate or not
      */
-    public ResponseFormUser(int pageSize, int currentPage, ArrayList<User> groups, boolean paginate) {
-        metadata = new Metadata(pageSize, currentPage, groups.size());
-        if (groups.size() > 1) {
-            result = new ResultUser(groups, metadata.getPagination(), paginate);
+    public ResponseFormUser(int pageSize, int currentPage, ArrayList<User> users, boolean paginate) {
+        metadata = new Metadata(pageSize, currentPage, users.size());
+        if (users.size() > 1) {
+            result = new ResultUser(users, metadata.getPagination(), paginate);
         } else {
-            result = new ResultUser(groups);
+            result = new ResultUser(users);
+        }
+    }
+    
+    /**
+     * Initiate metadata and result response fields
+     * @param pageSize results per page
+     * @param currentPage current page
+     * @param users users results list
+     * @param paginate need to be paginate or not
+     * @param totalCount the number of the items returned by the query
+     */
+    public ResponseFormUser(int pageSize, int currentPage, ArrayList<User> users, boolean paginate, int totalCount) {
+        metadata = new Metadata(pageSize, currentPage, totalCount);
+        if (users.size() > 1) {
+            result = new ResultUser(users, metadata.getPagination(), paginate);
+        } else {
+            result = new ResultUser(users);
         }
     }
 }

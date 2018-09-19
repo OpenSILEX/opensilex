9e915b61a0ed7fc786d21fd986d86b1132ddb245
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2018-09-19 14:38:19 +0200

    Append offset to all paginate features

--- phis2-ws/src/main/java/phis2ws/service/dao/phis/UserDaoPhisBrapi.java
+++ phis2-ws/src/main/java/phis2ws/service/dao/phis/UserDaoPhisBrapi.java
@@ -408,6 +408,7 @@ public class UserDaoPhisBrapi extends DAOPhisBrapi<User, UserDTO> {
                 query.appendANDWhereConditionIfNeeded(sqlFields.get("uri"), String.valueOf(uri), "=", null, tableAlias);
             }
             query.appendLimit(String.valueOf(pageSize));
+            query.appendOffset(Integer.toString(this.getPage() * this.getPageSize()));
 
             queryResult = statement.executeQuery(query.toString());
 

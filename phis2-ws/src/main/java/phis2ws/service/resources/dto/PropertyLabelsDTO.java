14aa9c6134222f6be59adc49b903de92bb1d23db
vincentm <vincentm@mistea-adonis.supagro.inra.lan>
2018-09-24 13:59:54 +0200

    Add comments

--- phis2-ws/src/main/java/phis2ws/service/resources/dto/PropertyLabelsDTO.java
+++ phis2-ws/src/main/java/phis2ws/service/resources/dto/PropertyLabelsDTO.java
@@ -1,13 +1,9 @@
 //******************************************************************************
-//                                       PropertyDTO.java
-//
-// Author(s): Vincent Migot <vincent.migot@inra.fr>
-// PHIS-SILEX version 1.0
-// Copyright © - INRA - 2018
-// Creation date: 10 septembre 2018
+//                                       PropertyLabelsDTO.java
+// SILEX-PHIS
+// Copyright © INRA 2018
+// Creation date: 20 September, 2018
 // Contact: vincent.migot@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
-// Last modification date:  10 septembre 2018
-// Subject: Represents the submitted JSON for a property
 //******************************************************************************
 package phis2ws.service.resources.dto;
 
@@ -17,10 +13,17 @@ import java.util.LinkedList;
 import phis2ws.service.documentation.DocumentationAnnotation;
 import phis2ws.service.view.model.phis.Property;
 
+/**
+ * Represent the submitted JSON of a property with associated labels
+ * @author Vincent Migot <vincent.migot@inra.fr>
+ */
 public class PropertyLabelsDTO extends PropertyDTO {
 
+    // Labels list for the value "rdfType" of the property
     private LinkedList<String> rdfTypeLabels = new LinkedList<>();
+    // Labels list for the value "relation" of the property
     private LinkedList<String> relationLabels = new LinkedList<>();
+    // Labels list for the value "value" of the property
     private LinkedList<String> valueLabels = new LinkedList<>();
 
     @Override

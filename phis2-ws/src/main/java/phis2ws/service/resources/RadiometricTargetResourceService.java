bc239b7ab070a1662a507673dcb94b2f3b95da25
vidalmor <morgane.vidal@wanadoo.fr>
2018-09-26 09:28:14 +0200

    radiometric target resource service

---
+++ phis2-ws/src/main/java/phis2ws/service/resources/RadiometricTargetResourceService.java
@@ -0,0 +1,16 @@
+//******************************************************************************
+//                                       RadiometricTargetResourceService.java
+// SILEX-PHIS
+// Copyright © INRA 2018
+// Creation date: 26 sept. 2018
+// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+//******************************************************************************
+package phis2ws.service.resources;
+
+/**
+ * Radiometric target service.
+ * @author Morgane Vidal <morgane.vidal@inra.fr>
+ */
+public class RadiometricTargetResourceService {
+    
+}

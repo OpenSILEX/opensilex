ed33f7e87185ac5640b166f5fbc9258f42bf3c8b
Andréas <andreas.garcia@lilo.org>
2018-11-13 09:26:46 +0100

    Correct english mistake : "founded"  replaced by "found"

--- phis2-ws/src/main/java/phis2ws/service/dao/sesame/RadiometricTargetDAOSesame.java
+++ phis2-ws/src/main/java/phis2ws/service/dao/sesame/RadiometricTargetDAOSesame.java
@@ -89,7 +89,8 @@ public class RadiometricTargetDAOSesame extends DAOSesame<RadiometricTarget> {
      * Get a radiometric target from a given binding set.
      * Assume that the following attributes exist : uri, label.
      * @param bindingSet a binding set, result from a search query
-     * @return a radiometric target with data extracted from the given binding set
+     * @return a radiometric target with data extracted from the given binding 
+     * set
      */
     private RadiometricTarget getFromBindingSet(BindingSet bindingSet) {
         RadiometricTarget radiometricTarget = new RadiometricTarget();
@@ -107,7 +108,7 @@ public class RadiometricTargetDAOSesame extends DAOSesame<RadiometricTarget> {
     
     /**
      * Get the radiometric targets (uri, label) of the triplestore.
-     * @return the list of the radiometric target founded
+     * @return the list of the radiometric target found
      */
     public ArrayList<RadiometricTarget> allPaginate() {
         SPARQLQueryBuilder query = prepareSearchQuery();
@@ -172,7 +173,7 @@ public class RadiometricTargetDAOSesame extends DAOSesame<RadiometricTarget> {
      * Check the given list of radiometric targets (check properties domain, etc.)
      * @param radiometricTargets
      * @see PropertyDAOSesame
-     * @return the result with the list of the founded errors (empty if no error)
+     * @return the result with the list of the found errors (empty if no error)
      */
     public POSTResultsReturn check(List<RadiometricTarget> radiometricTargets) {
         POSTResultsReturn checkResult = null;
@@ -331,14 +332,14 @@ public class RadiometricTargetDAOSesame extends DAOSesame<RadiometricTarget> {
     /**
      * Check and insert the given radiometric targets in the triplestore
      * @param radiometricTargets
-     * @return the insertion result. Message error if errors founded in data
+     * @return the insertion result. Message error if errors found in data
      *         the list of the generated uri of the radiometric targets if the insertion has been done
      */
     public POSTResultsReturn checkAndInsert(List<RadiometricTarget> radiometricTargets) {
         POSTResultsReturn checkResult = check(radiometricTargets);
         if (checkResult.getDataState()) {
             return insert(radiometricTargets);
-        } else { //errors founded in data
+        } else { //errors found in data
             return checkResult;
         }
     }
@@ -395,8 +396,8 @@ public class RadiometricTargetDAOSesame extends DAOSesame<RadiometricTarget> {
      * @see RadiometricTargetDAOSesame#update(java.util.List)
      * @param newRadiometricTargetData the new radiometric target data
      * @param oldRadiometricTargetData the old radiometric target data
-     * @return the list of the status with the errors if some has been founded.
-     *         null if no error founded
+     * @return the list of the status with the errors if some has been found.
+     *         null if no error found
      */
     private List<Status> compareNewAndOldRadiometricTarget(RadiometricTarget newRadiometricTargetData, RadiometricTarget oldRadiometricTargetData) {
         //Check each new radiometric target property to see if it has the same number or more occurences than the oldRadiometric.
@@ -468,7 +469,7 @@ public class RadiometricTargetDAOSesame extends DAOSesame<RadiometricTarget> {
             //If a property has a null value, the relation will be deleted. 
             //\SILEX:info
             List<Status> compareNewToOldRadiometricTarget = compareNewAndOldRadiometricTarget(radiometricTarget, oldRadiometricTarget);
-            if (compareNewToOldRadiometricTarget.isEmpty()) { //No error has been founded
+            if (compareNewToOldRadiometricTarget.isEmpty()) { //No error has been found
                 //1. genereate query to delete already existing data
                 //SILEX:info
                 //We only delete the already existing data received by the client. 
@@ -492,7 +493,7 @@ public class RadiometricTargetDAOSesame extends DAOSesame<RadiometricTarget> {
                     annotationUpdate = false;
                     updateStatus.add(new Status(StatusCodeMsg.QUERY_ERROR, StatusCodeMsg.ERR, "Malformed update query: " + e.getMessage()));
                 }
-            } else { //errors has been founded
+            } else { //errors has been found
                 updateStatus.addAll(compareNewToOldRadiometricTarget);
                 annotationUpdate = false;
             }
@@ -530,14 +531,14 @@ public class RadiometricTargetDAOSesame extends DAOSesame<RadiometricTarget> {
     /**
      * Update the given radiometric targets in the triplestore
      * @param radiometricTargets
-     * @return the update result. Message error if errors founded in data
+     * @return the update result. Message error if errors found in data
      *         the list of the generated uri of the radiometric targets if the update has been done
      */
     public POSTResultsReturn checkAndUpdate(List<RadiometricTarget> radiometricTargets) {
         POSTResultsReturn checkResult = check(radiometricTargets);
         if (checkResult.getDataState()) {
             return update(radiometricTargets);
-        } else { //errors founded in data
+        } else { //errors found in data
             return checkResult;
         }
     }

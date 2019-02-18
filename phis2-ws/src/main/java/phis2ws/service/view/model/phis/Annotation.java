f8aca4bf71a597a9324aaf4dd65e097a1bfc2a75
Andréas <andreas.garcia@lilo.org>
2019-02-18 09:38:05 +0100

    Improve coding style

--- phis2-ws/src/main/java/phis2ws/service/view/model/phis/Annotation.java
+++ phis2-ws/src/main/java/phis2ws/service/view/model/phis/Annotation.java
@@ -1,13 +1,9 @@
 //******************************************************************************
 //                                       Annotation.java
-//
-// Author(s): Arnaud Charleroy<arnaud.charleroy@inra.fr>
-// PHIS-SILEX version 1.0
-// Copyright © - INRA - 2018
+// SILEX PHIS
+// Copyright © INRA 2018
 // Creation date: 14 juin 2018
-// Contact: arnaud.charleroy@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
-// Last modification date:  14 juin 2018
-// Subject: Represents an annotation
+// Contact: arnaud.charleroy@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr// Subject: Represents an annotation
 //******************************************************************************
 package phis2ws.service.view.model.phis;
 
@@ -16,27 +12,45 @@ import org.joda.time.DateTime;
 
 /**
  * Represents an annotation
- *
- * @author Arnaud Charleroy<arnaud.charleroy@inra.fr>
+ * @update [Andréas Garcia] 15 Feb., 2019: Improve coding style
+ * @author Arnaud Charleroy <arnaud.charleroy@inra.fr>
  */
 public class Annotation {
 
-    // uri of this annotation eg.  http://www.phenome-fppn.fr/platform/id/annotation/8247af37-769c-495b-8e7e-78b1141176c2
+    /**
+     * Uri of this annotation
+     * @example http://www.phenome-fppn.fr/platform/id/annotation/8247af37-769c-495b-8e7e-78b1141176c2
+     */
     private String uri;
 
-    // Creation date of this annotation format yyyy-MM-ddTHH:mm:ssZ eg. 2018-06-25T15:13:59+0200
+    /** 
+     * Creation date of this annotation format yyyy-MM-ddTHH:mm:ssZ
+     * @example 2018-06-25T15:13:59+0200
+     */
     private DateTime created;
 
-    // creator of this annotations eg. http://www.phenome-fppn.fr/diaphen/id/agent/arnaud_charleroy
-    private String creator;
+    /** 
+     * Creator of this annotations
+     * @example http://www.phenome-fppn.fr/diaphen/id/agent/arnaud_charleroy
+     */
+     private String creator;
 
-    // Comments that describe this annotation eg. Ustilago maydis infection
-    private ArrayList<String> bodiesValue;
+    /** 
+     * Comments that describe this annotation
+     * @example Ustilago maydis infection
+     */
+    private ArrayList<String> bodyValues;
 
-    // motivation instance uri that describe the purpose of this annotation  eg. http://www.w3.org/ns/oa#commenting
+    /**
+     * Motivation instance uri that describe the purpose of this annotation
+     * @example http://www.w3.org/ns/oa#commenting
+     */
     private String motivatedBy;
 
-    // uris that are annoted by this annotation  eg. http://www.phenome-fppn.fr/diaphen/2017/o1032481
+    /**
+     * uris that are annoted by this annotation
+     * @example http://www.phenome-fppn.fr/diaphen/2017/o1032481
+     */
     private ArrayList<String> targets;
 
     public String getUri() {
@@ -64,19 +78,19 @@ public class Annotation {
     }
 
     public ArrayList<String> getBodiesValue() {
-        return bodiesValue;
+        return bodyValues;
     }
 
     public void setBodiesValue(ArrayList<String> bodiesValue) {
-        this.bodiesValue = bodiesValue;
+        this.bodyValues = bodiesValue;
     }
 
     public void addBody(String body) {
         // If null arraylist is initialized
-        if (this.bodiesValue == null) {
-            this.bodiesValue = new ArrayList<>();
+        if (this.bodyValues == null) {
+            this.bodyValues = new ArrayList<>();
         }
-        this.bodiesValue.add(body);
+        this.bodyValues.add(body);
     }
 
     public ArrayList<String> getTargets() {

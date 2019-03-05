f3e7f299eb1c0a4db393697303403b1bfe7d805d
Vincent Migot <vincent.migot@inra.fr>
2019-03-05 15:33:03 +0100

    Add data post service

--- phis2-ws/src/main/java/phis2ws/service/view/model/phis/Data.java
+++ phis2-ws/src/main/java/phis2ws/service/view/model/phis/Data.java
@@ -1,79 +1,71 @@
-//**********************************************************************************************
-//                                       Data.java 
-//
-// Author(s): Morgane Vidal
-// PHIS-SILEX version 1.0
-// Copyright © - INRA - 2017
-// Creation date: September 2017
-// Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
-// Last modification date:  September, 14 2017
-// Subject: Represents the data view 
-//***********************************************************************************************
+//******************************************************************************
+//                          Data.java
+// SILEX-PHIS
+// Copyright © INRA 2019
+// Creation date: 1 March 2019
+// Contact: vincent.migotl@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
+//******************************************************************************
 package phis2ws.service.view.model.phis;
 
+import java.util.Date;
+
+/**
+ * This is the model for the phenotypes data
+ */
 public class Data {
-    private String agronomicalObject;
-    private String date;
-    private String value;
-    private String variable; 
-    private String sensor;
-    private String incertitude;
-    
-    public Data() {
-        
-    }
-    
-    public Data(Data data) {
-        agronomicalObject = data.getAgronomicalObject();
-        date = data.getDate();
-        value = data.getValue();
+    //The uri of the provenance from which data come.
+    //e.g. http://www.phenome-fppn.fr/mtp/2018/s18003
+    protected String provenanceUri;
+    //The uri of the scientific object on which data is related.
+    //e.g. http://www.phenome-fppn.fr/mtp/2018/s18003
+    protected String objectUri;
+    //The uri of the measured variable
+    //e.g. http://www.phenome-fppn.fr/mtp/id/variables/v002
+    protected String variableUri;
+    //The date corresponding to the given value. The format should be yyyy-MM-ddTHH:mm:ssZ
+    //e.g. 2018-06-25T15:13:59+0200
+    protected Date date;
+    //The measured value.
+    //e.g. 1.2
+    protected Object value;
+
+    public String getObjectUri() {
+        return objectUri;
     }
 
-    public String getAgronomicalObject() {
-        return agronomicalObject;
+    public void setObjectUri(String objectUri) {
+        this.objectUri = objectUri;
     }
 
-    public void setAgronomicalObject(String agronomicalObject) {
-        this.agronomicalObject = agronomicalObject;
+    public String getVariableUri() {
+        return variableUri;
     }
 
-    public String getDate() {
+    public void setVariableUri(String variableUri) {
+        this.variableUri = variableUri;
+    }
+
+    public Date getDate() {
         return date;
     }
 
-    public void setDate(String date) {
+    public void setDate(Date date) {
         this.date = date;
     }
-
-    public String getValue() {
+    
+    public Object getValue() {
         return value;
     }
 
-    public void setValue(String value) {
+    public void setValue(Object value) {
         this.value = value;
     }
 
-    public String getVariable() {
-        return variable;
+    public String getProvenanceUri() {
+        return provenanceUri;
     }
 
-    public void setVariable(String variable) {
-        this.variable = variable;
-    }
-
-    public String getSensor() {
-        return sensor;
-    }
-
-    public void setSensor(String sensor) {
-        this.sensor = sensor;
-    }
-
-    public String getIncertitude() {
-        return incertitude;
-    }
-
-    public void setIncertitude(String incertitude) {
-        this.incertitude = incertitude;
+    public void setProvenanceUri(String provenanceUri) {
+        this.provenanceUri = provenanceUri;
     }
 }

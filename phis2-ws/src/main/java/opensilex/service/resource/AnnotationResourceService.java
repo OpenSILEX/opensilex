a9af1d24b29d12ea1c893193437ce53e63c006b7
Andréas <andreas.garcia@lilo.org>
2019-04-09 11:14:46 +0200

    Remove unused import

--- phis2-ws/src/main/java/opensilex/service/resource/AnnotationResourceService.java
+++ phis2-ws/src/main/java/opensilex/service/resource/AnnotationResourceService.java
@@ -16,8 +16,6 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;

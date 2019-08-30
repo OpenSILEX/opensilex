08ca6b46108a49bb5ee0bf0dd60dc4040a299a71
Vincent Migot <vincent.migot@inra.fr>
2019-08-30 15:09:12 +0200

    Update dev server launcher to automatically reload Angular plugin on change

--- opensilex-dev-tools/src/main/java/org/opensilex/dev/StartServer.java
+++ opensilex-dev-tools/src/main/java/org/opensilex/dev/StartServer.java
@@ -3,9 +3,16 @@ package org.opensilex.dev;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.opensilex.OpenSilex;
 import org.opensilex.cli.MainCommand;
+import org.opensilex.front.app.FrontAppExtension;
+import org.opensilex.front.app.FrontAppModule;
+import org.opensilex.utils.ClassInfo;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -20,8 +27,14 @@ public class StartServer {
 
     public static void main(String[] args) throws IOException {
 
+        // Start server in development mode
+        MainCommand.main(new String[]{"server", "start",
+            "--" + OpenSilex.PROFILE_ID_ARG_KEY + "=" + OpenSilex.DEV_PROFILE_ID});
+
+        // Define current directory to launch node.js processes
         Path currentDirectory = Paths.get(System.getProperty("user.dir"));
 
+        // Start process to automatically rebuild Angular shared library
         ProcessBuilder sharedBuilder = new ProcessBuilder(
                 currentDirectory.resolve("../.ng/node/node").toFile().getCanonicalPath(),
                 currentDirectory.resolve("../opensilex-front-app/angular/dev-tools/build-shared.js").toFile()
@@ -30,15 +43,26 @@ public class StartServer {
         sharedBuilder.inheritIO();
         Process sharedBuilderProcess = sharedBuilder.start();
 
-        ProcessBuilder pluginBuilder = new ProcessBuilder(
-                currentDirectory.resolve("../.ng/node/node").toFile().getCanonicalPath(),
-                currentDirectory.resolve("../opensilex-front-app/angular/dev-tools/build-plugins.js").toFile()
-                        .getCanonicalPath(),
-                "--module=opensilex-front-ue");
+        // Start process to automatically rebuild Angular plugin modules
+        List<String> appPluginBuilderArgs = new ArrayList<>();
+        appPluginBuilderArgs.add(currentDirectory.resolve("../.ng/node/node").toFile().getCanonicalPath());
+        appPluginBuilderArgs.add(currentDirectory.resolve("../opensilex-front-app/angular/dev-tools/build-plugins.js").toFile().getCanonicalPath());
+
+        Set<String> modulesPluginBuilderArgs = new HashSet<>();
+        OpenSilex.getInstance().getModulesImplementingInterface(FrontAppExtension.class).forEach((FrontAppExtension extension) -> {
+            if (!(extension instanceof FrontAppModule)) {
+                modulesPluginBuilderArgs.add("--module=" + ClassInfo.getProjectIdFromClass(extension.getClass()));
+            }
+        });
+        appPluginBuilderArgs.addAll(modulesPluginBuilderArgs);
+
+        ProcessBuilder pluginBuilder = new ProcessBuilder(appPluginBuilderArgs);
+
         pluginBuilder.directory(currentDirectory.resolve("../opensilex-front-app/angular/dev-tools").toFile());
         pluginBuilder.inheritIO();
         Process pluginBuilderProcess = pluginBuilder.start();
 
+        // Start process to run Angular development server on port 4200
         ProcessBuilder devServer = new ProcessBuilder(
                 currentDirectory.resolve("../.ng/node/node").toFile().getCanonicalPath(),
                 currentDirectory.resolve("../node_modules/.bin/ng").toFile().getCanonicalPath(), "serve",
@@ -47,6 +71,7 @@ public class StartServer {
         devServer.inheritIO();
         Process devServerProcess = devServer.start();
 
+        // Add hook to clean node.js processes on shutdown
         Runtime.getRuntime().addShutdownHook(new Thread() {
             public void run() {
                 sharedBuilderProcess.destroy();
@@ -54,11 +79,5 @@ public class StartServer {
                 devServerProcess.destroy();
             }
         });
-        
-        String classpathStr = System.getProperty("java.class.path");
-        System.out.print(classpathStr);
-        MainCommand.main(new String[]{"server", "start",
-            "--" + OpenSilex.PROFILE_ID_ARG_KEY + "=" + OpenSilex.DEV_PROFILE_ID});
-
     }
 }
--- opensilex-front-app/angular/dev-tools/build-plugins.js
+++ opensilex-front-app/angular/dev-tools/build-plugins.js
@@ -5,128 +5,134 @@ const chokidar = require('chokidar');
 const path = require("path");
 const {fork} = require('child_process');
 
+let sharedLibPath = path.resolve(__dirname, "../../target/classes/angular/plugins/shared.js");
+
+function copyPlugin(origin) {
+    let dest = path.resolve(__dirname, "../src/assets/.plugins", path.basename(origin));
+    fs.copyFile(origin, dest, (err) => {
+        if (err) {
+            throw err;
+        }
+//            console.log('Copy ' + origin + ' to ' + dest);
+    });
+}
+
+let buildPipe = [];
+let buildParamsByPlugin = {};
+let pluginBuildCountInPipe = {};
+
+function addPluginToBuildPipe(pluginId) {
+//        console.log('Add plugin to build pipe', pluginId);
+    if (!pluginBuildCountInPipe.hasOwnProperty(pluginId)) {
+        pluginBuildCountInPipe[pluginId] = 0;
+    }
+    pluginBuildCountInPipe[pluginId]++;
+
+    if (buildPipe.length == 0) {
+        buildPipe.push(new Promise(function (_resolve, _reject) {
+            createPluginBuildPromise(pluginId)
+                    .then(function () {
+//                            console.log('Plugin built !', pluginId);
+                        pluginBuildCountInPipe[pluginId]--;
+                        buildPipe.shift();
+                        _resolve();
+                    })
+                    .catch(_reject);
+        }));
+    } else {
+        let lastBuildPromise = buildPipe[buildPipe.length - 1];
+        buildPipe.push(new Promise(function (_resolve, _reject) {
+            lastBuildPromise
+                    .then(function () {
+                        createPluginBuildPromise(pluginId)
+                                .then(function () {
+//                                        console.log('Plugin built !', pluginId);
+                                    pluginBuildCountInPipe[pluginId]--;
+                                    buildPipe.shift();
+                                    _resolve();
+                                })
+                                .catch(_reject);
+                    })
+                    .catch(_reject)
+        }));
+    }
+}
+
+function createPluginBuildPromise(pluginId) {
+    return new Promise(function (_resolve, _reject) {
+        let currentModulePath = buildParamsByPlugin[pluginId].modulePath
+//            console.log('Create plugin promise', pluginId, pluginBuildCountInPipe[pluginId]);
+        if (pluginBuildCountInPipe[pluginId] == 1) {
+            fork(path.resolve(currentModulePath, '../node_modules/.bin/ng'), buildParamsByPlugin[pluginId].args, {
+                cwd: path.resolve(currentModulePath, 'angular/')
+            }).on('exit', () => {
+                // TODO: if compile with error no copy
+//                    console.log('Plugin file created !', pluginId);
+                if (pluginBuildCountInPipe[pluginId] == 1) {
+                    copyPlugin(buildParamsByPlugin[pluginId].outputFile);
+                }
+
+                _resolve();
+            });
+        } else {
+            _resolve();
+        }
+    });
+}
+
+function startWatchingSources(pluginId) {
+    let srcPath = buildParamsByPlugin[pluginId].srcPath;
+
+    console.log("Initialize source watcher for", pluginId);
+    let pluginWatcher = chokidar.watch([
+        srcPath + "/**",
+        sharedLibPath
+    ], {
+        ignored: /(^|[\/\\])\../,
+        persistent: true,
+        ignoreInitial: true,
+        awaitWriteFinish: true
+    });
+
+    pluginWatcher.on('all', function () {
+        addPluginToBuildPipe(pluginId);
+    });
+}
+
 var argv = require('yargs-parser')(process.argv.slice(2));
 
 if (argv.hasOwnProperty("module")) {
-    let modulePath = path.resolve(__dirname, "../../../", argv.module);
-    let jsonData = require(path.resolve(modulePath, 'angular/opensilex.json'));
+    var modules = argv.module;
+    if (!Array.isArray(argv.module)) {
+        modules = [argv.module];
+    }
 
-    let outputPath = path.resolve(modulePath, "./target/classes/angular/plugins");
+    for (let m in modules) {
+        let module = modules[m];
 
-    let sharedLibPath = path.resolve(__dirname, "../../target/classes/angular/plugins/shared.js");
+        let modulePath = path.resolve(__dirname, "../../../", module);
+        let jsonData = require(path.resolve(modulePath, 'angular/opensilex.json'));
 
-    function copyPlugin(origin) {
-        let dest = path.resolve(__dirname, "../src/assets/.plugins", path.basename(origin));
-        fs.copyFile(origin, dest, (err) => {
-            if (err) {
-                throw err;
+        let outputPath = path.resolve(modulePath, "./target/classes/angular/plugins");
+
+        for (let i in jsonData.plugins) {
+            let plugin = jsonData.plugins[i];
+
+            buildParamsByPlugin[plugin.id] = {
+                modulePath: modulePath,
+                args: [
+                    "build",
+                    "--prod",
+                    "--modulePath=" + plugin.modulePath + "#" + plugin.moduleClass,
+                    "--pluginName=" + plugin.id,
+                    "--sharedLibs=shared",
+                    "--outputPath=" + outputPath
+                ],
+                srcPath: path.resolve(modulePath, 'angular/src', plugin.id),
+                outputFile: path.resolve(outputPath, plugin.id + ".js")
             }
-//            console.log('Copy ' + origin + ' to ' + dest);
-        });
-    }
 
-    let buildPipe = [];
-
-    let buildParamsByPlugin = {};
-
-    let pluginBuildCountInPipe = {};
-
-    function addPluginToBuildPipe(pluginId) {
-//        console.log('Add plugin to build pipe', pluginId);
-        if (!pluginBuildCountInPipe.hasOwnProperty(pluginId)) {
-            pluginBuildCountInPipe[pluginId] = 0;    
-        }
-        pluginBuildCountInPipe[pluginId]++;
-
-        if (buildPipe.length == 0) {
-            buildPipe.push(new Promise(function (_resolve, _reject) {
-                createPluginBuildPromise(pluginId)
-                        .then(function () {
-//                            console.log('Plugin built !', pluginId);
-                            pluginBuildCountInPipe[pluginId]--;
-                            buildPipe.shift();
-                            _resolve();
-                        })
-                        .catch(_reject);
-            }));
-        } else {
-            let lastBuildPromise = buildPipe[buildPipe.length - 1];
-            buildPipe.push(new Promise(function (_resolve, _reject) {
-                lastBuildPromise
-                        .then(function () {
-                            createPluginBuildPromise(pluginId)
-                                    .then(function () {
-//                                        console.log('Plugin built !', pluginId);
-                                        pluginBuildCountInPipe[pluginId]--;
-                                        buildPipe.shift();
-                                        _resolve();
-                                    })
-                                    .catch(_reject);
-                        })
-                        .catch(_reject)
-            }));
+            startWatchingSources(plugin.id);
         }
     }
-
-    function createPluginBuildPromise(pluginId) {
-        return new Promise(function (_resolve, _reject) {
-//            console.log('Create plugin promise', pluginId, pluginBuildCountInPipe[pluginId]);
-            if (pluginBuildCountInPipe[pluginId] == 1) {
-                fork(path.resolve(modulePath, '../node_modules/.bin/ng'), buildParamsByPlugin[pluginId].args, {
-                    cwd: path.resolve(modulePath, 'angular/')
-                }).on('exit', () => {
-                    // TODO: if compile with error no copy
-//                    console.log('Plugin file created !', pluginId);
-                    if (pluginBuildCountInPipe[pluginId] == 1) {
-                        copyPlugin(buildParamsByPlugin[pluginId].outputFile);
-                    }
-
-                    _resolve();
-                });
-            } else {
-                _resolve();
-            }
-        });
-    }
-
-    function startWatchingSources(pluginId) {
-        let srcPath = buildParamsByPlugin[pluginId].srcPath;
-        
-        console.log("Initialize source watcher for", pluginId);
-        let pluginWatcher = chokidar.watch([
-                srcPath + "/**",
-                sharedLibPath
-        ], {
-            ignored: /(^|[\/\\])\../,
-            persistent: true,
-            ignoreInitial: true,
-            awaitWriteFinish: true
-        });
-        
-        pluginWatcher.on('all', function() {
-            addPluginToBuildPipe(pluginId);
-        });
-    }
-
-    for (let i in jsonData.plugins) {
-        let plugin = jsonData.plugins[i];
-        
-        buildParamsByPlugin[plugin.id] = {
-            args: [
-                "build",
-                "--prod",
-                "--modulePath=" + plugin.modulePath + "#" + plugin.moduleClass,
-                "--pluginName=" + plugin.id,
-                "--sharedLibs=shared",
-                "--outputPath=" + outputPath
-            ],
-            srcPath: path.resolve(modulePath, 'angular/src', plugin.id),
-            outputFile: path.resolve(outputPath, plugin.id + ".js")
-        }
-        
-        startWatchingSources(plugin.id);
-    }
-
-} else {
-    // return error status
 }

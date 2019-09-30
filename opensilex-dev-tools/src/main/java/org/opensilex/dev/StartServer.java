7ef0488b0abad00d8186c4a8ad762240b806257b
Vincent Migot <vincent.migot@inra.fr>
2019-09-30 12:19:03 +0200

    Remove dependency to front

--- opensilex-dev-tools/src/main/java/org/opensilex/dev/StartServer.java
+++ opensilex-dev-tools/src/main/java/org/opensilex/dev/StartServer.java
@@ -1,18 +1,10 @@
 package org.opensilex.dev;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import org.opensilex.OpenSilex;
 import org.opensilex.cli.MainCommand;
-import org.opensilex.front.app.FrontAppExtension;
-import org.opensilex.utils.ClassInfo;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -26,8 +18,6 @@ import org.opensilex.utils.ClassInfo;
 public class StartServer {
 
     public static void main(String[] args) throws IOException {
-        boolean withFront = true;
-        withFront = false;        
         
         String configFile = StartServer.class.getClassLoader().getResource("./config/opensilex.yml").getPath();
         OpenSilex.setup(new HashMap<String, String>() {
@@ -36,58 +26,6 @@ public class StartServer {
                 put(OpenSilex.CONFIG_FILE_ARG_KEY, configFile);
             }
         });
-
-        if (withFront) {
-            // Define current directory to launch node.js processes
-            Path currentDirectory = Paths.get(System.getProperty("user.dir"));
-
-            // Start process to automatically rebuild Angular shared library
-            ProcessBuilder sharedBuilder = new ProcessBuilder(
-                    currentDirectory.resolve("../.ng/node/node").toFile().getCanonicalPath(),
-                    currentDirectory.resolve("../opensilex-front-app/angular/dev-tools/build-shared.js").toFile()
-                            .getCanonicalPath());
-            sharedBuilder.directory(currentDirectory.resolve("../opensilex-front-app/angular/dev-tools").toFile());
-            sharedBuilder.inheritIO();
-            Process sharedBuilderProcess = sharedBuilder.start();
-
-            // Start process to automatically rebuild Angular plugin modules
-            List<String> appPluginBuilderArgs = new ArrayList<>();
-            appPluginBuilderArgs.add(currentDirectory.resolve("../.ng/node/node").toFile().getCanonicalPath());
-            appPluginBuilderArgs.add(currentDirectory.resolve("../opensilex-front-app/angular/dev-tools/build-plugins.js").toFile().getCanonicalPath());
-
-            Set<String> modulesPluginBuilderArgs = new HashSet<>();
-            OpenSilex.getInstance().getModulesImplementingInterface(FrontAppExtension.class).forEach((FrontAppExtension extension) -> {
-                String moduleProjectId = ClassInfo.getProjectIdFromClass(extension.getClass());
-                if (!moduleProjectId.isEmpty()) {
-                    modulesPluginBuilderArgs.add("--module=" + moduleProjectId);
-                }
-            });
-            appPluginBuilderArgs.addAll(modulesPluginBuilderArgs);
-
-            ProcessBuilder pluginBuilder = new ProcessBuilder(appPluginBuilderArgs);
-
-            pluginBuilder.directory(currentDirectory.resolve("../opensilex-front-app/angular/dev-tools").toFile());
-            pluginBuilder.inheritIO();
-            Process pluginBuilderProcess = pluginBuilder.start();
-
-            // Start process to run Angular development server on port 4200
-            ProcessBuilder devServer = new ProcessBuilder(
-                    currentDirectory.resolve("../.ng/node/node").toFile().getCanonicalPath(),
-                    currentDirectory.resolve("../node_modules/.bin/ng").toFile().getCanonicalPath(), "serve",
-                    "--poll=2000");
-            devServer.directory(currentDirectory.resolve("../opensilex-front-app/angular").toFile());
-            devServer.inheritIO();
-            Process devServerProcess = devServer.start();
-
-            // Add hook to clean node.js processes on shutdown
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                public void run() {
-                    sharedBuilderProcess.destroy();
-                    pluginBuilderProcess.destroy();
-                    devServerProcess.destroy();
-                }
-            });
-        }
 
         MainCommand.run(new String[]{
             "server",

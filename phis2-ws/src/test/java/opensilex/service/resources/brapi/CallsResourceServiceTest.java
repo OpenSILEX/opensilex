3efad25c1ce8bcfa9afe01ae515d84f9a134b3dc
Vincent Migot <vincent.migot@inra.fr>
2019-09-30 18:10:30 +0200

    Restore phis unit test and update service generation config for brapi

--- phis2-ws/src/test/java/opensilex/service/resources/brapi/CallsResourceServiceTest.java
+++ phis2-ws/src/test/java/opensilex/service/resources/brapi/CallsResourceServiceTest.java
@@ -6,6 +6,9 @@
 // Contact: morgane.vidal@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
 //******************************************************************************
 package opensilex.service.resources.brapi;
+import javax.ws.rs.core.Response;
+import static junit.framework.TestCase.assertEquals;
+import org.junit.Test;
 import test.opensilex.server.rest.RestApplicationTest;
 
 /**
@@ -16,14 +19,14 @@ public class CallsResourceServiceTest extends RestApplicationTest {
     /**
      * Test if the returned code is 200 when the call is correct
      */
-//    @Test
-//    public void testCodeReturn() {
-//        final Response callResult = target("/brapi/v1/calls")
-//                .queryParam("datatype", "json")
-//                .queryParam("page", 0)
-//                .queryParam("pageSize", 1)
-//                .request().get();
-//
-//        assertEquals(200, callResult.getStatus());
-//    }
+    @Test
+    public void testCodeReturn() {
+        final Response callResult = target("/brapi/v1/calls")
+                .queryParam("datatype", "json")
+                .queryParam("page", 0)
+                .queryParam("pageSize", 1)
+                .request().get();
+
+        assertEquals(200, callResult.getStatus());
+    }
 }

25928cb7281781625f3d283c4a27e3ab7408856f
renaud colin <renaud.colin@inra.fr>
2020-01-09 16:09:33 +0100

    update Create and Get Experiment DTO with all ExperimentModel attributes
    Signed-off-by: renaud colin <renaud.colin@inra.fr>

--- opensilex-core/src/main/java/org/opensilex/core/experiment/api/ExperimentGetDTO.java
+++ opensilex-core/src/main/java/org/opensilex/core/experiment/api/ExperimentGetDTO.java
@@ -6,43 +6,65 @@
 package org.opensilex.core.experiment.api;
 
 import java.net.URI;
+import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.opensilex.core.experiment.dal.ExperimentModel;
+import org.opensilex.core.project.dal.ProjectModel;
+import org.opensilex.rest.user.dal.UserModel;
+import org.opensilex.sparql.model.SPARQLResourceModel;
 
 /**
- *
- * @author vidalmor
+ * @author Vincent MIGOT
  */
 public class ExperimentGetDTO {
-    
+
     private URI uri;
 
-    private String alias;
+    private String label;
 
-    private List<URI> projects;
+    private List<URI> projects = Collections.emptyList();
 
-    private String startDate;
+    private LocalDate startDate;
 
-    private String endDate;
+    private LocalDate endDate;
 
-    private String objectives;
+    private String objective;
 
     private String comment;
+
+    private Integer campaign;
+
+    private List<String> keywords = Collections.emptyList();
+
+    private List<URI> scientificSupervisors = Collections.emptyList();
+
+    private List<URI> technicalSupervisors = Collections.emptyList();
+
+    private List<URI> groups = Collections.emptyList();
+
+    public List<URI> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<URI> groups) {
+        this.groups = groups;
+    }
+
+    public void setUri(URI uri) { this.uri = uri; }
 
     public URI getUri() {
         return uri;
     }
 
-    public void setUri(URI uri) {
-        this.uri = uri;
+    public String getLabel() {
+        return label;
     }
 
-    public String getAlias() {
-        return alias;
-    }
-
-    public void setAlias(String alias) {
-        this.alias = alias;
+    public void setLabel(String label) {
+        this.label = label;
     }
 
     public List<URI> getProjects() {
@@ -53,28 +75,26 @@ public class ExperimentGetDTO {
         this.projects = projects;
     }
 
-    public String getStartDate() {
+    public LocalDate getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(String startDate) {
+    public void setStartDate(LocalDate startDate) {
         this.startDate = startDate;
     }
 
-    public String getEndDate() {
+    public LocalDate getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(String endDate) {
+    public void setEndDate(LocalDate endDate) {
         this.endDate = endDate;
     }
 
-    public String getObjectives() {
-        return objectives;
-    }
+    public String getObjective() { return objective; }
 
-    public void setObjectives(String objectives) {
-        this.objectives = objectives;
+    public void setObjective(String objective) {
+        this.objective = objective;
     }
 
     public String getComment() {
@@ -84,16 +104,67 @@ public class ExperimentGetDTO {
     public void setComment(String comment) {
         this.comment = comment;
     }
-    
+
+    public Integer getCampaign() { return campaign; }
+
+    public void setCampaign(Integer campaign) { this.campaign = campaign; }
+
+    public List<String> getKeywords() { return keywords; }
+
+    public void setKeywords(List<String> keywords) { this.keywords = keywords; }
+
+    public List<URI> getScientificSupervisors() { return scientificSupervisors;
+    }
+    public void setScientificSupervisors(List<URI> scientificSupervisors) { this.scientificSupervisors = scientificSupervisors; }
+
+    public List<URI> getTechnicalSupervisors() { return technicalSupervisors; }
+
+    public void setTechnicalSupervisors(List<URI> technicalSupervisors) { this.technicalSupervisors = technicalSupervisors; }
+
+
     public static ExperimentGetDTO fromModel(ExperimentModel model) {
+
         ExperimentGetDTO dto = new ExperimentGetDTO();
 
         dto.setUri(model.getUri());
-        dto.setAlias(model.getAlias());
+        dto.setLabel(model.getLabel());
+        dto.setStartDate(model.getStartDate());
+        dto.setEndDate(model.getEndDate());
 
-        dto.setObjectives(model.getObjectives());
+        dto.setCampaign(model.getCampaign());
+        dto.setObjective(model.getObjectives());
         dto.setComment(model.getComment());
-        
+        dto.setKeywords(model.getKeywords());
+
+        List<UserModel> scientificSupervisors = model.getScientificSupervisors();
+        if (scientificSupervisors != null) {
+            dto.setScientificSupervisors(scientificSupervisors.stream()
+                .map(SPARQLResourceModel::getUri)
+                .collect(Collectors.toList()));
+        } else {
+            dto.setScientificSupervisors(Collections.emptyList());
+        }
+
+        List<UserModel> technicalSupervisors = model.getTechnicalSupervisors();
+        if (technicalSupervisors != null) {
+            dto.setTechnicalSupervisors(technicalSupervisors.stream()
+                .map(SPARQLResourceModel::getUri)
+                .collect(Collectors.toList()));
+        }
+        else {
+            dto.setTechnicalSupervisors(Collections.emptyList());
+        }
+
+        List<ProjectModel> projectModels = model.getProjects();
+        if (projectModels != null) {
+            dto.setProjects(projectModels.stream()
+                .map(SPARQLResourceModel::getUri)
+                .collect(Collectors.toList()));
+        }
+        else{
+            dto.setProjects(Collections.emptyList());
+        }
+
         return dto;
     }
 }

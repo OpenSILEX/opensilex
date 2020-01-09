6d952a8a00c060b5cbf06c6db11b2a87aea48ec8
renaud colin <renaud.colin@inra.fr>
2020-01-09 16:10:19 +0100

    add ExperimentDao unit tests
    Signed-off-by: renaud colin <renaud.colin@inra.fr>

---
+++ opensilex-core/src/test/java/org/opensilex/core/AbstractResourceModelTest.java
@@ -0,0 +1,51 @@
+package org.opensilex.core;
+
+import org.eclipse.rdf4j.repository.Repository;
+import org.eclipse.rdf4j.repository.sail.SailRepository;
+import org.eclipse.rdf4j.sail.memory.MemoryStore;
+import org.eclipse.rdf4j.sail.shacl.ShaclSail;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.opensilex.sparql.SPARQLModule;
+import org.opensilex.sparql.exceptions.SPARQLQueryException;
+import org.opensilex.sparql.rdf4j.RDF4JConnection;
+import org.opensilex.sparql.service.SPARQLService;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+/**
+ * @author Renaud COLIN
+ */
+public abstract class AbstractResourceModelTest {
+
+    protected static Repository repository;
+    protected static SPARQLService service;
+
+    @BeforeClass
+    public static void initialize() throws Exception {
+
+        repository = new SailRepository(new ShaclSail(new MemoryStore()));
+        repository.init();
+
+        service = new SPARQLService(new RDF4JConnection(repository.getConnection()));
+        service.startup();
+    }
+
+    @AfterClass
+    public static void end() throws Exception {
+        service.shutdown();
+    }
+
+    @After
+    public void cleanGraph() throws URISyntaxException, SPARQLQueryException {
+
+        for(String graphName : getGraphsToCleanNames()){
+            service.clearGraph(SPARQLModule.getPlatformDomainGraphURI(graphName));
+        }
+        service.clearGraph(SPARQLModule.getPlatformURI());
+    }
+
+    protected abstract List<String> getGraphsToCleanNames();
+}

6fdb1f2bedc2b0d511cee96fe0052e77fbda03ff
renaud colin <renaud.colin@inra.fr>
2020-01-21 13:17:09 +0100

    add species to GetDTO and  add a search DTO

--- opensilex-core/src/main/java/org/opensilex/core/experiment/api/ExperimentGetDTO.java
+++ opensilex-core/src/main/java/org/opensilex/core/experiment/api/ExperimentGetDTO.java
@@ -11,49 +11,53 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.opensilex.core.experiment.dal.ExperimentModel;
 import org.opensilex.core.project.dal.ProjectModel;
+import org.opensilex.rest.group.dal.GroupModel;
+import org.opensilex.rest.serialization.LocalDateDeserializer;
+import org.opensilex.rest.serialization.LocalDateSerializer;
 import org.opensilex.rest.user.dal.UserModel;
 import org.opensilex.sparql.model.SPARQLResourceModel;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 /**
  * @author Vincent MIGOT
  */
 public class ExperimentGetDTO {
 
-    private URI uri;
+    protected URI uri;
 
-    private String label;
+    protected String label;
 
-    private List<URI> projects = Collections.emptyList();
+    protected List<URI> projects = Collections.emptyList();
 
-    private LocalDate startDate;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    protected LocalDate startDate;
 
-    private LocalDate endDate;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    protected LocalDate endDate;
 
-    private String objective;
+    protected String objective;
 
-    private String comment;
+    protected String comment;
 
-    private Integer campaign;
+    protected Integer campaign;
 
-    private List<String> keywords = Collections.emptyList();
+    protected List<String> keywords = Collections.emptyList();
 
-    private List<URI> scientificSupervisors = Collections.emptyList();
+    protected List<URI> scientificSupervisors = Collections.emptyList();
 
-    private List<URI> technicalSupervisors = Collections.emptyList();
+    protected List<URI> technicalSupervisors = Collections.emptyList();
 
-    private List<URI> groups = Collections.emptyList();
+    protected List<URI> groups = Collections.emptyList();
 
-    public List<URI> getGroups() {
-        return groups;
-    }
+    protected List<URI> species = Collections.emptyList();
 
-    public void setGroups(List<URI> groups) {
-        this.groups = groups;
-    }
-
-    public void setUri(URI uri) { this.uri = uri; }
 
     public URI getUri() {
         return uri;
@@ -63,84 +67,133 @@ public class ExperimentGetDTO {
         return label;
     }
 
-    public void setLabel(String label) {
-        this.label = label;
-    }
-
     public List<URI> getProjects() {
         return projects;
-    }
-
-    public void setProjects(List<URI> projects) {
-        this.projects = projects;
     }
 
     public LocalDate getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
     public LocalDate getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getObjective() { return objective; }
-
-    public void setObjective(String objective) {
-        this.objective = objective;
+    public String getObjective() {
+        return objective;
     }
 
     public String getComment() {
         return comment;
     }
 
-    public void setComment(String comment) {
+    public Integer getCampaign() {
+        return campaign;
+    }
+
+    public List<String> getKeywords() {
+        return keywords;
+    }
+
+    public List<URI> getScientificSupervisors() {
+        return scientificSupervisors;
+    }
+
+    public List<URI> getTechnicalSupervisors() {
+        return technicalSupervisors;
+    }
+
+    public List<URI> getGroups() {
+        return groups;
+    }
+
+    public List<URI> getSpecies() {
+        return species;
+    }
+
+    public ExperimentGetDTO setUri(URI uri) {
+        this.uri = uri;
+        return this;
+    }
+
+    public ExperimentGetDTO setLabel(String label) {
+        this.label = label;
+        return this;
+    }
+
+    public ExperimentGetDTO setProjects(List<URI> projects) {
+        this.projects = projects;
+        return this;
+    }
+
+    public ExperimentGetDTO setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+        return this;
+    }
+
+    public ExperimentGetDTO setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+        return this;
+    }
+
+    public ExperimentGetDTO setObjective(String objective) {
+        this.objective = objective;
+        return this;
+    }
+
+    public ExperimentGetDTO setComment(String comment) {
         this.comment = comment;
+        return this;
     }
 
-    public Integer getCampaign() { return campaign; }
-
-    public void setCampaign(Integer campaign) { this.campaign = campaign; }
-
-    public List<String> getKeywords() { return keywords; }
-
-    public void setKeywords(List<String> keywords) { this.keywords = keywords; }
-
-    public List<URI> getScientificSupervisors() { return scientificSupervisors;
+    public ExperimentGetDTO setCampaign(Integer campaign) {
+        this.campaign = campaign;
+        return this;
     }
-    public void setScientificSupervisors(List<URI> scientificSupervisors) { this.scientificSupervisors = scientificSupervisors; }
 
-    public List<URI> getTechnicalSupervisors() { return technicalSupervisors; }
+    public ExperimentGetDTO setKeywords(List<String> keywords) {
+        this.keywords = keywords;
+        return this;
+    }
 
-    public void setTechnicalSupervisors(List<URI> technicalSupervisors) { this.technicalSupervisors = technicalSupervisors; }
+    public ExperimentGetDTO setScientificSupervisors(List<URI> scientificSupervisors) {
+        this.scientificSupervisors = scientificSupervisors;
+        return this;
+    }
 
+    public ExperimentGetDTO setTechnicalSupervisors(List<URI> technicalSupervisors) {
+        this.technicalSupervisors = technicalSupervisors;
+        return this;
+    }
+
+    public ExperimentGetDTO setGroups(List<URI> groups) {
+        this.groups = groups;
+        return this;
+    }
+
+    public ExperimentGetDTO setSpecies(List<URI> species) {
+        this.species = species;
+        return this;
+    }
 
     public static ExperimentGetDTO fromModel(ExperimentModel model) {
 
         ExperimentGetDTO dto = new ExperimentGetDTO();
 
-        dto.setUri(model.getUri());
-        dto.setLabel(model.getLabel());
-        dto.setStartDate(model.getStartDate());
-        dto.setEndDate(model.getEndDate());
-
-        dto.setCampaign(model.getCampaign());
-        dto.setObjective(model.getObjectives());
-        dto.setComment(model.getComment());
-        dto.setKeywords(model.getKeywords());
+        dto.setUri(model.getUri())
+                .setLabel(model.getLabel())
+                .setStartDate(model.getStartDate())
+                .setEndDate(model.getEndDate())
+                .setCampaign(model.getCampaign())
+                .setObjective(model.getObjectives())
+                .setComment(model.getComment())
+                .setKeywords(model.getKeywords());
 
         List<UserModel> scientificSupervisors = model.getScientificSupervisors();
         if (scientificSupervisors != null) {
             dto.setScientificSupervisors(scientificSupervisors.stream()
-                .map(SPARQLResourceModel::getUri)
-                .collect(Collectors.toList()));
+                    .map(SPARQLResourceModel::getUri)
+                    .collect(Collectors.toList()));
         } else {
             dto.setScientificSupervisors(Collections.emptyList());
         }
@@ -148,23 +201,29 @@ public class ExperimentGetDTO {
         List<UserModel> technicalSupervisors = model.getTechnicalSupervisors();
         if (technicalSupervisors != null) {
             dto.setTechnicalSupervisors(technicalSupervisors.stream()
-                .map(SPARQLResourceModel::getUri)
-                .collect(Collectors.toList()));
-        }
-        else {
+                    .map(SPARQLResourceModel::getUri)
+                    .collect(Collectors.toList()));
+        } else {
             dto.setTechnicalSupervisors(Collections.emptyList());
         }
 
         List<ProjectModel> projectModels = model.getProjects();
         if (projectModels != null) {
             dto.setProjects(projectModels.stream()
-                .map(SPARQLResourceModel::getUri)
-                .collect(Collectors.toList()));
-        }
-        else{
+                    .map(SPARQLResourceModel::getUri)
+                    .collect(Collectors.toList()));
+        } else {
             dto.setProjects(Collections.emptyList());
         }
 
+        List<GroupModel> groupModels = model.getGroups();
+        if (groupModels != null) {
+            dto.setGroups(groupModels.stream()
+                    .map(SPARQLResourceModel::getUri)
+                    .collect(Collectors.toList()));
+        } else {
+            dto.setGroups(Collections.emptyList());
+        }
         return dto;
     }
 }

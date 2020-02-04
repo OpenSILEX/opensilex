50498edc67e3471a9f26056662059b4c85437ccb
Vincent Migot <vincent.migot@inrae.fr>
2020-02-04 15:26:53 +0100

    Add logs to debug cookie issues

--- opensilex-front/front/src/main.ts
+++ opensilex-front/front/src/main.ts
@@ -48,18 +48,18 @@ console.debug("Logger initialized in debug mode");
 
 // Initialize service API container
 let baseApi = DEV_BASE_API_PATH;
-if (isDebug) {
+if (isDevMode) {
   console.warn(
     'Vue is running in development mode, with base API set by default to ' + DEV_BASE_API_PATH + '\n' +
     'If you start your webservices server with another host or port configuration,\n' +
     'please edit opensilex-front/front/src/main.ts and update DEV_BASE_API_PATH constant'
   );
-}
-
-if (!isDevMode) {
+} else {
   let splitURI = window.location.href.split("/");
   baseApi = splitURI[0] + "//" + splitURI[2] + "/rest"
 }
+
+console.debug("Base API URI:", baseApi);
 
 // Setup store imports
 import store from './models/Store'

f7c9405ab7e268f7cb1c1019f8017594dcc3d7d2
jbonnefont <jbonnefont2@gmail.com>
2020-02-28 13:58:39 +0100

    case when no sci object in a selected experiment

--- phis2-ws/front/src/components/images/ImageView.vue
+++ phis2-ws/front/src/components/images/ImageView.vue
@@ -99,7 +99,16 @@ export default class ImageView extends Vue {
       this.searchImagesFields.concernedItemsValue = form.objectList;
     }
     if (form.rdfType !== null) {
-      this.loadData();
+      if (
+        this.searchImagesFields.experiment !== null &&
+        form.objectList.length === 0
+      ) {
+        this.images = [];
+        this.totalImages = 0;
+        this.showedImages = 0;
+      } else {
+        this.loadData();
+      }
     }
   }
 
@@ -150,7 +159,7 @@ export default class ImageView extends Vue {
         (http: HttpResponse<OpenSilexResponse<Array<FileDescriptionDTO>>>) => {
           const res = http.response.result as any;
           const data = res.data as Array<FileDescriptionDTO>;
-          if (http.response.metadata.pagination!==null) {
+          if (http.response.metadata.pagination !== null) {
             this.totalImages = http.response.metadata.pagination.totalCount;
           } else {
             if (data.length === 1) {

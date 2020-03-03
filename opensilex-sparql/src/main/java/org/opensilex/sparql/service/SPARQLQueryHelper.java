9870fe71c6966c1a5bdc118966989cf96d805aae
Vincent Migot <vincent.migot@inrae.fr>
2020-03-03 11:57:40 +0100

    Allow lang filter for not translated messages

--- opensilex-sparql/src/main/java/org/opensilex/sparql/service/SPARQLQueryHelper.java
+++ opensilex-sparql/src/main/java/org/opensilex/sparql/service/SPARQLQueryHelper.java
@@ -125,7 +125,10 @@ public class SPARQLQueryHelper {
     }
     
     public static Expr langFilter(String varName, String lang) {
-        return exprFactory.langMatches(exprFactory.lang(NodeFactory.createVariable(varName)), lang);
+        return or(
+                exprFactory.langMatches(exprFactory.lang(NodeFactory.createVariable(varName)), lang),
+                exprFactory.eq(exprFactory.lang(NodeFactory.createVariable(varName)), "")
+        );
     }
 
     /**

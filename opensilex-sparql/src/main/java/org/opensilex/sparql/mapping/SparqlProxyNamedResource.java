3d45f50c6d61579374c92e8e98c9781802313253
renaud colin <renaud.colin@inra.fr>
2020-07-15 14:33:38 +0200

    add the SparqlProxyNamedResource
    Signed-off-by: renaud colin <renaud.colin@inra.fr>

---
+++ opensilex-sparql/src/main/java/org/opensilex/sparql/mapping/SparqlProxyNamedResource.java
@@ -0,0 +1,44 @@
+package org.opensilex.sparql.mapping;
+
+import org.apache.jena.graph.Node;
+import org.opensilex.sparql.model.SPARQLNamedResourceModel;
+import org.opensilex.sparql.service.SPARQLService;
+
+import java.lang.reflect.Method;
+import java.net.URI;
+
+/**
+ * A specialisation of a {@link SPARQLProxyResource} for retrieving a {@link SparqlProxyNamedResource#name}
+ *
+ * @author Vincent Migot
+ * @author Renaud COLIN
+ */
+class SparqlProxyNamedResource<T extends SPARQLNamedResourceModel<T>> extends SPARQLProxyResource<T> {
+
+    protected final String name;
+
+    private static final Method getNameMethod;
+
+    static {
+        try{
+            getNameMethod = SPARQLNamedResourceModel.class.getMethod("getName");
+        }catch (NoSuchMethodException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    SparqlProxyNamedResource(SPARQLClassObjectMapperIndex repository, Node graph, URI uri, Class<T> type, String name, String lang, SPARQLService service) throws Exception {
+        super(repository,graph,uri, type, lang, service);
+        this.name = name;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (getNameMethod.equals(method)){
+            return name;
+        } else {
+            return super.invoke(proxy, method, args);
+        }
+    }
+
+}

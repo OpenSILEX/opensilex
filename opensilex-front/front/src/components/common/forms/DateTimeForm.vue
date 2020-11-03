b30ac4830c3d74435ca1854c4fd161647b88ba34
rcolin <renaud.colin@inrae.fr>
2020-11-03 14:51:56 +0100

    Update DateTimeForm by using vue-datetime component
    Signed-off-by: rcolin <renaud.colin@inrae.fr>

--- opensilex-front/front/src/components/common/forms/DateTimeForm.vue
+++ opensilex-front/front/src/components/common/forms/DateTimeForm.vue
@@ -1,69 +1,71 @@
 <template>
-  <opensilex-FormField
-    :required="required"
-    :label="label"
-    :helpMessage="helpMessage"
-  >
-    <template v-slot:field="field">
-      <b-form-input
-        :id="field.id"
-        :value="stringValue"
-        @update="updateValue($event)"
-        @change="change"
-        :disabled="disabled"
-        type="text"
-        :required="isRequired"
-        :placeholder="$t(placeholder)"
-      ></b-form-input>
-    </template>
-  </opensilex-FormField>
+    <opensilex-FormField
+            :required="required"
+            :label="label"
+            :helpMessage="helpMessage"
+    >
+        <template v-slot:field="field">
+            <datetime
+                    v-model="datetime"
+                    :type="type"
+                    :zone="zone"
+                    :value-zone="valueZone"
+                    :flow="flow"
+                    :format="format"
+                    @input="$emit('input', $event)"
+                    @close="$emit('close', $event)"
+            >
+            </datetime>
+        </template>
+    </opensilex-FormField>
 </template>
 
 <script lang="ts">
-import {
-  Component,
-  Prop,
-  Model,
-  Provide,
-  PropSync
-} from "vue-property-decorator";
-import Vue from "vue";
+    import {
+        Component,
+        Prop,
+        PropSync
+    } from "vue-property-decorator";
+    import Vue from "vue";
 
-@Component
-export default class DateTimeForm extends Vue {
-  $opensilex: any;
+    @Component
+    export default class DateTimeForm extends Vue {
 
-  @PropSync("value")
-  stringValue: string;
+        $opensilex: any;
 
-  updateValue(newValue) {
-    if (newValue == "") {
-      this.stringValue = null;
-    } else {
-      this.stringValue = newValue;
+        @Prop()
+        label: string;
+
+        @Prop()
+        helpMessage: string;
+
+        @Prop()
+        placeholder: string;
+
+        @PropSync("required")
+        isRequired: boolean;
+
+        @Prop({default: false})
+        disabled: boolean;
+
+        @PropSync("value")
+        datetime: string;
+
+        @Prop({default: "datetime"})
+        type: string;
+
+        @Prop({default: "local"})
+        zone: string;
+
+        @Prop({default: "local"})
+        valueZone: string;
+
+        @Prop({default: "dd-MMM-yyyy HH:mm ZZ"})
+        format: string;
+
+        flow: Array<string> = ['date','time'];
+
     }
-  }
-
-  change(value){
-    this.$emit("change",value);
-  }
-
-  @Prop()
-  label: string;
-
-  @Prop()
-  helpMessage: string;
-
-  @Prop()
-  placeholder: string;
-
-  @PropSync("required")
-  isRequired: boolean;
-
-  @Prop()
-  disabled: boolean;
-
-}
 </script>
 
 <style scoped lang="scss">

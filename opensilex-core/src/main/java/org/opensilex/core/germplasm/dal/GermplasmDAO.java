1ca44e4113a1e9173a55197743b02f55e8bf5b6d
Vincent Migot <vincent.migot@inrae.fr>
2021-01-04 18:58:49 +0100

    Restore scientific objects list search

--- opensilex-core/src/main/java/org/opensilex/core/germplasm/dal/GermplasmDAO.java
+++ opensilex-core/src/main/java/org/opensilex/core/germplasm/dal/GermplasmDAO.java
@@ -28,6 +28,7 @@ import org.apache.jena.sparql.core.Var;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
 import org.opensilex.core.experiment.dal.ExperimentModel;
+import org.opensilex.core.factor.dal.FactorModel;
 import org.opensilex.core.germplasm.api.GermplasmCreationDTO;
 import org.opensilex.core.ontology.Oeso;
 import org.opensilex.nosql.exceptions.NoSQLInvalidURIException;
@@ -201,6 +202,10 @@ public class GermplasmDAO {
         }
         return germplasm;
 
+    }
+
+    public List<GermplasmModel> getList(List<URI> uris) throws Exception {
+        return sparql.getListByURIs(GermplasmModel.class, uris, null);
     }
 
     public ListWithPagination<GermplasmModel> search(
--- opensilex-front/front/src/components/common/views/TableAsyncView.vue
+++ opensilex-front/front/src/components/common/views/TableAsyncView.vue
@@ -254,6 +254,10 @@ export default class TableAsyncView extends Vue {
     this.tableRef.refresh();
   }
 
+  update() {
+    this.tableRef.refresh();
+  }
+
 
   onRefreshed() {
     let that = this;

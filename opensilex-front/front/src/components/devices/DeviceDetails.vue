faa8f9e2a225b74aa14a939c2c324f701c94b00a
Emilie Fernandez <emilie.fernandez@inrae.fr>
2021-01-21 16:43:56 +0100

    device / add details page

--- opensilex-front/front/src/components/devices/DeviceDetails.vue
+++ opensilex-front/front/src/components/devices/DeviceDetails.vue
@@ -1,148 +1,180 @@
 <template>
-    <div class="container-fluid">
+  <div class="container-fluid">
     <opensilex-PageHeader
-      icon="ik#ik-thermometer"
+      icon="ik#ik-file-text"
       title="DeviceDetails.title"
       :description="device.name"
     ></opensilex-PageHeader>
 
-    <opensilex-PageActions :tabs="true" :returnButton="true" >       
-      <template v-slot>
-        <b-nav-item
-          :active="isDetailsTab()"
-          :to="{path: '/device/details/' + encodeURIComponent(uri)}"
-        >{{ $t('DeviceDetails.details') }}
-        </b-nav-item>
-
-        <b-nav-item
-          class="ml-3"
-          :active="isAnnotationTab()"
-          :to="{ path: '/device/annotations/' + encodeURIComponent(uri) }"
-        >{{ $t("DeviceDetails.annotation") }}
-        </b-nav-item>
-
-        <b-nav-item
-          :active="isDocumentTab()"
-          :to="{ path: '/device/documents/' + encodeURIComponent(uri) }"
-        >{{ $t('DeviceDetails.documents') }}</b-nav-item> 
-
-        </template>
+    <opensilex-PageActions :returnButton="true" >       
     </opensilex-PageActions>
 
     <opensilex-PageContent>
-        <template v-slot>
-          <opensilex-DeviceDescription v-if="isDetailsTab()" :uri="uri"></opensilex-DeviceDescription>
-          <opensilex-DocumentTabList
-            v-else-if="isDocumentTab()"
-            :uri="uri"
-            :modificationCredentialId="credentials.CREDENTIAL_DEVICE_MODIFICATION_ID"
-            :deleteCredentialId="credentials.CREDENTIAL_DEVICE_DELETE_ID"
-          ></opensilex-DocumentTabList>
+      <b-row>
+        <b-col sm="5">
+          <opensilex-Card label="DeviceDetails.description" icon="ik#ik-clipboard">            
+          <template v-slot:rightHeader>
+            <opensilex-EditButton
+              v-if="user.hasCredential(credentials.CREDENTIAL_DEVICE_MODIFICATION_ID)"
+              label="DeviceDetails.update"
+            ></opensilex-EditButton>
+            <opensilex-DeleteButton
+              v-if="user.hasCredential(credentials.CREDENTIAL_DEVICE_DELETE_ID)"
+              label="DeviceDetails.delete"
+              :small="true"
+            ></opensilex-DeleteButton>
 
-          <opensilex-AnnotationList
-            v-else-if="isAnnotationTab()"
-            ref="annotationList"
-            :target="uri"
-            :displayTargetColumn="false"
-            :enableActions="true"
-            :modificationCredentialId="credentials.CREDENTIAL_DEVICE_MODIFICATION_ID"
-            :deleteCredentialId="credentials.CREDENTIAL_DEVICE_DELETE_ID"
-            @onEdit="annotationModalForm.showEditForm($event)"
-            ></opensilex-AnnotationList>
-        </template>
+          </template>
+            <template v-slot:body>
+              <opensilex-UriView :uri="device.uri"></opensilex-UriView>
+              <opensilex-StringView label="DeviceDetails.type" :value="device.rdf_type"></opensilex-StringView>
+              <opensilex-StringView label="DeviceDetails.brand" :value="device.brand"></opensilex-StringView>
+              <opensilex-StringView label="DeviceDetails.constructorModel" :value="device.constructor_model"></opensilex-StringView>
+              <opensilex-StringView label="DeviceDetails.serialNumber" :value="device.serial_number"></opensilex-StringView>
+              <opensilex-StringView label="DeviceDetails.personInCharge" :uri="device.person_in_charge"></opensilex-StringView> 
+              <opensilex-StringView label="DeviceDetails.obtained" :value="device.obtained"></opensilex-StringView>
+            </template>
+          </opensilex-Card>
+      </b-col>
+
+      <b-col sm="5">
+        <opensilex-Card label="DeviceDetails.annex">
+            <template v-slot:body>
+
+            </template>
+        </opensilex-Card>
+      </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col sm="5">
+          <opensilex-Card label="DeviceDetails.localisation" icon="ik#ik-clipboard">            
+          <template v-slot:rightHeader>
+
+          </template>
+            <template v-slot:body>
+
+            </template>
+          </opensilex-Card>
+      </b-col>
+
+      <b-col sm="5">
+        <opensilex-Card label="DeviceDetails.variables">
+            <template v-slot:body>
+
+            </template>
+        </opensilex-Card>
+      </b-col>
+
+      </b-row>
     </opensilex-PageContent>
-    </div>
+
+  </div>
 </template>
 
 <script lang="ts">
-    import VueRouter from "vue-router";
-    import {Component, Ref} from "vue-property-decorator";
-    import Vue from "vue";
-    import HttpResponse, {OpenSilexResponse} from "../../lib/HttpResponse";
-    import {DeviceGetDTO, DevicesService} from "opensilex-core/index";
-    import AnnotationList from "../annotations/list/AnnotationList.vue";
+import { Component, Prop, Ref } from "vue-property-decorator";
+import Vue from "vue";
+import {
+  DeviceDTO,
+  DevicesService
+} from "opensilex-core/index";
+import {
+  SecurityService,
+  UserGetDTO
+} from "opensilex-security/index";
+import HttpResponse, { OpenSilexResponse } from "../../lib/HttpResponse";
 
-    @Component
-    export default class DeviceDetails extends Vue {
-        $opensilex: any;
-        $route: any;
+@Component
+export default class DeviceDetails extends Vue {
+  $opensilex: any;
+  $store: any;
+  $route: any;
+  $t: any;
+  $i18n: any;
+  service: DevicesService;
+  uri: string = null;
 
-        service: DevicesService;
-        uri = null;
-        name: string = "";
+  @Ref("modalRef") readonly modalRef!: any;
 
-        @Ref("annotationList") readonly annotationList!: AnnotationList;
+  get user() {
+    return this.$store.state.user;
+  }
 
-        get user() {
-            return this.$store.state.user;
-        }
+  get credentials() {
+    return this.$store.state.credentials;
+  }
+  
+  refresh() {
+    this.modalRef.refresh();
+  }
 
-        get credentials() {
-            return this.$store.state.credentials;
-        }
+  device: DeviceDTO = { 
+        uri: null,
+        rdf_type: null,
+        name: null,
+        brand: null,
+        constructor_model: null,
+        serial_number: null,
+        person_in_charge: null,
+        obtained: null,
+        date_of_last_use: null,
+        relations: null
+      };
+  
+  created() {
+    this.service = this.$opensilex.getService("opensilex.DevicesService");
+    this.uri = decodeURIComponent(this.$route.params.uri);
+    this.loadDevice(this.uri);
+  }
 
-        device: DeviceGetDTO = { 
-              uri: null,
-              rdf_type: null,
-              name: null,
-              brand: null,
-              constructor_model: null,
-              serial_number: null,
-              person_in_charge: null,
-              start_up: null,
-              removal: null,
-              relations: null,
-              description: null
-            };
-            
-        created() {
-          this.service = this.$opensilex.getService("opensilex.DevicesService");
-          this.uri = decodeURIComponent(this.$route.params.uri);
-          this.loadDevice(this.uri);
-        }
-
-        loadDevice(uri: string) {
-          this.service
-            .getDevice(uri)
-            .then((http: HttpResponse<OpenSilexResponse<DeviceGetDTO>>) => {
-              this.device = http.response.result;
-            })
-            .catch(this.$opensilex.errorHandler);
-        }
-
-        isDetailsTab() {
-            return this.$route.path.startsWith("/device/details/");
-        }
-
-        isDocumentTab() {
-            return this.$route.path.startsWith("/device/documents/");
-        }
-
-        isAnnotationTab() {
-            return this.$route.path.startsWith("/device/annotations/");
-        }
-
-    }
+  loadDevice(uri: string) {
+    this.service
+      .getDevice(uri)
+      .then((http: HttpResponse<OpenSilexResponse<DeviceDTO>>) => {
+        this.device = http.response.result;
+      })
+      .catch(this.$opensilex.errorHandler);
+  }
+  
+}
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 </style>
 
 <i18n>
 en:
   DeviceDetails:
+    annex: Annex
     title: Device
     description: Description
-    details: Details
-    annotation: Annotation
-    documents: Documents
+    uri: URI
+    name: Name
+    type: Type
+    obtained: Obtained
+    brand: Brand
+    constructorModel: Constructor model
+    serialNumber: Serial number
+    personInCharge: Person in charge
+    localisation: Localisation
+    variables: Variables
 
 fr:
   DeviceDetails:
+    annex: Annexe
     title: Dispositif
     description: Description
-    details: Details
-    annotations: Annotations
-    documents: Documents
+    uri: URI
+    name: Nom
+    type: Type
+    obtained: Date d'obtention
+    brand: Marque
+    constructorModel: Modèle du constructeur
+    serialNumber: Numéro de série
+    personInCharge: Personne en charge
+    localisation: Localisation
+    variables: Variables
+
 
 </i18n>

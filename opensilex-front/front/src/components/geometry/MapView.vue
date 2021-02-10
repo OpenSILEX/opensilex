44449b837d3722b19aeababb21e4d5b57b8fae2f
Jean Philippe Vert <jean-philippe.vert@inrae.fr>
2021-02-10 15:01:46 +0100

    Display of the name of a scientific object or a zone when hovering over the cursor

--- opensilex-front/front/src/components/geometry/MapView.vue
+++ opensilex-front/front/src/components/geometry/MapView.vue
@@ -67,7 +67,7 @@
         >
           <template slot-scope="scope">
             <div class="panel-content">
-              {{ selectPointerMove[0] + " (" + selectPointerMove[1] + ")" }}
+              {{ $t('MapView.hover') + " : " + selectPointerMove }}
             </div>
           </template>
         </vl-overlay>
@@ -205,7 +205,7 @@ export default class MapView extends Vue {
   featuresOS: any[] = [];
   featuresArea: any[] = [];
   temporaryArea: any[] = [];
-  selectPointerMove: any[] = [];
+  selectPointerMove: string = "";
   overlayCoordinate: any[] = [];
   fieldsSelected = [
     {
@@ -271,9 +271,10 @@ export default class MapView extends Vue {
         (feature) => feature
     );
     if (hitFeature) {
-      this.selectPointerMove = [hitFeature.values_.name, this.nameType(hitFeature.values_.type)];
+      console.log(hitFeature);
+      this.selectPointerMove = hitFeature.values_.name;
     } else {
-      this.selectPointerMove = [];
+      this.selectPointerMove = "";
     }
   }
 
@@ -483,7 +484,6 @@ export default class MapView extends Vue {
 
   private areaRecovery() {
     let coordinateExtent = transformExtent(this.mapView.$view.calculateExtent(), "EPSG:3857", "EPSG:4326")
-    this.overlayCoordinate = [coordinateExtent[0] + (coordinateExtent[2] - coordinateExtent[0]) * 0.0303111, coordinateExtent[3] + (coordinateExtent[1] - coordinateExtent[3]) * 0.025747];
 
     let geometry = {
       type: "Polygon",
@@ -593,7 +593,8 @@ en:
     Legend: Legend
     LegendSO: Scientific Object
     LegendArea: Area
-    Instruction: Press Shift to <b>select item by item</b> on the map. Press and hold Shift + Alt + left Click and move the mouse to rotate the map. Press Ctrl + left Click while dragging to <b>select multiple scientific objects</b>.
+    Instruction: Press Shift to <b>select item by item</b> on the map. Press and hold Shift +Alt + Right Click and move the mouse to rotate the map. Press Ctrl+Right Click while dragging to <b>select multiple scientific objects</b>.
+    hover: name hover
   Area:
     title: Area
     add: Description of the area
@@ -613,7 +614,8 @@ fr:
     Legend: Légende
     LegendSO: Objet scientifique
     LegendArea: Zone
-    Instruction: Appuyez sur Shift pour <b>sélectionner élément par élément</b> sur la carte. Appuyez et maintenez Shift +Alt + Clic  gauche  puis déplacer la souris pour faire <b>pivoter</b> la carte. Appuyez sur Ctrl + Clic gauche tout en faisant glisser pour <b>sélectionner plusieurs objets scientifiques</b>.
+    Instruction: Appuyez sur Shift pour <b>sélectionner élément par élément</b> sur la carte. Appuyez et maintenez Shift +Alt + Clic  Droit  puis déplacer la souris pour faire <b>pivoter</b> la carte. Appuyez sur Ctrl+Clic droit tout en faisant glisser pour <b>sélectionner plusieurs objets scientifiques</b>.
+    hover: nom survolez
   Area:
     title: Zone
     add: Description de la zone

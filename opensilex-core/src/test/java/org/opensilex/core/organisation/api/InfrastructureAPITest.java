05f41d10051d06a41499c6aa0e4ded8cfadf13e4
Jean Philippe Vert <jean-philippe.vert@inrae.fr>
2021-03-12 09:36:31 +0100

    Correction of the name of the infrastructure directory in organization

--- opensilex-core/src/test/java/org/opensilex/core/organisation/api/InfrastructureAPITest.java
+++ opensilex-core/src/test/java/org/opensilex/core/organisation/api/InfrastructureAPITest.java
@@ -1,5 +1,5 @@
 //******************************************************************************
-//                          ExperimentAPITest.java
+//                          InfrastructureAPITest.java
 // OpenSILEX - Licence AGPL V3.0 - https://www.gnu.org/licenses/agpl-3.0.en.html
 // Copyright © INRAE 2020
 // Contact: renaud.colin@inrae.fr, anne.tireau@inrae.fr, pascal.neveu@inrae.fr

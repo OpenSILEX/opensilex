ad23ae7ccb244e5e93fcf7b898d36e6e6169f066
rcolin <renaud.colin@inrae.fr>
2021-03-25 00:24:29 +0100

    Append constraint and validator on move
    Signed-off-by: rcolin <renaud.colin@inrae.fr>

--- opensilex-core/src/main/java/org/opensilex/core/event/api/validation/MoveLocationOrPositionNotNullValidator.java
+++ opensilex-core/src/main/java/org/opensilex/core/event/api/validation/MoveLocationOrPositionNotNullValidator.java
@@ -29,12 +29,8 @@ public class MoveLocationOrPositionNotNullValidator implements ConstraintValidat
 
         List<ConcernedItemPositionCreationDTO> positions = moveCreationDTO.getConcernedItemPositions();
 
-        if (moveCreationDTO.getTo() == null && moveCreationDTO.getFrom() == null && CollectionUtils.isEmpty(positions)) {
-            return updateContextViolationTemplateWithMessage(context, "no location or position : to, from and targets_positions are null or empty");
-        }
-
-        if(positions == null){
-            return true;
+        if (moveCreationDTO.getTo() == null && CollectionUtils.isEmpty(positions)) {
+            return updateContextViolationTemplateWithMessage(context, "no location or position : to and targets_positions are null or empty");
         }
 
         for (int i = 0; i < positions.size(); i++) {

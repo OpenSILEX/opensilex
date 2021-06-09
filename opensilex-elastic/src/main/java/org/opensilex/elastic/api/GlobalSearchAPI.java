aabae7a0aa1a71f9fa40b461542440e37de95cfa
assarar <cheimae.assarar@inrae.fr>
2021-06-09 10:34:21 +0200

    Update GlabalearchAPI

--- opensilex-elastic/src/main/java/org/opensilex/elastic/api/GlobalSearchAPI.java
+++ opensilex-elastic/src/main/java/org/opensilex/elastic/api/GlobalSearchAPI.java
@@ -93,7 +93,7 @@ public class GlobalSearchAPI  {
         int from = page*pageSize;
         
         //Creates the SearchRequest. Without arguments this runs against all indices.
-        SearchRequest searchRequest = new SearchRequest();
+        SearchRequest searchRequest = new SearchRequest("variables","projects");
         
         QueryStringQueryBuilder query  = QueryBuilders.queryStringQuery(namePattern);
         SearchSourceBuilder sourceBuilder = new SearchSourceBuilder(); 

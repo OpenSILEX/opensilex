4800410df3ebe1e4e8f7341e893a22f8384d83a5
assarar <cheimae.assarar@inrae.fr>
2021-06-14 09:15:31 +0200

    add configuration files to opensilex-elastic/front

---
+++ opensilex-elastic/front/vue.config.js
@@ -0,0 +1,27 @@
+const TerserPlugin = require('terser-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    configureWebpack: {
+        externals: {
+            'vue': 'Vue',
+            'core-js': 'core-js',
+            'vue-router': 'vue-router',
+            'vuex': 'vuex',
+            'node-fetch': 'node-fetch',
+            'vee-validate': 'vee-validate'
+        },
+        resolve: {
+			alias: {
+				'vue$': path.resolve('../../node_modules/vue/dist/vue.esm.js')
+            }
+		},
+        performance: {
+            hints: false
+        },
+        optimization: {
+            minimize: (process.env.NODE_ENV === 'production'),
+            minimizer: [new TerserPlugin()]
+        }
+    }
+};

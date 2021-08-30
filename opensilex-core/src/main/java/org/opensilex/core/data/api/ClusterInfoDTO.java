5613811577f1bdb59f2a9ea5d9a3ac61e312cfae
Alexandre Juan <juandev-pro@protonmail.com>
2021-08-30 15:26:54 +0200

    feat(core/data): create Objects that handle result of clusters

---
+++ opensilex-core/src/main/java/org/opensilex/core/data/api/ClusterInfoDTO.java
@@ -0,0 +1,76 @@
+package org.opensilex.core.data.api;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.IOException;
+
+/**
+ * @author alexandre
+ */
+public class ClusterInfoDTO {
+
+    private int id;
+    private int minValue = Integer.MAX_VALUE;
+    private int maxValue = Integer.MIN_VALUE;
+
+    public ClusterInfoDTO(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    @JsonProperty("min_value")
+    public int getMinValue() {
+        return this.minValue;
+    }
+
+    @JsonProperty("max_value")
+    public int getMaxValue() {
+        return this.maxValue;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public boolean isInRange(int value) {
+        return value >= this.minValue && value <= this.maxValue;
+    }
+    
+    public boolean updateMinMaxValue(int value) {
+        if (setIfMinValue(value)) {
+            return true;
+        } else return setIfMaxValue(value);
+    }
+
+    public boolean setIfMinValue(int value) {
+        if (!isMinValueIsLessThanValue(value)) {
+            this.minValue = value;
+            if (this.maxValue == Integer.MIN_VALUE) // Handle if there is only one value entered
+                this.maxValue = value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean setIfMaxValue(int value) {
+        if (!isMaxValueIsGreaterThanValue(value)) {
+            this.maxValue = value;
+            if (this.minValue == Integer.MAX_VALUE) // Handle if there is only one value entered
+                this.minValue = value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isMinValueIsLessThanValue(int value) {
+        return this.minValue < value;
+    }
+
+    private boolean isMaxValueIsGreaterThanValue(int value) {
+        return this.maxValue > value;
+    }
+}
---
+++ opensilex-core/src/main/java/org/opensilex/core/data/api/DataClusterReturn.java
@@ -0,0 +1,72 @@
+package org.opensilex.core.data.api;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author alexandre
+ */
+public class DataClusterReturn {
+
+    public String method;
+    public int nbClusters;
+    public List<DataGetDTOCluster> data;
+    public List<ClusterInfoDTO> clusterInfo;
+
+    public DataClusterReturn(String method, int nbCluster) throws IOException {
+        this.method = method;
+        this.nbClusters = nbCluster;
+    }
+    
+    public void setNbClusters(int nbClusters) {
+        this.nbClusters = nbClusters;
+    }
+    
+    public void setData(List<DataGetDTOCluster> datas) {
+        this.clusterInfo = new ArrayList<>();
+        for (int i = 0; i < this.nbClusters; i++) {
+            ClusterInfoDTO cluster = new ClusterInfoDTO(i + 1);
+            this.clusterInfo.add(cluster);
+        }
+        for (DataGetDTOCluster dataCluster : datas) {
+            if (dataCluster.getClusterID() == 0) { // Avoid cluster id n°0
+                dataCluster.setClusterID(this.nbClusters);
+            }
+            int value = (int) dataCluster.getValue();
+            this.clusterInfo.get(dataCluster.getClusterID() - 1).updateMinMaxValue(value);
+        }
+        this.data = datas;
+        orderClusterInfo();
+        updateClustersById();
+    }
+    
+    private void orderClusterInfo() {
+        // Sort by values
+        Collections.sort(this.clusterInfo, (ClusterInfoDTO cluster1, ClusterInfoDTO cluster2) -> {
+            if (cluster1.getMinValue() > cluster2.getMinValue()) {
+                return 1;
+            } else if (cluster1.getMinValue() < cluster2.getMinValue()) {
+                return -1;
+            } else {
+                return 0;
+            }
+        });
+        for (int i = 0; i < this.nbClusters; i++) { // Update cluster id n°
+            this.clusterInfo.get(i).setId(i + 1);
+        }
+    }
+    
+    private void updateClustersById() {
+        for (DataGetDTOCluster dataCluster : this.data) {
+            for (ClusterInfoDTO cluster : this.clusterInfo) {
+                int value = (int) dataCluster.getValue();
+                if (cluster.isInRange(value)) {
+                    dataCluster.setClusterID(cluster.getId());
+                    break;
+                }
+            }
+        }
+    }
+}

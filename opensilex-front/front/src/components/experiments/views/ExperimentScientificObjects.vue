912aa5a3e6343a8379aebefdbce217ff6e993432
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2021-09-14 08:09:10 +0200

    Add select deselect

--- opensilex-front/front/src/components/experiments/views/ExperimentScientificObjects.vue
+++ opensilex-front/front/src/components/experiments/views/ExperimentScientificObjects.vue
@@ -133,7 +133,7 @@
                   >
                 </b-form-checkbox>
               </b-col>
-              <b-col class="ml-2 mt-1"> {{$t('component.common.select-all')}}</b-col>
+              <b-col class="ml-2 mt-1"> {{!selectAll ? $t('component.common.select-all')} : $t('component.common.unselect-all')}}</b-col>
             </b-row>
           </div>
 

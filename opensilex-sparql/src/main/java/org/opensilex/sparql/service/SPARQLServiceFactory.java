ad99a43f9336dd78be0b32b579fd15fa8d98269e
renaud colin <renaud.colin@inra.fr>
2021-10-25 11:45:47 +0200

    Clean SPARQLServiceFactory
    Signed-off-by: renaud colin <renaud.colin@inra.fr>

--- opensilex-sparql/src/main/java/org/opensilex/sparql/service/SPARQLServiceFactory.java
+++ opensilex-sparql/src/main/java/org/opensilex/sparql/service/SPARQLServiceFactory.java
@@ -18,6 +18,7 @@ import org.opensilex.service.ServiceFactory;
 import org.opensilex.sparql.SPARQLConfig;
 import org.opensilex.sparql.SPARQLModule;
 import org.opensilex.sparql.annotations.SPARQLResource;
+import org.opensilex.sparql.deserializer.SPARQLDeserializers;
 import org.opensilex.sparql.deserializer.URIDeserializer;
 import org.opensilex.sparql.extensions.OntologyFileDefinition;
 import org.opensilex.sparql.extensions.SPARQLExtension;
@@ -36,9 +37,9 @@ import org.slf4j.LoggerFactory;
 @ServiceDefaultDefinition(implementation = RDF4JServiceFactory.class)
 public abstract class SPARQLServiceFactory extends ServiceFactory<SPARQLService> {
 
-    private final static Logger LOGGER = LoggerFactory.getLogger(SPARQLServiceFactory.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(SPARQLServiceFactory.class);
 
-    public SPARQLServiceFactory(ServiceConfig config) {
+    protected SPARQLServiceFactory(ServiceConfig config) {
         super(config);
     }
 
@@ -70,7 +71,7 @@ public abstract class SPARQLServiceFactory extends ServiceFactory<SPARQLService>
     public void setup() throws Exception {
         sparqlModule = getOpenSilex().getModuleByClass(SPARQLModule.class);
         baseURI = sparqlModule.getBaseURI();
-        generationPrefixURI = sparqlModule.getGenerationPrefixURI();
+        generationPrefixURI = sparqlModule.getGenerationUri();
     }
 
     @Override
@@ -91,20 +92,21 @@ public abstract class SPARQLServiceFactory extends ServiceFactory<SPARQLService>
                 String resourceNamespace = mapper.getResourceGraphNamespace();
                 String resourcePrefix = mapper.getResourceGraphPrefix();
                 if (resourceNamespace != null && resourcePrefix != null && !resourcePrefix.isEmpty()) {
-                    SPARQLService.addPrefix(sparqlModule.getBaseURIAlias().toString() + mapper.getResourceGraphPrefix(), resourceNamespace + "#");
+                    SPARQLService.addPrefix(sparqlModule.getBaseURIAlias() + mapper.getResourceGraphPrefix(), resourceNamespace + "#");
                 }
             });
 
             SPARQLService.addPrefix(sparqlConfig.baseURIAlias(), sparqlConfig.baseURI());
 
             if (!StringUtils.isBlank(sparqlConfig.generationBaseURI())
-                    && !sparqlModule.getGenerationPrefixURI().equals(sparqlModule.getBaseURI().toString())
+                    && !sparqlModule.getGenerationUri().equals(sparqlModule.getBaseURI())
                     && !sparqlConfig.baseURIAlias().equals(sparqlConfig.generationBaseURIAlias())) {
+
                 SPARQLService.addPrefix(sparqlConfig.generationBaseURIAlias(), sparqlConfig.generationBaseURI());
             }
-            sparqlModule.getCustomPrefixes().forEach((prefix, uri) -> {
-                SPARQLService.addPrefix(prefix, uri.toString());
-            });
+            sparqlModule.getCustomPrefixes().forEach((prefix, uri) ->
+                SPARQLService.addPrefix(prefix, uri.toString())
+            );
 
             for (SPARQLExtension module : getOpenSilex().getModulesImplementingInterface(SPARQLExtension.class)) {
                 for (OntologyFileDefinition ontologyDef : module.getOntologiesFiles()) {
@@ -119,12 +121,13 @@ public abstract class SPARQLServiceFactory extends ServiceFactory<SPARQLService>
         URIDeserializer.setPrefixes(SPARQLService.getPrefixMapping(), sparqlConfig.usePrefixes());
     }
 
+    @Override
     public void shutdown() {
         SPARQLService.clearPrefixes();
         URIDeserializer.clearPrefixes();
     }
 
-    public SPARQLClassObjectMapperIndex getMapperIndex() throws Exception {
+    public SPARQLClassObjectMapperIndex getMapperIndex() {
         return mapperIndex;
     }
 

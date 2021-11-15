3ef0e39790f3c74a324a947a461704c957316140
renaud colin <renaud.colin@inra.fr>
2021-11-15 13:53:37 +0100

    Revert "[OntologyClassView] Fix modal placement and collision with search box : remove sticky class on OntologyClassDetail div"
    This reverts commit fdf94e56d5a94e95d9f96c16f160b895752f672e.

--- opensilex-front/front/src/components/ontology/OntologyClassView.vue
+++ opensilex-front/front/src/components/ontology/OntologyClassView.vue
@@ -33,7 +33,7 @@
       </b-card>
     </div>
     <div class="col-md-6 ">
-      <div>
+      <div class="sticky">
       <opensilex-OntologyClassDetail
         :rdfType="rdfType"
         :selected="selected"

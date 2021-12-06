851689e5203975074d3a403e1e6e9f8d96937f1f
Maximilian <maximilian.hart@inrae.fr>
2021-12-06 17:24:08 +0100

    please work!

--- opensilex-mobile/src/main/java/org/opensilex/mobile/api/FormGetDTO.java
+++ opensilex-mobile/src/main/java/org/opensilex/mobile/api/FormGetDTO.java
@@ -6,18 +6,18 @@ public class FormGetDTO extends FormUpdateDTO{
 
     public static FormGetDTO fromModel(FormModel model) {
         FormGetDTO formGetDTO = new FormGetDTO();
-        /*formGetDTO.setCreationDate(model.getCreationDate().toString());
-        formGetDTO.setUpdateDate(model.getLastUpdateDate().toString());*/
+        formGetDTO.setCreationDate(model.getCreationDate().toString());
+        formGetDTO.setUpdateDate(model.getLastUpdateDate().toString());
         formGetDTO.setUri(model.getUri());
         formGetDTO.setCommitAddress(model.getCommitAddress());
         formGetDTO.setCodeLot(model.getCodeLot());
-        /*formGetDTO.setAvailableChildren(model.getAvailableChildren());
+        formGetDTO.setAvailableChildren(model.getAvailableChildren());
         formGetDTO.setChildren(model.getChildren());
         formGetDTO.setRoot(model.isRoot());
         formGetDTO.setType(model.getType());
         formGetDTO.setOffset(model.getOffset());
         formGetDTO.setSectionUris(model.getSectionUris());
-        formGetDTO.setParents(model.getParents());*/
+        formGetDTO.setParents(model.getParents());
         return formGetDTO;
     }
 }

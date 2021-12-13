038a8d3ddf0565062bffc928d6191b4848b68c2f
renaud colin <renaud.colin@inra.fr>
2021-12-13 11:56:24 +0100

    [OntologyStore] test ancestors computation
    Signed-off-by: renaud colin <renaud.colin@inra.fr>

---
+++ opensilex-sparql/src/main/java/org/opensilex/sparql/ontology/store/OntologyStore.java
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ *                         OntologyStore.java
+ * OpenSILEX - Licence AGPL V3.0 - https://www.gnu.org/licenses/agpl-3.0.en.html
+ * Copyright © INRAE 2021.
+ * Contact: renaud.colin@inrae.fr, anne.tireau@inrae.fr, pascal.neveu@inrae.fr
+ *
+ ******************************************************************************/
+
+package org.opensilex.sparql.ontology.store;
+
+public interface OntologyStore {
+
+}

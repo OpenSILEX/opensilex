de76ef6e0303959370b1adb18084b7872423131c
rcolin <renaud.colin@inrae.fr>
2022-01-20 00:47:46 +0100

    [InfrastructureFacilityModel] Fix error on rebase
    Signed-off-by: rcolin <renaud.colin@inrae.fr>

--- opensilex-core/src/main/java/org/opensilex/core/organisation/dal/InfrastructureFacilityModel.java
+++ opensilex-core/src/main/java/org/opensilex/core/organisation/dal/InfrastructureFacilityModel.java
@@ -10,15 +10,13 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.jena.vocabulary.VCARD4;
 import org.opensilex.core.ontology.Oeso;
 import org.opensilex.sparql.annotations.SPARQLProperty;
 import org.opensilex.sparql.annotations.SPARQLResource;
 import org.opensilex.sparql.model.SPARQLTreeModel;
 
-import java.net.URI;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @SPARQLResource(
         ontology = Oeso.class,

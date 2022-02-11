af57c0801141463d3bb8acfe1d44601243ea4fce
rcolin <renaud.colin@inrae.fr>
2022-02-11 02:47:39 +0100

    OntologyStoreTest skeleton
    Signed-off-by: rcolin <renaud.colin@inrae.fr>

---
+++ opensilex-core/src/test/java/org/opensilex/core/ontology/api/OntologyAPITest.java
@@ -0,0 +1,71 @@
+//******************************************************************************
+//                          OntologyAPITest.java
+// OpenSILEX - Licence AGPL V3.0 - https://www.gnu.org/licenses/agpl-3.0.en.html
+// Copyright © INRAE 2022
+// Contact: renaud.colin@inrae.fr, anne.tireau@inrae.fr, pascal.neveu@inrae.fr
+//******************************************************************************
+
+package org.opensilex.core.ontology.api;
+
+import org.junit.Test;
+import org.opensilex.integration.test.security.AbstractSecurityIntegrationTest;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author rcolin
+ */
+public class OntologyAPITest extends AbstractSecurityIntegrationTest {
+
+
+
+    @Test
+    public void testCreateClass(){
+    }
+
+    @Test
+    public void testGetClass(){
+
+    }
+
+    @Test
+    public void testSearchSubClasses(){
+
+    }
+
+    @Test
+    public void testUpdateClass(){
+
+    }
+
+    @Test
+    public void testDeleteClass(){
+
+    }
+
+    @Test
+    public void testCreateProperty(){
+
+    }
+
+    @Test
+    public void testGetProperty(){
+
+    }
+
+    @Test
+    public void testSearchPropertiesOnDomain(){
+
+    }
+
+    @Test
+    public void testUpdateProperty(){
+
+    }
+
+    @Test
+    public void testDeleteProperty(){
+
+    }
+
+}

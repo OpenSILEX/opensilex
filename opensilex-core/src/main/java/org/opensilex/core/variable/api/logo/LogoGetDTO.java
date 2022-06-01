91a05ded373865902f37bb72a2c52b7e6cf8cc3a
Alexia Leroy <alexia.leroy@inrae.fr>
2022-06-01 15:57:11 +0200

    ajout champ logo d'une variable

---
+++ opensilex-core/src/main/java/org/opensilex/core/variable/api/logo/LogoGetDTO.java
@@ -0,0 +1,18 @@
+package org.opensilex.core.variable.api.logo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.opensilex.core.variable.dal.LogoModel;
+import org.opensilex.sparql.model.SPARQLNamedResourceModel;
+import org.opensilex.sparql.response.ObjectNamedResourceDTO;
+
+import java.net.URI;
+
+public class LogoGetDTO {
+
+    //public LogoGetDTO(LogoModel model) {super(model);}
+
+    public LogoGetDTO() {
+    }
+
+
+}

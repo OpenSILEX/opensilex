a94c8f562fcda29fe6f5dda7614aa8ffa6cf56cf
Alexia Leroy <alexia.leroy@inrae.fr>
2022-06-02 10:25:44 +0200

    ajout filtre ressources

---
+++ opensilex-front/front/src/components/resources/ResourcesSelector.vue
@@ -0,0 +1,18 @@
+<template>
+  <select v-model="selected">
+    <option disabled value="">Choisissez une ressource</option>
+    <option>Instance locale</option>
+    <option>Phenome</option>
+    <option>Vitioeno</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "ResourcesSelector"
+}
+</script>
+
+<style scoped>
+
+</style>

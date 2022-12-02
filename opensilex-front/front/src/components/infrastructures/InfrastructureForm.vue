d8e61cfc820e59d74241b556ae4e8907bc07c49a
maussang <brice.maussang@inrae.fr>
2022-12-02 11:29:53 +0100

    fix traduction

--- opensilex-front/front/src/components/infrastructures/InfrastructureForm.vue
+++ opensilex-front/front/src/components/infrastructures/InfrastructureForm.vue
@@ -202,5 +202,5 @@ fr:
     form-parent-placeholder: Sélectionner l'organisation parente
     infrastructure-already-exists: Une organisation existe déjà avec cette URI
     form-group-label: Groupes
-    form-facilities-labe: Installations environnementales
+    form-facilities-label: Installations environnementales
 </i18n>

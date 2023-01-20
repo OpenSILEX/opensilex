1cfd18196e3c232b832a8e89215b36994ef94e91
maussang <brice.maussang@inrae.fr>
2023-01-20 12:34:21 +0100

    add device facility service

--- opensilex-front/front/src/components/facilities/views/FacilityAssociatedDevices.vue
+++ opensilex-front/front/src/components/facilities/views/FacilityAssociatedDevices.vue
@@ -1,40 +1,17 @@
-
 <template>
-  <div>
-    <opensilex-TextView v-if="isNoVariableFound"
-                        id="no-variable-text"
-        :label="$t('FacilityAssociatedDevices.no-variable')">
-    </opensilex-TextView>
-
-    <GridLayout v-if="isItemsLoaded"
-                class="grid-layout"
-        :layout.sync="layout"
-        :col-num="NB_COL"
-        :is-draggable="true"
-        :is-resizable="false"
-        :is-mirrored="false"
-        :vertical-compact="true"
-        :is-bounded="true"
-        :autoSize="false"
-        :margin="[10, 10]"
-        :use-css-transforms="true">
-
-        <GridItem class="tile"
-                  v-for="item in layout"
-                    :x="item.x"
-                    :y="item.y"
-                    :w="item.w"
-                    :h="item.h"
-                    :i="item.i"
-                    :key="item.i"
-                    :dragIgnoreFrom="'#data-infos, #devices-list, #graphic, #btn-show'">
-          <opensilex-VariableVisualizationTile
-              class="tile-content"
-              v-bind="item.content">
-          </opensilex-VariableVisualizationTile>
-        </GridItem>
-    </GridLayout>
-  </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="facilityDescription">
+            <opensilex-FacilityDescription
+              :selected="selected"
+              :devices="devices"
+              :withActions="true"
+              @onUpdate="refresh"
+            >
+            </opensilex-FacilityDescription>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -42,35 +19,29 @@ import { Component, Ref, Watch } from "vue-property-decorator";
 import Vue from "vue";
 import HttpResponse, { OpenSilexResponse } from "../../../lib/HttpResponse";
 import { OrganizationGetDTO } from "opensilex-core/index";
+import { ExperimentGetListDTO } from "opensilex-core/model/experimentGetListDTO";
+import { DeviceGetDTO } from "opensilex-core/model/deviceGetDTO";
 import {OrganizationsService} from "opensilex-core/api/organizations.service";
+import {ExperimentsService} from "opensilex-core/api/experiments.service";
 import {DevicesService} from "opensilex-core/api/devices.service";
 import OpenSilexVuePlugin from "../../../models/OpenSilexVuePlugin";
+import AssociatedExperimentsList from "../../experiments/AssociatedExperimentsList.vue";
 import {PositionsService} from "opensilex-core/api/positions.service";
-import {VariablesService} from "opensilex-core/api/variables.service";
-import {DataService} from "opensilex-core/api/data.service";
-import {NamedResourceDTOVariableModel} from "opensilex-core/model/namedResourceDTOVariableModel";
-
+import {PositionGetDTO} from "opensilex-core/model/positionGetDTO";
 
 @Component
 export default class FacilityAssociatedDevices extends Vue {
   $opensilex: OpenSilexVuePlugin;
 
-  NB_COL = 4;
-
-  uri: string = null;
   selected: OrganizationGetDTO = null;
-  usedVariables: NamedResourceDTOVariableModel[] = [];
-  layout = [];
+  devices: Array<DeviceGetDTO> = [];
+  uri = null;
 
-  isNoVariableFound: boolean = false;
-  isItemsLoaded: boolean = false;
-
-  /// services
   organizationService: OrganizationsService;
   deviceService: DevicesService;
-  variablesService: VariablesService;
   positionService: PositionsService;
-  dataService: DataService;
+
+  @Ref("infrastructureFacilityForm") readonly infrastructureFacilityForm!: any;
 
   get user() {
     return this.$store.state.user;
@@ -82,112 +53,73 @@ export default class FacilityAssociatedDevices extends Vue {
 
   created() {
     this.uri = decodeURIComponent(this.$route.params.uri);
-    this.organizationService = this.$opensilex.getService<OrganizationsService>(
+    this.organizationService = this.$opensilex.getService(
       "opensilex-core.OrganizationsService"
     );
-    this.variablesService = this.$opensilex.getService<VariablesService>(
-        "opensilex-core.VariablesService"
-    );
-    this.deviceService = this.$opensilex.getService<DevicesService>(
+    this.deviceService = this.$opensilex.getService(
         "opensilex-core.DevicesService"
     );
-    this.positionService = this.$opensilex.getService<PositionsService>(
+    this.positionService = this.$opensilex.getService(
         "opensilex-core.PositionsService"
-    );
-    this.dataService = this.$opensilex.getService<DataService>(
-        "opensilex-core.DataService"
     );
     this.refresh();
   }
 
   refresh() {
     this.organizationService
-        .getFacility(this.uri)
-        .then((http: HttpResponse<OpenSilexResponse<OrganizationGetDTO>>) => {
-            let detailDTO: OrganizationGetDTO = http.response.result;
-            this.selected = detailDTO;
-            this.loadVariables();
-        });
-  }
-
-  /**
-   * Get all variables with this facility as target.
-   */
-  loadVariables() {
-    this.dataService
-        .getUsedVariables(
-            null,
-            [this.uri],
-            null,
-            null)
-        .then((http) => {
-          let resultList = http.response.result;
-          this.usedVariables = [];
-          for (let i in resultList) {
-            let variable = resultList[i];
-            this.usedVariables.push({
-              uri: variable.uri,
-              name: variable.name
-            });
-          }
-
-          if (this.usedVariables.length === 0) {
-            this.isNoVariableFound = true;
-            return;
-          }
-
-          this.loadTiles();
-        });
-  }
-
-  /**
-   * Create add fill tiles according to the previously collected variables.
-   */
-  loadTiles() {
-    let i = 0;
-    for (let v of this.usedVariables) {
-      let x = i % this.NB_COL;
-      let y = ~~(i / this.NB_COL);
-
-      this.layout.push({
-        "x":x, "y":y, "w":1, "h":2, "i":i,
-        "content": { target: this.uri, variableUri: v }
+      .getFacility(this.uri)
+      .then((http: HttpResponse<OpenSilexResponse<OrganizationGetDTO>>) => {
+        let detailDTO: OrganizationGetDTO = http.response.result;
+        this.selected = detailDTO;
+        this.loadDevices();
       });
-      ++i;
-    }
-    this.isItemsLoaded = true;
+  }
+
+  loadDevices() {
+    this.devices = [];
+    this.deviceService.getDevicesByFacility(this.uri)
+        .then(
+            (
+                http: HttpResponse<OpenSilexResponse<Array<DeviceGetDTO>>>
+            ) => {
+              if (http && http.response) {
+                this.devices = http.response.result;
+                this.loadPositionsHistory();
+              }
+            }
+        )
+        .catch(this.$opensilex.errorHandler);
+  }
+
+  loadPositionsHistory() {
+    this.devices.forEach(device => {
+        this.positionService.searchPositionHistory(device.uri)
+            .then(
+                (
+                    http: HttpResponse<OpenSilexResponse<Array<PositionGetDTO>>>
+                ) => {
+                  if (http && http.response) {
+                    console.log(http.response);
+                  }
+                }
+            )
+      }
+    )
+
   }
 
 }
 </script>
 
 <style scoped lang="scss">
-
-.tile {
-  display: table;
+.facilityDescription {
+  margin-top: -25px;
 }
 
-.tile-content {
-  height: 100%;
-  width: 100%;
+@media (max-width: 769px) {
+  .facilityDescription {
+    margin-top: 0;
+  }
 }
-
-#no-data-text {
-  margin: 10px;
-  font-size: 1em;
-  font-weight: bold;
-}
-
 </style>
 
-<i18n>
-en:
-  FacilityAssociatedDevices:
-    no-data: No data found
-    no-variable: No environnemental variable found
-
-fr:
-  FacilityAssociatedDevices:
-    no-data: Aucunes données trouvées
-    no-variable: Aucunes variables environnementales trouvées
-</i18n>

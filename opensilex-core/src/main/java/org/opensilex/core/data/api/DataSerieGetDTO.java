cefb1e16b1a2eee3270d685f396068fb007ee922
maussang <brice.maussang@inrae.fr>
2023-02-21 15:19:00 +0100

    add data service

--- opensilex-core/src/main/java/org/opensilex/core/data/api/DataSerieGetDTO.java
+++ opensilex-core/src/main/java/org/opensilex/core/data/api/DataSerieGetDTO.java
@@ -6,49 +6,73 @@
 //******************************************************************************
 package org.opensilex.core.data.api;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.swagger.annotations.ApiModelProperty;
+import org.opensilex.core.data.dal.DataDAO;
+import org.opensilex.core.data.dal.DataModel;
 import org.opensilex.core.data.dal.DataProvenanceModel;
+import org.opensilex.core.variable.api.VariableGetDTO;
+import org.opensilex.core.variable.dal.VariableModel;
+import org.opensilex.server.rest.validation.DateFormat;
+import org.opensilex.server.rest.validation.ValidURI;
+import org.opensilex.utils.ListWithPagination;
 
-import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.net.URI;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 /**
- * This class defines a serie of data as a list of data associated with a provenance
  *
  * @author brice maussang
  */
 @JsonPropertyOrder({
-        "provenance", "data"
+        "variable", "provenance", "data"
 })
 public class DataSerieGetDTO {
 
+    @JsonProperty("variable")
+    private VariableGetDTO variable;
+
     @JsonProperty("provenance")
-    private DataSimpleProvenanceGetDTO provenance;
+    private DataProvenanceModel provenance;
 
-    @Valid
     @JsonProperty("data")
-    private List<DataSimpleGetDTO> data;
+    private List<DataGetDTO> data;
 
 
-    public DataSerieGetDTO(DataSimpleProvenanceGetDTO provenance, List<DataSimpleGetDTO> data) {
+    public DataSerieGetDTO(VariableGetDTO variable, DataProvenanceModel provenance, List<DataGetDTO> data) {
+        this.variable = variable;
         this.provenance = provenance;
         this.data = data;
     }
 
-    public DataSimpleProvenanceGetDTO getProvenance() {
+    public VariableGetDTO getVariable() {
+        return variable;
+    }
+
+    public void setVariable(VariableGetDTO variable) {
+        this.variable = variable;
+    }
+
+    public DataProvenanceModel getProvenance() {
         return provenance;
     }
 
-    public void setProvenance(DataSimpleProvenanceGetDTO provenance) {
+    public void setProvenance(DataProvenanceModel provenance) {
         this.provenance = provenance;
     }
 
-    public List<DataSimpleGetDTO> getData() {
+    public List<DataGetDTO> getData() {
         return data;
     }
 
-    public void setData(List<DataSimpleGetDTO> data) {
+    public void setData(List<DataGetDTO> data) {
         this.data = data;
     }
 

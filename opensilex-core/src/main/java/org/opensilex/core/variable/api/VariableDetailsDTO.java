7613aab11d9acf951e0060e4a5994d3332831fa9
maussang <brice.maussang@inrae.fr>
2023-03-06 15:17:29 +0100

    UI changes update variable details dto

--- opensilex-core/src/main/java/org/opensilex/core/variable/api/VariableDetailsDTO.java
+++ opensilex-core/src/main/java/org/opensilex/core/variable/api/VariableDetailsDTO.java
@@ -28,6 +28,7 @@ import org.opensilex.core.variable.api.entity.EntityGetDTO;
 import org.opensilex.core.variable.api.entityOfInterest.InterestEntityGetDTO;
 import org.opensilex.core.variable.api.method.MethodGetDTO;
 import org.opensilex.core.variable.api.characteristic.CharacteristicGetDTO;
+import org.opensilex.core.variable.api.unit.UnitDetailsDTO;
 import org.opensilex.core.variable.api.unit.UnitGetDTO;
 import org.opensilex.core.variable.dal.*;
 import org.opensilex.security.user.api.UserGetDTO;
@@ -78,7 +79,7 @@ public class VariableDetailsDTO extends BaseVariableDetailsDTO<VariableModel> {
     private MethodGetDTO method;
 
     @JsonProperty("unit")
-    private UnitGetDTO unit;
+    private UnitDetailsDTO unit;
 
     @JsonProperty("trait")
     private URI trait;
@@ -120,7 +121,7 @@ public class VariableDetailsDTO extends BaseVariableDetailsDTO<VariableModel> {
         this.method = new MethodGetDTO(method);
 
         UnitModel unit = model.getUnit();
-        this.unit = new UnitGetDTO(unit);
+        this.unit = new UnitDetailsDTO(unit);
 
         this.alternativeName = model.getAlternativeName();
 
@@ -236,11 +237,11 @@ public class VariableDetailsDTO extends BaseVariableDetailsDTO<VariableModel> {
         this.method = method;
     }
 
-    public UnitGetDTO getUnit() {
+    public UnitDetailsDTO getUnit() {
         return unit;
     }
 
-    public void setUnit(UnitGetDTO unit) {
+    public void setUnit(UnitDetailsDTO unit) {
         this.unit = unit;
     }
 

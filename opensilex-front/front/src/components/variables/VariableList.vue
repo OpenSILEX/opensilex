3267dfca90efeb1bf12774c6dadcfcdd5f32881a
RIGOLLE Valentin <valentin.rigolle@inrae.fr>
2023-04-19 12:11:53 +0200

    [Shared Variables] Fix credentials for shared variable copy/import

--- opensilex-front/front/src/components/variables/VariableList.vue
+++ opensilex-front/front/src/components/variables/VariableList.vue
@@ -214,7 +214,7 @@
                         <b-dropdown-item-button @click="classicExportVariables()">{{$t('VariableList.export-variables')}}</b-dropdown-item-button>
                         <b-dropdown-item-button @click="detailsExportVariables()">{{$t('VariableList.export-variables-details')}}</b-dropdown-item-button>
                         <b-dropdown-item-button
-                            v-if="filter.sharedResourceInstance"
+                            v-if="filter.sharedResourceInstance && user.hasCredential(credentials.CREDENTIAL_VARIABLE_MODIFICATION_ID)"
                             @click="importVariablesOnLocal()"
                         >{{$t('VariableList.import-variables-from-shared-resources')}}</b-dropdown-item-button>
 
@@ -302,8 +302,8 @@
                     v-if="user.hasCredential(credentials.CREDENTIAL_VARIABLE_MODIFICATION_ID) && !noActions && loadGroupVariablesForm"
                     ref="groupVariablesForm"
                     modalSize="lg"
-                    @onCreate="refresh($event)"
-                    @onUpdate="refresh($event)"
+                    @onCreate="refresh()"
+                    @onUpdate="refresh()"
                     component="opensilex-GroupVariablesForm"
                     createTitle="GroupVariablesForm.add"
                     editTitle="GroupVariablesForm.edit"
@@ -337,12 +337,13 @@ import OpenSilexVuePlugin from "../../models/OpenSilexVuePlugin";
 import {CopyResourceDTO} from "opensilex-core/model/copyResourceDTO";
 import TableAsyncView from "../common/views/TableAsyncView.vue";
 import {VariableGetDTO} from "opensilex-core/model/variableGetDTO";
+import {OpenSilexStore} from "../../models/Store";
 
 @Component
 export default class VariableList extends Vue {
     $opensilex: OpenSilexVuePlugin;
     $service: VariablesService;
-    $store: any;
+    $store: OpenSilexStore;
     $route: any;
     $i18n: any;
 

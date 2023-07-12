1dbeb2f7ee0ba19f5a6707aa8050468a1c6ee859
maussang <brice.maussang@inrae.fr>
2023-07-12 14:22:47 +0200

    test composant recherche

---
+++ opensilex-front/front/src/components/variables/AgroportalSearch.vue
@@ -0,0 +1,39 @@
+<template>
+  <b-input
+      type="text"
+      v-model="searchBar"
+      placeholder="search"
+      v-on:change="foo"/>
+</template>
+
+
+<script lang="ts">
+
+import {Component, Prop, Ref} from "vue-property-decorator";
+import Vue from 'vue';
+
+@Component
+export default class AgroportalSearch extends Vue {
+
+  searchBar;
+
+  created() {
+    this.searchBar = "";
+  }
+
+  foo() {
+    console.log(this.searchBar);
+    this.$emit('change', this.searchBar);
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
+
+
+<i18n>
+
+</i18n>

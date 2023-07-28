010351bdeca35363f9ab1ba6ddf68e0c34c71096
Valentin Rigolle <valentin.rigolle@inrae.fr>
2023-07-28 15:32:42 +0200

    Premier test GraphQL en tant que service

---
+++ opensilex-core/src/main/java/org/opensilex/core/graphql/GraphQLAPI.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ *                         GraphQLAPI.java
+ * OpenSILEX - Licence AGPL V3.0 - https://www.gnu.org/licenses/agpl-3.0.en.html
+ * Copyright © INRAE 2023.
+ * Last Modification: 28/07/2023
+ * Contact: valentin.rigolle@inrae.fr, anne.tireau@inrae.fr, pascal.neveu@inrae.fr
+ *
+ ******************************************************************************/
+package org.opensilex.core.graphql;
+
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+import io.swagger.annotations.Api;
+import org.opensilex.security.authentication.ApiProtected;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author rigolle
+ */
+@Api("GraphQL")
+@Path("core/graphql")
+@Produces(MediaType.APPLICATION_JSON)
+public class GraphQLAPI {
+    @GET
+    @ApiProtected
+    public Response executeGraphQLQuery(
+            @QueryParam("query") String query
+    ) {
+        String schema = "type Query{hello: String}";
+
+        SchemaParser schemaParser = new SchemaParser();
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+        RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
+                .type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
+                .build();
+
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+
+        GraphQL graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+        ExecutionResult executionResult = graphQL.execute(query);
+
+        return Response.ok(executionResult).build();
+    }
+}

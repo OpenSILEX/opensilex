ff24632c4e0548434516b7c78fae54074d978a36
Valentin Rigolle <valentin.rigolle@inrae.fr>
2023-07-31 14:12:12 +0200

    Test SPARQL (expe) + fichier graphqls

--- opensilex-core/src/main/java/org/opensilex/core/graphql/GraphQLAPI.java
+++ opensilex-core/src/main/java/org/opensilex/core/graphql/GraphQLAPI.java
@@ -8,6 +8,7 @@
  ******************************************************************************/
 package org.opensilex.core.graphql;
 
+import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
@@ -17,14 +18,17 @@ import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
 import io.swagger.annotations.Api;
+import org.opensilex.core.experiment.dal.ExperimentModel;
 import org.opensilex.security.authentication.ApiProtected;
+import org.opensilex.sparql.service.SPARQLService;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.inject.Inject;
+import javax.validation.Valid;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.InputStream;
+
 
 /**
  * @author rigolle
@@ -33,26 +37,70 @@ import javax.ws.rs.core.Response;
 @Path("core/graphql")
 @Produces(MediaType.APPLICATION_JSON)
 public class GraphQLAPI {
+    @Inject
+    SPARQLService sparql;
+
+    //@todo brancher à une ressource RDF
+    //@todo brancher à une ressource mongo
+    //@todo injection de dépendance dans les fetchers ?
     @GET
     @ApiProtected
-    public Response executeGraphQLQuery(
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getGraphQL(
             @QueryParam("query") String query
     ) {
-        String schema = "type Query{hello: String}";
+        ExecutionInput input = ExecutionInput.newExecutionInput()
+                .query(query)
+                .build();
 
+        return Response
+                .ok(execute(input).toSpecification())
+                .build();
+    }
+
+    @POST
+    @ApiProtected
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response postGraphQL(
+            @Valid GraphQLPostDTO postDTO
+    ) {
+        ExecutionInput.Builder inputBuilder = ExecutionInput.newExecutionInput()
+                .query(postDTO.getQuery());
+
+        if (postDTO.getOperationName() != null) {
+            inputBuilder.operationName(postDTO.getOperationName());
+        }
+
+        if (postDTO.getVariables() != null) {
+            inputBuilder.variables(postDTO.getVariables());
+        }
+
+        ExecutionInput input = inputBuilder.build();
+
+        return Response
+                .ok(execute(input).toSpecification())
+                .build();
+    }
+
+    private ExecutionResult execute(ExecutionInput input) {
+        // Définition du schéma
+        InputStream schemaFile = getClass().getClassLoader().getResourceAsStream("graphql/schema.graphqls");
         SchemaParser schemaParser = new SchemaParser();
-        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schemaFile);
 
+        // Chercher en bdd
         RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
-                .type("Query", builder -> builder.dataFetcher("hello", new StaticDataFetcher("world")))
+                .type("Query", typeWiring -> typeWiring
+                        .dataFetcher("hello", new StaticDataFetcher("world"))
+                        .dataFetcher("experiments", environment -> sparql.search(ExperimentModel.class, "en"))
+                )
                 .build();
 
         SchemaGenerator schemaGenerator = new SchemaGenerator();
         GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
 
         GraphQL graphQL = GraphQL.newGraphQL(graphQLSchema).build();
-        ExecutionResult executionResult = graphQL.execute(query);
-
-        return Response.ok(executionResult).build();
+        return graphQL.execute(input);
     }
 }
---
+++ opensilex-core/src/main/java/org/opensilex/core/graphql/GraphQLPostDTO.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ *                         GraphQLPostDTO.java
+ * OpenSILEX - Licence AGPL V3.0 - https://www.gnu.org/licenses/agpl-3.0.en.html
+ * Copyright © INRAE 2023.
+ * Last Modification: 31/07/2023
+ * Contact: valentin.rigolle@inrae.fr, anne.tireau@inrae.fr, pascal.neveu@inrae.fr
+ *
+ ******************************************************************************/
+package org.opensilex.core.graphql;
+
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
+/**
+ * @author rigolle
+ */
+public class GraphQLPostDTO {
+    @NotNull
+    private String query;
+    private String operationName;
+    private Map<String, Object> variables;
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getOperationName() {
+        return operationName;
+    }
+
+    public void setOperationName(String operationName) {
+        this.operationName = operationName;
+    }
+
+    public Map<String, Object> getVariables() {
+        return variables;
+    }
+
+    public void setVariables(Map<String, Object> variables) {
+        this.variables = variables;
+    }
+}

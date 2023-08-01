b575e1e7eb48e1e2e4ec019cd40613009d1115be
Valentin Rigolle <valentin.rigolle@inrae.fr>
2023-08-01 11:05:21 +0200

    Test construction schéma par reflexivité

--- opensilex-core/src/main/java/org/opensilex/core/graphql/GraphQLAPI.java
+++ opensilex-core/src/main/java/org/opensilex/core/graphql/GraphQLAPI.java
@@ -11,6 +11,10 @@ package org.opensilex.core.graphql;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
 import graphql.GraphQL;
+import graphql.Scalars;
+import graphql.language.*;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLObjectType;
 import graphql.schema.GraphQLSchema;
 import graphql.schema.StaticDataFetcher;
 import graphql.schema.idl.RuntimeWiring;
@@ -19,6 +23,7 @@ import graphql.schema.idl.SchemaParser;
 import graphql.schema.idl.TypeDefinitionRegistry;
 import io.swagger.annotations.Api;
 import org.bson.Document;
+import org.opensilex.OpenSilex;
 import org.opensilex.core.device.dal.DeviceAttributeModel;
 import org.opensilex.core.device.dal.DeviceDAO;
 import org.opensilex.core.device.dal.DeviceModel;
@@ -27,6 +32,9 @@ import org.opensilex.core.provenance.dal.ProvenanceDAO;
 import org.opensilex.core.provenance.dal.ProvenanceModel;
 import org.opensilex.nosql.mongodb.MongoDBService;
 import org.opensilex.security.authentication.ApiProtected;
+import org.opensilex.sparql.annotations.SPARQLResource;
+import org.opensilex.sparql.mapping.SPARQLClassAnalyzer;
+import org.opensilex.sparql.model.SPARQLResourceModel;
 import org.opensilex.sparql.service.SPARQLService;
 
 import javax.inject.Inject;
@@ -35,6 +43,12 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.net.URI;
+import java.util.Set;
+
+import static graphql.Scalars.GraphQLString;
 
 
 /**
@@ -44,6 +58,10 @@ import java.io.InputStream;
 @Path("core/graphql")
 @Produces(MediaType.APPLICATION_JSON)
 public class GraphQLAPI {
+
+    @Inject
+    OpenSilex openSilex;
+
     @Inject
     SPARQLService sparql;
 
@@ -94,7 +112,7 @@ public class GraphQLAPI {
                 .build();
     }
 
-    private ExecutionResult execute(ExecutionInput input) {
+    private GraphQLSchema exampleSchema() {
         // Définition du schéma
         InputStream schemaFile = getClass().getClassLoader().getResourceAsStream("graphql/schema.graphqls");
         SchemaParser schemaParser = new SchemaParser();
@@ -114,9 +132,88 @@ public class GraphQLAPI {
                 .build();
 
         SchemaGenerator schemaGenerator = new SchemaGenerator();
-        GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+        return schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+    }
 
-        GraphQL graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+    private static final TypeName STRING_TYPE = new TypeName("String");
+    private static final NonNullType REQ_STRING_TYPE = new NonNullType(new TypeName("String"));
+
+    private GraphQLSchema computeSchema() {
+        TypeDefinitionRegistry registry = new TypeDefinitionRegistry();
+        RuntimeWiring.Builder wiring = RuntimeWiring.newRuntimeWiring();
+
+        ObjectTypeDefinition.Builder queryTypeBuilder = ObjectTypeDefinition.newObjectTypeDefinition()
+                .name("Query");
+        SchemaDefinition schemaDefinition = SchemaDefinition.newSchemaDefinition()
+                .operationTypeDefinition(OperationTypeDefinition.newOperationTypeDefinition()
+                        .name("Query")
+                        .typeName(new TypeName("Query"))
+                        .build())
+                .build();
+        registry.add(schemaDefinition);
+
+        try {
+            for (Class clazz : openSilex.getReflections().getTypesAnnotatedWith(SPARQLResource.class, true)) {
+                SPARQLResource resourceAnnotation = (SPARQLResource) clazz.getDeclaredAnnotation(SPARQLResource.class);
+                if (resourceAnnotation == null) {
+                    // Skip classes with no annotation
+                    continue;
+                }
+                String typeName = clazz.getSimpleName();
+
+                // Model type def
+                ObjectTypeDefinition.Builder objectTypeBuilder = ObjectTypeDefinition.newObjectTypeDefinition()
+                        .name(typeName);
+                SPARQLClassAnalyzer analyzer = sparql.getForClass(clazz).getClassAnalizer();
+                for (Field f : analyzer.getDataPropertyFields()) {
+                    objectTypeBuilder
+                            .fieldDefinition(FieldDefinition.newFieldDefinition()
+                                    .name(f.getName())
+                                    .type(STRING_TYPE)
+                                    .build()
+                            );
+                }
+                objectTypeBuilder.fieldDefinition(FieldDefinition.newFieldDefinition()
+                        .name(analyzer.getURIFieldName())
+                        .type(REQ_STRING_TYPE)
+                        .build());
+                registry.add(objectTypeBuilder.build());
+
+                // Query
+                String simpleQueryName = typeName;
+                String listQueryName = simpleQueryName + "List";
+
+                // Definition
+                queryTypeBuilder
+                        .fieldDefinition(FieldDefinition.newFieldDefinition()
+                                .name(listQueryName)
+                                .type(new ListType(new TypeName(typeName)))
+                                .build())
+                        .fieldDefinition(FieldDefinition.newFieldDefinition()
+                                .name(simpleQueryName)
+                                .type(new TypeName(typeName))
+                                .inputValueDefinition(InputValueDefinition.newInputValueDefinition()
+                                        .name(analyzer.getURIFieldName())
+                                        .type(REQ_STRING_TYPE)
+                                        .build())
+                                .build());
+
+                // Wiring
+                wiring.type("Query", typeWiring -> typeWiring.dataFetcher(listQueryName, environment -> sparql.search(clazz, "en")));
+                wiring.type("Query", typeWiring -> typeWiring.dataFetcher(simpleQueryName, environment -> sparql.getByURI(clazz, URI.create(environment.getArgument(analyzer.getURIFieldName())), "en")));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        registry.add(queryTypeBuilder.build());
+
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        return schemaGenerator.makeExecutableSchema(registry, wiring.build());
+    }
+
+    private ExecutionResult execute(ExecutionInput input) {
+        GraphQL graphQL = GraphQL.newGraphQL(computeSchema()).build();
         return graphQL.execute(input);
     }
 }

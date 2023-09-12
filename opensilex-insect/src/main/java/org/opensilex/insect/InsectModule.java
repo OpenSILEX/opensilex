ea9483a7ff54208bf38c6320fd638772f1d7b4ed
bernhard gschloessl <bernhard.gschloessl@inrae.fr>
2023-09-12 14:38:19 +0200

    OESO-INSECT update2 java classes

---
+++ opensilex-insect/src/main/java/org/opensilex/insect/InsectModule.java
@@ -0,0 +1,89 @@
+//******************************************************************************
+//                           InsectModule.java
+// OpenSILEX
+// Copyright © INRA 2023
+// Creation date: 12 sep. 2023
+// Contact: bernhard.gschloessl@inrae.fr, catherine.roussey@inrae.fr, anne.tireau@inrae.fr, pascal.neveu@inrae.fr
+// ******************************************************************************
+package org.opensilex.insect;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.apache.jena.riot.Lang;
+
+import org.opensilex.OpenSilex;
+import org.opensilex.insect.ontology.OesoInsect;
+import org.opensilex.sparql.rdf4j.RDF4JConfig;
+import org.opensilex.OpenSilexModule;
+import org.opensilex.OpenSilexModuleNotFoundException;
+import org.opensilex.core.CoreModule;
+import org.opensilex.core.germplasm.dal.GermplasmModel;
+import org.opensilex.nosql.mongodb.MongoDBConfig;
+import org.opensilex.server.extensions.APIExtension;
+import org.opensilex.sparql.SPARQLConfig;
+import org.opensilex.sparql.SPARQLModule;
+import org.opensilex.sparql.exceptions.SPARQLException;
+import org.opensilex.sparql.extensions.OntologyFileDefinition;
+import org.opensilex.sparql.extensions.SPARQLExtension;
+import org.opensilex.sparql.service.SPARQLService;
+import org.opensilex.sparql.service.SPARQLServiceFactory;
+import org.opensilex.utils.ClassUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Phis opensilex module implementation
+ */
+public class PhisWsModule extends OpenSilexModule implements APIExtension, SPARQLExtension {
+    
+    private final static Logger LOGGER = LoggerFactory.getLogger(PhisWsModule.class);
+
+    @Override
+    public List<OntologyFileDefinition> getOntologiesFiles() throws Exception {
+        SPARQLConfig sparqlConfig = getOpenSilex().getModuleConfig(SPARQLModule.class, SPARQLConfig.class);
+        List<OntologyFileDefinition> list = SPARQLExtension.super.getOntologiesFiles();
+        list.add(new OntologyFileDefinition(
+                OesoExt.NS,
+                "ontologies/oeso-ext.owl",
+                Lang.RDFXML,
+                OesoExt.PREFIX
+        ));
+        return list;
+    }
+    
+    @Override
+    public void install(boolean reset) throws Exception {
+        LOGGER.info("Insert agrovoc species");
+        insertDefaultSpecies();
+    }
+
+    private void insertDefaultSpecies() throws URISyntaxException, OpenSilexModuleNotFoundException, IOException, SPARQLException {
+        try {
+
+            SPARQLServiceFactory factory = getOpenSilex().getServiceInstance(SPARQLService.DEFAULT_SPARQL_SERVICE, SPARQLServiceFactory.class);
+            SPARQLService sparql = factory.provide();
+
+            SPARQLConfig sparqlConfig = getOpenSilex().getModuleConfig(SPARQLModule.class, SPARQLConfig.class);
+            String graph = sparql.getDefaultGraphURI(GermplasmModel.class).toString();
+            OntologyFileDefinition ontologyDef = new OntologyFileDefinition(
+                    graph,
+                    "ontologies/species.ttl",
+                    Lang.TTL,
+                    null
+            );
+
+            InputStream ontologyStream = new FileInputStream(ClassUtils.getFileFromClassArtifact(getClass(), ontologyDef.getFilePath()));
+            sparql.loadOntology(ontologyDef.getUri(), ontologyStream, ontologyDef.getFileType());
+            ontologyStream.close();
+        
+        } catch (Exception ex) {
+            LOGGER.error("Error while insering default species", ex);
+        } 
+        
+    }
+}

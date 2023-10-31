17654906272f65e7ec55aa406f2404efc4f52b41
Valentin Rigolle <valentin.rigolle@inrae.fr>
2023-10-31 15:37:10 +0100

    Service get resource graph map

--- opensilex-graphql/src/main/java/org/opensilex/graphql/staple/api/StapleAPI.java
+++ opensilex-graphql/src/main/java/org/opensilex/graphql/staple/api/StapleAPI.java
@@ -13,7 +13,14 @@ import io.swagger.annotations.ApiOperation;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFWriter;
+import org.opensilex.OpenSilex;
 import org.opensilex.graphql.staple.StapleApiUtils;
+import org.opensilex.server.response.SingleObjectResponse;
+import org.opensilex.sparql.annotations.SPARQLResource;
+import org.opensilex.sparql.mapping.SPARQLClassObjectMapper;
+import org.opensilex.sparql.mapping.SPARQLClassObjectMapperIndex;
+import org.opensilex.sparql.model.SPARQLResourceModel;
+import org.opensilex.sparql.service.SPARQLService;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -22,6 +29,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.ByteArrayOutputStream;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Services useful only for the Staple GraphQL API
@@ -32,6 +42,12 @@ import java.io.ByteArrayOutputStream;
 @Path(StapleAPI.PATH)
 public class StapleAPI {
     public static final String PATH = "/staple";
+
+    @Inject
+    private OpenSilex openSilex;
+
+    @Inject
+    private SPARQLService sparql;
 
     @Inject
     private StapleApiUtils stapleApiUtils;
@@ -52,5 +68,23 @@ public class StapleAPI {
                 .output(baos);
 
         return Response.ok(baos.toByteArray(), new MediaType("application", "x-turtle")).build();
+    }
+    @GET
+    @Path("resource_graph")
+    @ApiOperation("Get all graphs associated with resources")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getResourceGraphs() throws Exception {
+        Map<URI, URI> result = new HashMap<>();
+        SPARQLClassObjectMapperIndex index = sparql.getMapperIndex();
+        for (Class<?> resourceClass : openSilex.getAnnotatedClasses(SPARQLResource.class)) {
+            if (!(SPARQLResourceModel.class.isAssignableFrom(resourceClass))) {
+                break;
+            }
+            SPARQLClassObjectMapper<?> mapper = index.getForClass(resourceClass);
+            if (mapper.getDefaultGraphURI() != null) {
+                result.put(URI.create(mapper.getRDFType().getURI()), mapper.getDefaultGraphURI());
+            }
+        }
+        return new SingleObjectResponse<>(result).getResponse();
     }
 }

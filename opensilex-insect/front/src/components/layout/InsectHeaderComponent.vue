7a793f63ed88226e677cbced6d89441ccf60d5aa
bernhard gschloessl <bernhard.gschloessl@inrae.fr>
2023-12-12 17:00:40 +0100

    update bernhardInsectes add component layout specific for insects

---
+++ opensilex-insect/front/src/components/layout/InsectHeaderComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <opensilex-DefaultHeaderComponent>
+    <template v-slot:headerLogo>
+        <img
+            v-bind:src="$opensilex.getResourceURI('images/insectPlantBiosLogo.png')"
+            class="header-brand-img"
+            alt="lavalite"
+        />
+    </template>
+  </opensilex-DefaultHeaderComponent>
+  
+</template>
+
+<script lang="ts">
+import { Component, Prop } from "vue-property-decorator";
+import Vue from "vue";
+
+@Component
+export default class InsectHeaderComponent extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+</style>
+
+<i18n>    
+</i18n>

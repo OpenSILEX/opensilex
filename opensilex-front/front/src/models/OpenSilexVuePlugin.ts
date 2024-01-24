b9d16bdc7abad441f23b6277adc1a7bd04a4a10b
Maximilian <maximilian.hart@inrae.fr>
2024-01-24 11:46:11 +0100

    An other of valentins recommendations

--- opensilex-front/front/src/models/OpenSilexVuePlugin.ts
+++ opensilex-front/front/src/models/OpenSilexVuePlugin.ts
@@ -31,6 +31,8 @@ import {OntologyService, VariableDatatypeDTO, VariablesService} from 'opensilex-
 import DateTimeFormatter from "./DateTimeFormatter";
 import NumberFormatter from "./NumberFormatter";
 import {BvToastOptions} from "bootstrap-vue/src/components/toast";
+import HttpResponse, {OpenSilexResponse} from "../lib/HttpResponse";
+import {NamedResourceDTO} from "opensilex-core/model/namedResourceDTO";
 
 declare var $cookies: VueCookies;
 
@@ -141,13 +143,12 @@ export default class OpenSilexVuePlugin {
      * @param context , uri of the containg experiment, null if no experiment
      * @param objectsPath , the pre-calculated uri to path map to search in
      */
-    getTargetPath(uri: string, context: string, objectsPath: {[keys: string] : string}) : string {
-        let defaultOsPath: string = objectsPath[uri];
-        if(! defaultOsPath){
+    getTargetPath(uri: string, context: string, objectPath : string) : string {
+        if(! objectPath){
             return "";
         }
 
-        let osPath = defaultOsPath.replace(':uri', encodeURIComponent(uri))
+        let osPath = objectPath.replace(':uri', encodeURIComponent(uri))
 
         // pass encoded experiment inside OS path URL
         if(context && context.length > 0){
@@ -155,6 +156,22 @@ export default class OpenSilexVuePlugin {
         }else{ // no experiment passed
             return osPath.replace(':experiment', "");
         }
+    }
+
+    /**
+     * Creates strings of format 'name (type)', places them in a uri to result map with vue.set
+     *
+     * @param objectsToLoad , the uris of ontology objects that we want to create labels for
+     * @param context , experimental context
+     */
+    loadOntologyLabelsWithType(objectsToLoad: Array<string>, context: string, uriResultMap : {[x: string]:string}, ontologyService: OntologyService):  Promise<void | HttpResponse<OpenSilexResponse<NamedResourceDTO[]>>>{
+        return ontologyService
+            .getURILabelsList(objectsToLoad, context, true)
+            .then((httpObj) => {
+                for (let obj of httpObj.response.result) {
+                    Vue.set(uriResultMap, obj.uri, obj.name + " (" + obj.rdf_type_name + ")");
+                }
+            });
     }
 
     // get front ressources depending to a specific module theme

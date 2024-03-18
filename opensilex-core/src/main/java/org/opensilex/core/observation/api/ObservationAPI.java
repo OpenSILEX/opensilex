20462d437bacc1897592e15a826ae623bbc531b5
Valentin Rigolle <valentin.rigolle@inrae.fr>
2024-03-18 10:11:15 +0100

    observation v2 prototype branch, initial commit

---
+++ opensilex-core/src/main/java/org/opensilex/core/observation/api/ObservationAPI.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ *                         ObservationAPI.java
+ * OpenSILEX - Licence AGPL V3.0 - https://www.gnu.org/licenses/agpl-3.0.en.html
+ * Copyright © INRAE 2024.
+ * Last Modification: 18/03/2024
+ * Contact: valentin.rigolle@inrae.fr, anne.tireau@inrae.fr, pascal.neveu@inrae.fr
+ *
+ ******************************************************************************/
+package org.opensilex.core.observation.api;
+
+/**
+ * Prototype !
+ *
+ * @author Valentin Rigolle
+ */
+public class ObservationAPI {
+
+}

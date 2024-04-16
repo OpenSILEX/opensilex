51467920a6f4a8d43156ded0e614cadd3d655523
Gabriel Besombes <gabriel.besombes@inrae.fr>
2024-04-16 09:21:32 +0200

    Switch to BrAPI test server

--- opensilex-olga/src/main/java/org/opensilex/olga/api/GermplasmAPI.java
+++ opensilex-olga/src/main/java/org/opensilex/olga/api/GermplasmAPI.java
@@ -1,38 +1,25 @@
 package org.opensilex.olga.api;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.reflect.TypeToken;
 import io.swagger.annotations.*;
 import org.brapi.client.v2.BrAPIClient;
-import org.brapi.client.v2.JSON;
 import org.brapi.client.v2.auth.Authentication;
 import org.brapi.client.v2.auth.OAuth;
-import org.brapi.client.v2.model.queryParams.germplasm.GermplasmQueryParams;
 import org.brapi.client.v2.modules.germplasm.GermplasmApi;
-import org.brapi.v2.model.germ.BrAPIGermplasm;
-import org.brapi.v2.model.germ.response.BrAPIGermplasmListResponse;
-import org.brapi.v2.model.germ.response.BrAPIGermplasmListResponseResult;
+import org.brapi.v2.model.germ.response.BrAPIGermplasmSingleResponse;
 import org.opensilex.fs.service.FileStorageService;
 import org.opensilex.nosql.mongodb.MongoDBService;
 import org.opensilex.olga.OlgaConfig;
 import org.opensilex.olga.OlgaModule;
-import org.opensilex.olga.model.GermplasmDTO;
 import org.opensilex.security.account.dal.AccountModel;
 import org.opensilex.security.authentication.ApiCredentialGroup;
 import org.opensilex.security.authentication.ApiProtected;
 import org.opensilex.security.authentication.injection.CurrentUser;
-import org.opensilex.server.response.SingleObjectResponse;
 import org.opensilex.sparql.service.SPARQLService;
 
 import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import java.io.File;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 
 /**
  * @author Gabriel Besombes
@@ -50,6 +37,9 @@ public class GermplasmAPI {
     public static final String CREDENTIAL_OLGA_GERMPLASM_GROUP_LABEL_KEY = "credential-groups.olga-germplasm";
     public static final int DEFAULT_TIMEOUT = 20000;
 
+    public static final String GERMPLASM_EXAMPLE_DBID = "germplasm1";
+    public static final String GERMPLASM_DBID = GERMPLASM_EXAMPLE_DBID;
+
     @CurrentUser
     AccountModel currentUser;
 
@@ -66,36 +56,26 @@ public class GermplasmAPI {
     private OlgaModule olgaModule;
 
     @GET
-    @Path("")
-    @ApiOperation("Get olga germplasms")
+    @Path("{germplasmDbId}")
+    @ApiOperation("Get olga germplasm by germplasmDbId")
     @ApiProtected
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Return list of olga germplasms", response = GermplasmDTO.class, responseContainer = "List")
+            @ApiResponse(code = 200, message = "Return details of an olga germplasm", response = org.brapi.client.v2.ApiResponse.class)
     })
-    public Response getGermplasms() throws Exception {
+    public org.brapi.client.v2.ApiResponse<BrAPIGermplasmSingleResponse> getGermplasmByDbId(
+            @ApiParam(value = GERMPLASM_DBID, example = GERMPLASM_EXAMPLE_DBID, required = true) @PathParam("germplasmDbId") @NotNull String germplasmDbId
+    ) throws Exception {
         OlgaConfig olgaConfig = olgaModule.getConfig(OlgaConfig.class);
         BrAPIClient brAPIClient = new BrAPIClient(olgaConfig.host(), DEFAULT_TIMEOUT);
         Authentication authorizationToken = brAPIClient.getAuthentication("AuthorizationToken");
         if (authorizationToken instanceof OAuth) {
             ((OAuth)authorizationToken).setAccessToken(olgaConfig.token());
-        };
+        }
         GermplasmApi germplasmApi = new GermplasmApi(brAPIClient);
-        /*org.brapi.client.v2.ApiResponse<BrAPIGermplasmListResponse> germplasms = germplasmApi.germplasmGet(new GermplasmQueryParams());*/
+        org.brapi.client.v2.ApiResponse<BrAPIGermplasmSingleResponse> germplasmDetail = germplasmApi.germplasmGermplasmDbIdGet(germplasmDbId);
 
-        // TODO : remove this tmp workaround for malformed response
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode response = mapper.readValue(
-                new File("/home/besombes/Documents/gitlab/opensilex-dev/opensilex-olga/src/main/java/org/opensilex/olga/tmp.json"),
-                JsonNode.class
-        );
-        Type resultType = new TypeToken<BrAPIGermplasmListResponseResult>(){}.getType();
-
-        JSON json = new JSON().setLenientOnJson(true);
-        BrAPIGermplasmListResponseResult result = json.deserialize(response.get("result").toString(), resultType);
-        // TODO : end of workaround
-
-        return new SingleObjectResponse<>(new GermplasmDTO().setGermplasmDbId("test")).getResponse();
+        return germplasmDetail;
     }
 }

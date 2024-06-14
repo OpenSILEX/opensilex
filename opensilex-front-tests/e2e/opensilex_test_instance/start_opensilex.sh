4a8a6359fec1f5bbd49c20091a7fabbfa046a9ac
BESOMBES Gabriel <gabriel.besombes@inrae.fr>
2024-06-14 13:14:54 +0200

    Re-added tree

--- opensilex-front-tests/e2e/opensilex_test_instance/start_opensilex.sh
+++ opensilex-front-tests/e2e/opensilex_test_instance/start_opensilex.sh
@@ -5,7 +5,7 @@
 #                         start_opensilex.sh
 # OpenSILEX - Licence AGPL V3.0 - https://www.gnu.org/licenses/agpl-3.0.en.html
 # Copyright © INRAE 2024.
-# Last Modification: 14/06/2024 12:41
+# Last Modification: 14/06/2024 13:13
 # Contact: gabriel.besombes@inrae.fr
 # ******************************************************************************
 #
@@ -18,8 +18,6 @@ CONFIG_FILE="$(readlink -f "opensilex.yml")"
 cd "${SCRIPT_DIR}/../../../opensilex-release/target/opensilex" || exit 1
 
 OPENSILEX="java -jar opensilex.jar"
-
-ls
 
 $OPENSILEX system install --CONFIG_FILE="$CONFIG_FILE" &&
 $OPENSILEX --CONFIG_FILE="$CONFIG_FILE" user add --admin &&

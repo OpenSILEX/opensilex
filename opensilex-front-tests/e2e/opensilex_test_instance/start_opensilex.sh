9037d9b03207450ee151dae452b281ccacb72cfe
BESOMBES Gabriel <gabriel.besombes@inrae.fr>
2024-06-20 18:11:53 +0200

    Added flexibility to start/not opensilex

--- opensilex-front-tests/e2e/opensilex_test_instance/start_opensilex.sh
+++ opensilex-front-tests/e2e/opensilex_test_instance/start_opensilex.sh
@@ -5,7 +5,7 @@
 #                         start_opensilex.sh
 # OpenSILEX - Licence AGPL V3.0 - https://www.gnu.org/licenses/agpl-3.0.en.html
 # Copyright © INRAE 2024.
-# Last Modification: 18/06/2024 15:59
+# Last Modification: 20/06/2024 17:45
 # Contact: gabriel.besombes@inrae.fr
 # ******************************************************************************
 #
@@ -13,26 +13,40 @@
 # Redirect all subsequent commands' stdout and stderr to the terminal
 exec > >(tee) 2>&1
 
+# Get dockerisedBases flag
+dockerisedBases=0
+
+while getopts ":d" opt; do
+  case "$opt" in
+    d) dockerisedBases=1 ;;
+    \?) echo "Invalid option -$OPTARG" >&2 ;;
+  esac
+done
+
+# Get absolute path of this script's directory
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-cd "$SCRIPT_DIR" || exit 1
-CONFIG_FILE="$(readlink -f "opensilex.yml")"
+# Get the config file
+CONFIG_FILE="${SCRIPT_DIR}/../../../opensilex-dev-tools/src/main/resources/config/opensilex.yml"
 
-cd "${SCRIPT_DIR}/../../.." || exit 1
-mvn clean install -DskipTests
 
-cd "${SCRIPT_DIR}/../../../opensilex-dev-tools/src/main/resources/docker" || exit 1
-
-if ! docker ps | grep "opensilex-mongodb\|opensilex-rdf4j"
-  then
-    docker compose -p test up &
-    sleep 30
+# Start the dockers if needed
+if [ "$dockerisedBases" = 1 ]; then
+  cd "${SCRIPT_DIR}/../../../opensilex-dev-tools/src/main/resources/docker" || exit 1
+  if ! docker ps | grep "opensilex-mongodb\|opensilex-rdf4j"
+    then
+      docker compose -p test up &
+      sleep 30
+  fi
 fi
 
+# Run maven clean install to get the latest changes
+cd "${SCRIPT_DIR}/../../.." || exit 1
+mvn clean install -DskipTests &&
+
+# Install and start OpenSILEX
 cd "${SCRIPT_DIR}/../../../opensilex-release/target/opensilex" || exit 1
-
 OPENSILEX="java -jar opensilex.jar"
-
 $OPENSILEX system install --CONFIG_FILE="$CONFIG_FILE" &&
 $OPENSILEX --CONFIG_FILE="$CONFIG_FILE" user add --admin &&
 $OPENSILEX server start --CONFIG_FILE="$CONFIG_FILE" --port=8080 --adminPort=4080 --DEBUG
--- opensilex-front-tests/e2e/playwright.config.ts
+++ opensilex-front-tests/e2e/playwright.config.ts
@@ -3,83 +3,99 @@
  *                         playwright.config.ts
  * OpenSILEX - Licence AGPL V3.0 - https://www.gnu.org/licenses/agpl-3.0.en.html
  * Copyright © INRAE 2024.
- * Last Modification: 18/06/2024 16:16
+ * Last Modification: 20/06/2024 18:11
  * Contact: gabriel.besombes@inrae.fr
  * *****************************************************************************
  */
 
-import { defineConfig, devices } from '@playwright/test';
-import * as path from 'path';
+import {defineConfig} from '@playwright/test';
 
-process.env.BASE_URL = "http://localhost:8080"
+let DEFAULT_BASE_URL = "http://localhost:8080"
+
+if (!process.env.BASE_URL) {
+    process.env.BASE_URL = DEFAULT_BASE_URL
+}
+
 process.env.APP_URL = process.env.BASE_URL + "/app"
+
+let COMMAND = './opensilex_test_instance/start_opensilex.sh'
+
+if (process.env.DOCKERISED_BASES) {
+    COMMAND += ' -d'
+}
+
+let webServer = undefined;
+
+if (process.env.START_OPENSILEX) {
+    webServer = {
+        command: COMMAND,
+        url: process.env.APP_URL,
+        reuseExistingServer: !process.env.CI,
+        timeout: 1200000, // 20 minutes
+        stdout: "pipe",
+        stderr: "pipe"
+    }
+}
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
-    storageState: 'state.json'
-  },
-
-  globalSetup: require.resolve('./global-setup'),
-
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium'
+    testDir: './tests',
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 0,
+    /* Opt out of parallel tests on CI. */
+    workers: process.env.CI ? 1 : undefined,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: 'html',
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: 'retain-on-failure',
+        storageState: 'state.json'
     },
 
-    {
-      name: 'firefox'
-    },
+    globalSetup: require.resolve('./global-setup'),
 
-    {
-      name: 'webkit'
-    },
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: 'chromium'
+        },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+        {
+            name: 'firefox'
+        },
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
+        {
+            name: 'webkit'
+        },
 
-  /* Run your local dev server before starting the tests */
-  webServer: !process.env.ACCESS_DISTANT ? {
-    command: './opensilex_test_instance/start_opensilex.sh',
-    url: process.env.APP_URL,
-    reuseExistingServer: !process.env.CI,
-    timeout: 1200000, // 20 minutes
-    stdout: "pipe",
-    stderr: "pipe"
-  } : undefined,
+        /* Test against mobile viewports. */
+        // {
+        //   name: 'Mobile Chrome',
+        //   use: { ...devices['Pixel 5'] },
+        // },
+        // {
+        //   name: 'Mobile Safari',
+        //   use: { ...devices['iPhone 12'] },
+        // },
+
+        /* Test against branded browsers. */
+        // {
+        //   name: 'Microsoft Edge',
+        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+        // },
+        // {
+        //   name: 'Google Chrome',
+        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+        // },
+    ],
+
+    /* Run your local dev server before starting the tests */
+    webServer: webServer
 });

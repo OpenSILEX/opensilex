bd49c211107b3c51bc84dc32b1ad08f13e88abef
PRADO SEBASTIEN <sebastien.prado@inrae.fr>
2024-06-24 15:21:34 +0200

    Reverted copyright

--- opensilex-front/front/src/opensilex.dev.ts
+++ opensilex-front/front/src/opensilex.dev.ts
@@ -1,11 +1,1 @@
-/*
- * *****************************************************************************
- *                         opensilex.dev.ts
- * OpenSILEX - Licence AGPL V3.0 - https://www.gnu.org/licenses/agpl-3.0.en.html
- * Copyright © INRAE 2024.
- * Last Modification: 25/06/2024 10:46
- * Contact: gabriel.besombes@inrae.fr
- * *****************************************************************************
- */
-
 export default { "last-dev-update": 0};

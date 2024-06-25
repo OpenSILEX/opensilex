5dbab125256141ecb46b06f07dfe2e2baedc22e3
BESOMBES Gabriel <gabriel.besombes@inrae.fr>
2024-06-25 10:33:37 +0200

    Revert change to opensilex.dev.ts

--- opensilex-front/front/src/opensilex.dev.ts
+++ opensilex-front/front/src/opensilex.dev.ts
@@ -1,1 +1,11 @@
-export default { "last-dev-update": 1718115889440};
+/*
+ * *****************************************************************************
+ *                         opensilex.dev.ts
+ * OpenSILEX - Licence AGPL V3.0 - https://www.gnu.org/licenses/agpl-3.0.en.html
+ * Copyright © INRAE 2024.
+ * Last Modification: 25/06/2024 10:33
+ * Contact: gabriel.besombes@inrae.fr
+ * *****************************************************************************
+ */
+
+export default { "last-dev-update": 0};

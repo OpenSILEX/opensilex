0670d901ca5b9fe756f95492249535b935e8cad4
CHIAVARINO ALEXIA <alexia.chiavarino@inrae.fr>
2024-07-04 08:42:22 +0200

    fix: list counter
    OpenSILEX/opensilex-dev!1239

--- opensilex-front/front/src/components/germplasm/GermplasmList.vue
+++ opensilex-front/front/src/components/germplasm/GermplasmList.vue
@@ -246,7 +246,7 @@
                         ></opensilex-EditButton>
                         <opensilex-DeleteButton
                             v-if="user.hasCredential(credentials.CREDENTIAL_GERMPLASM_DELETE_ID)"
-                            @click="$emit('onDelete', data.item.uri)"
+                            @click="deleteGermplasm(data.item.uri)"
                             label="GermplasmList.delete"
                             :small="true"
                         ></opensilex-DeleteButton>
@@ -601,6 +601,11 @@ export default class GermplasmList extends Vue {
                 that.tableRef.selectAll = false;
             }
         }, 1);
+    }
+
+    deleteGermplasm(uri){
+        this.tableRef.checkSelectedItems(uri);
+        this.$emit('onDelete', uri);
     }
 }
 </script>

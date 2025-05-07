375209be31004435f4ef65700c504c14948f0a92
Sebastien <sebapra34@yahoo.fr>
2025-05-07 15:16:23 +0200

    fix menu is not correct - fix rgpd label - get back data details modal

--- opensilex-dataverse/front/src/index.ts
+++ opensilex-dataverse/front/src/index.ts
@@ -1,21 +1,35 @@
-/// <reference path="../../../opensilex-dataverse/front/types/opensilex-dataverse.d.ts" />
-import {ApiServiceBinder} from './lib';
+// src/modules/dataverse/index.ts
+import type { App } from 'vue';
+import { ApiServiceBinder } from './lib';
 // import DatasetList from "./components/datasets/DatasetList.vue";
 // import DatasetForm from "./components/datasets/DatasetForm.vue";
 // import DataverseHelp from "./components/datasets/DataverseHelp.vue";
 
+import fr from "./lang/dataverse-fr.json";
+import en from "./lang/dataverse-en.json";
+
 export default {
-    install(Vue, options) {
-        ApiServiceBinder.with(Vue.$opensilex.getServiceContainer());
-    },
-    components : {
-    //     "opensilex-dataverse-DatasetList" : DatasetList,
-    //     "opensilex-dataverse-DatasetForm" : DatasetForm,
-    //     "opensilex-dataverse-DataverseHelp" : DataverseHelp
-    // },
-    },
-    lang: {
-        // "fr": require("./lang/dataverse-fr.json"),
-        // "en": require("./lang/dataverse-en.json")
+  install(app: App, options: any) {
+    // Accès au service container via les options, si nécessaire
+    const serviceContainer = options?.opensilex?.getServiceContainer?.();
+    if (serviceContainer) {
+      ApiServiceBinder.with(serviceContainer);
     }
-}
+
+    // Enregistrement global si souhaité (ou à utiliser localement)
+    // app.component("opensilex-dataverse-DatasetList", DatasetList);
+    // app.component("opensilex-dataverse-DatasetForm", DatasetForm);
+    // app.component("opensilex-dataverse-DataverseHelp", DataverseHelp);
+  },
+
+  components: {
+    // "opensilex-dataverse-DatasetList": DatasetList,
+    // "opensilex-dataverse-DatasetForm": DatasetForm,
+    // "opensilex-dataverse-DataverseHelp": DataverseHelp,
+  },
+
+  lang: {
+    fr: fr,
+    en: en,
+  }
+};
---
+++ opensilex-front/front/src/components/data/DataProvenanceModalView.vue
@@ -0,0 +1,76 @@
+<template>
+  <Modal ref="modalRef">
+    <template #header>
+      <div class="container-fluid w-100">
+        <div class="row w-100 mt-1">
+          <div class="col-10">
+            <h4><opensilex-Icon icon="bi#bi-eye" /></h4>
+          </div>
+        </div>
+      </div>
+    </template>
+
+    <h3>{{ datafile ? t('DataProvenanceModalView.datafile') : t('DataProvenanceModalView.data') }}</h3>
+    <pre>{{ data }}</pre>
+    <h3>Provenance</h3>
+    <pre>{{ provenance }}</pre>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import { ref, defineProps, defineExpose } from 'vue';
+import { useI18n } from 'vue-i18n';
+import Modal from './../common/views/Modal.vue'
+
+const props = defineProps({
+  datafile: {
+    type: Boolean,
+    default: false
+  }
+});
+
+// i18n
+const { t } = useI18n();
+
+// const modalRef = ref<InstanceType<typeof Modal> | null>(null);
+const modalRef = ref<any>(null);
+const data = ref('');
+const provenance = ref('');
+
+function show() {
+  modalRef.value?.show();
+}
+function hide() {
+  modalRef.value?.hide();
+}
+
+function setProvenance(value: { data: any; provenance: any }) {
+  data.value = JSON.stringify(value.data, null, 2);
+  provenance.value = JSON.stringify(value.provenance, null, 2);
+}
+
+defineExpose({
+  show,
+  hide,
+  setProvenance
+});
+</script>
+
+<i18n>
+fr: 
+  DataProvenanceModalView:
+    data : Donnée
+    datafile : Fichier de données
+
+en: 
+  DataProvenanceModalView:
+    data : Data
+    datafile : Datafile
+</i18n>
+
+<style scoped>
+.modal.show {
+  display: block;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>

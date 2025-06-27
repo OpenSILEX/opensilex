578e98753541d9188cfe01cfc29b628563a9f021
Lydia <lydia.aliane@inrae.fr>
2025-06-27 15:33:31 +0200

    MAJ fonction formatDate

--- opensilex-front/front/src/components/annotations/list/AnnotationList.vue
+++ opensilex-front/front/src/components/annotations/list/AnnotationList.vue
@@ -101,7 +101,6 @@ import {SecurityService} from "opensilex-security/api/security.service";
 import {UserGetDTO} from 'opensilex-security/index';
 import {AnnotationGetDTO} from 'opensilex-core/index';
 import {AccountGetDTO} from "opensilex-security/model/accountGetDTO";
-import dayjs from 'dayjs';
 
 @Component
 export default class AnnotationList extends Vue {
@@ -143,7 +142,9 @@ export default class AnnotationList extends Vue {
   @Ref("annotationModalForm") readonly annotationModalForm!: AnnotationModalForm;
 
   formatDate(dateStr: string): string {
-    return dayjs(dateStr).format('YYYY-MM-DD HH:mm');
+    const localeString = new Date(dateStr).toLocaleString();
+
+    return localeString.replace(/\//g, '-');
   }
 
   static getDefaultColumns() {

f0817d5778d278528dfa36e3052792bdc45689a3
Lydia <lydia.aliane@inrae.fr>
2025-08-01 13:39:26 +0200

    MAJ format date annotation

--- opensilex-front/front/src/components/annotations/list/AnnotationList.vue
+++ opensilex-front/front/src/components/annotations/list/AnnotationList.vue
@@ -143,7 +143,17 @@ export default class AnnotationList extends Vue {
   @Ref("annotationModalForm") readonly annotationModalForm!: AnnotationModalForm;
 
   formatDate(dateStr: string): string {
-    return dayjs(dateStr).format('YYYY-MM-DD HH:mm:ss');
+    const date = new Date(dateStr);
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
   }
 
   static getDefaultColumns() {

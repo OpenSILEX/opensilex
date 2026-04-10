c0ca3ac13eb670c6fa5d2aee56dbc35260c4c3c5
Valentin Rigolle <valentin.rigolle@inrae.fr>
2026-04-10 10:01:45 +0200

    IconForm

--- opensilex-front/front/src/components/common/forms/IconForm.vue
+++ opensilex-front/front/src/components/common/forms/IconForm.vue
@@ -9,6 +9,7 @@
       <n-select
         :options="icons"
         v-model="value"
+        filterable
       >
       </n-select>
     </template>
@@ -17,27 +18,28 @@
 
 <script setup lang="ts">
 import OpenSilexVuePlugin from "@/models/OpenSilexVuePlugin";
-import {computed, inject, nextTick, onMounted, ref, useTemplateRef} from "vue";
-import {NSelect} from "naive-ui";
+import {h, inject, onMounted, ref, VNodeChild} from "vue";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {NSelect, SelectOption} from "naive-ui";
 
 const opensilex = inject<OpenSilexVuePlugin>("$opensilex")
 
-const observer = ref<IntersectionObserver>();
-const limit = ref(20);
-const search = ref("");
-const icons = computed(() => {
-  return opensilex.getSelectIconIDs().map(({id, label}) => ({
-    label,
+const icons = ref<Array<{
+  value: string,
+  label: string | ((option: SelectOption) => VNodeChild),
+}>>( []);
+
+onMounted(() => {
+  icons.value = opensilex.getSelectIconIDs().map(({id, iconName}) => ({
     value: id,
+    // There is no slot at the moment for select options, so we provide a render function
+    label: (option: SelectOption) => h('div', [h(FontAwesomeIcon, {icon: iconName, style: { 'margin-right': '10px' }}), option.value]),
   }))
 });
 
-// const filtered = computed(() => icons.value.filter(icon => icon.id.includes(search.value)));
-// const paginated = computed(() => filtered.value.slice(0, limit.value));
-
 const value = defineModel('value');
 
-const props = defineProps<{
+defineProps<{
   label: string,
   helpMessage: string,
   placeholder: string,
@@ -45,81 +47,8 @@ const props = defineProps<{
   disabled: boolean,
   rules: string | Function
 }>();
-
-//   withPopper(dropdownList, component, { width }) {
-//     /**
-//      * We need to explicitly define the dropdown width since
-//      * it is usually inherited from the parent with CSS.
-//      */
-//     dropdownList.style.width = width;
-//
-//     /**
-//      * Here we position the dropdownList relative to the $refs.toggle Element.
-//      *
-//      * The 'offset' modifier aligns the dropdown so that the $refs.toggle and
-//      * the dropdownList overlap by 1 pixel.
-//      *
-//      * The 'toggleClass' modifier adds a 'drop-up' class to the Vue Select
-//      * wrapper so that we can set some styles for when the dropdown is placed
-//      * above.
-//      */
-//     const popper = createPopper(component.$refs.toggle, dropdownList, {
-//       placement: "top",
-//       modifiers: [
-//         {
-//           name: "offset",
-//           options: {
-//             offset: [0, -1]
-//           }
-//         },
-//         {
-//           name: "toggleClass",
-//           enabled: true,
-//           phase: "write",
-//           fn({ state }) {
-//             component.$el.classList.toggle(
-//               "drop-up",
-//               state.placement === "top"
-//             );
-//           }
-//         }
-//       ]
-//     });
-//
-//     /**
-//      * To prevent memory leaks Popper needs to be destroyed.
-//      * If you return function, it will be called just before dropdown is removed from DOM.
-//      */
-//     return () => popper.destroy();
-//   }
-// }
 </script>
 
 <style scoped lang="scss">
-::v-deep .vs__selected {
-  white-space: break-spaces;
-}
-
-.open-indicator {
-  color: rgb(204, 204, 204);
-}
-
-.open-indicator:hover {
-  cursor: pointer;
-  color: rgb(97, 97, 97);
-}
-
-::v-deep .open-indicator svg {
-  font-size: 16px;
-}
-
-::v-deep .v-select-unselect {
-  color: rgb(204, 204, 204);
-}
-
-::v-deep .v-select-unselect:hover {
-  color: #e53935;
-  cursor: pointer;
-}
 </style>
 

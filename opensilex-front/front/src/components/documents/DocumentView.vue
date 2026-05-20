307a7b8e4f05693fd9fe8212c6ee476e3f00bb42
Emilie Fernandez <emilie.fernandez@inrae.fr>
2026-05-20 12:05:38 +0000

    feat(Vue3): Document
    OpenSILEX/opensilex-dev!1487

---
+++ opensilex-front/front/src/components/documents/DocumentView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container-fluid py-3">
+
+
+
+    <opensilex-PageContent>
+      <template v-slot>
+        <opensilex-DocumentList
+          ref="documentListRef"
+          :redirectAfterCreation="true"
+        ></opensilex-DocumentList>
+      </template>
+    </opensilex-PageContent>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
+
+const store = useStore()
+const router = useRouter()
+
+// refs 
+const documentListRef = ref<any>(null)
+
+// user + credentials  
+const user = computed(() => store.state.user)
+const credentials = computed(() => store.state.credentials)
+
+</script>
+
+<style scoped lang="scss">
+</style>
+
+<i18n>
+en:
+  DocumentView:
+    add: Add document
+    update: Update Document
+    delete: Delete DOcument
+fr:
+  DocumentView:
+    add: Ajouter un document
+    update: Editer un document
+    delete: Supprimer un document
+</i18n>

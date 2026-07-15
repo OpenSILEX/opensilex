9a86191b23f6397f04b5c12b04a3692a1db1409c
Valentin Rigolle <valentin.rigolle@inrae.fr>
2026-07-15 15:29:43 +0200

    First prototype for a deref module - test redirect

---
+++ opensilex-deref/src/main/java/org/opensilex/deref/DerefModule.java
@@ -0,0 +1,32 @@
+package org.opensilex.deref;
+
+import org.opensilex.OpenSilexModule;
+import org.opensilex.OpenSilexModuleNotFoundException;
+import org.opensilex.server.Server;
+import org.opensilex.server.ServerConfig;
+import org.opensilex.server.ServerModule;
+import org.opensilex.server.extensions.ServerExtension;
+import org.opensilex.server.scanner.IgnoreJarScanner;
+
+public class DerefModule extends OpenSilexModule implements ServerExtension {
+    public String getApplicationPathPrefix() {
+        try {
+            var cfg = getOpenSilex().getModuleConfig(ServerModule.class, ServerConfig.class);
+            return cfg.pathPrefix();
+        } catch (OpenSilexModuleNotFoundException ex) {
+            return "";
+        }
+    }
+
+    @Override
+    public void initServer(Server server) throws Exception {
+        ServerExtension.super.initServer(server);
+
+        var context = server.initApp(getApplicationPathPrefix() + "/id", "/", "/", DerefModule.class);
+        context.setJarScanner(new IgnoreJarScanner());
+
+        var valve = new DerefRewriteValve();
+        context.getPipeline().addValve(valve);
+        valve.initRules();
+    }
+}
---
+++ opensilex-deref/src/main/java/org/opensilex/deref/DerefRewriteValve.java
@@ -0,0 +1,34 @@
+package org.opensilex.deref;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.valves.rewrite.RewriteValve;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+
+public class DerefRewriteValve extends RewriteValve {
+    protected void parse(BufferedReader reader) throws LifecycleException {
+        reader = new BufferedReader(new StringReader(getRewriteRules()));
+        super.parse(reader);
+    }
+
+    public void initRules() throws Exception {
+        setConfiguration(getRewriteRules());
+    }
+
+    private String getRewriteRules() {
+        return """
+                    RewriteCond %{REQUEST_URI} ^.*/part0/.+$
+                    RewriteRule (.*)/part0/(.*)$ $0 [L,NE]
+                    RewriteCond %{REQUEST_URI} ^.*/part1/.+$
+                    RewriteRule (.*)/part1/(.*)$ $1 [L,NE]
+                    RewriteCond %{REQUEST_URI} ^.*/part2/.+$
+                    RewriteRule (.*)/part2/(.*)$ $2 [L,NE]
+                    RewriteCond %{REQUEST_PATH} ^.*/example/.+$
+                    RewriteRule .* http://example.org/myredirect [R=303,L,NE]
+                    RewriteCond %{REQUEST_PATH} ^/example3/$
+                    RewriteRule .* http://example.org/myredirect [R=303,L,NE]
+                """;
+
+    }
+}

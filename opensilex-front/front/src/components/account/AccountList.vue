af6ed5c426fe439517a8689fd3e4b6d65bf7b2aa
ROUX YVAN <yvan.roux@inrae.fr>
2026-07-17 13:31:49 +0200

    vue3(account): list ai migration

--- opensilex-front/front/src/components/account/AccountList.vue
+++ opensilex-front/front/src/components/account/AccountList.vue
@@ -1,245 +1,235 @@
 <template>
   <div>
-    <opensilex-StringFilter
-        :filter.sync="filter"
-        @update="updateFilter()"
-        placeholder="component.account.filter-placeholder"
-        :debounce="300"
-        :lazy="false"
-    ></opensilex-StringFilter>
+     <StringFilter
+      v-model:filter="filter"
+      @update="updateFilter()"
+      placeholder="component.account.filter-placeholder"
+      :debounce="300"
+      :lazy="false"
+    ></StringFilter>
 
-    <opensilex-TableAsyncView
-        ref="tableRef"
-        :searchMethod="searchAccounts"
-        :fields="fields"
-        defaultSortBy="email"
+    <TableAsyncView
+      ref="tableRef"
+      :searchMethod="searchAccounts"
+      :fields="fields"
+      defaultSortBy="email"
     >
-      <template v-slot:cell(uri)="{data}">
-        <opensilex-UriLink
-            :uri="data.item.uri"
-            :value="data.item.uri"
-            :noExternalLink="true"
-            :isClickable="false"
-        ></opensilex-UriLink>
+      <template #cell(uri)="{data}">
+        <UriLink
+          :uri="data.item.uri"
+          :value="data.item.uri"
+          :noExternalLink="true"
+          :isClickable="false"
+        ></UriLink>
       </template>
 
-      <template v-slot:cell(last_name)="{data}">
-        <opensilex-PersonContact
-            v-if="personByAccountUri[data.item.uri]"
-            :personContact="personByAccountUri[data.item.uri]"
+      <template #cell(last_name)="{data}">
+        <PersonContact
+          v-if="personByAccountUri[data.item.uri]"
+          :personContact="personByAccountUri[data.item.uri]"
         />
         <div v-else/>
       </template>
 
-      <template v-slot:cell(email)="{data}">
+      <template #cell(email)="{data}">
         <a :href="'mailto:' + data.item.email">{{ data.item.email }}</a>
       </template>
 
-      <template v-slot:cell(admin)="{data}">
-        <span class="capitalize-first-letter" v-if="data.item.admin">{{ $t("component.common.yes") }}</span>
-        <span class="capitalize-first-letter" v-if="!data.item.admin">{{ $t("component.common.no") }}</span>
+      <template #cell(admin)="{data}">
+        <span class="capitalize-first-letter" v-if="data.item.admin">{{ t("component.common.yes") }}</span>
+        <span class="capitalize-first-letter" v-if="!data.item.admin">{{ t("component.common.no") }}</span>
       </template>
 
-      <template v-slot:row-details="{data}">
-        <strong class="capitalize-first-letter">{{ $t("component.account.user-groups") }}:</strong>
+      <template #row-details="{data}">
+        <strong class="capitalize-first-letter">{{ t("component.account.user-groups") }}:</strong>
         <ul>
           <li
-              v-for="groupDetail in  groupDetailsByAccountUri[data.item.uri]"
-              v-bind:key="groupDetail.uri"
-          >{{ groupDetail.name }}
-          </li>
+            v-for="groupDetail in groupDetailsByAccountUri[data.item.uri]"
+            :key="groupDetail.uri"
+          >{{ groupDetail.name }}</li>
         </ul>
       </template>
 
-      <template v-slot:cell(actions)="{data}">
+      <template #cell(actions)="{data}">
         <b-button-group size="sm">
           <div class="checkEnable"
-               :title="data.item.enable ? $t('component.account.enable') : $t('component.account.disable')">
+            :title="data.item.enable ? t('component.account.enable') : t('component.account.disable')">
             <b-check
-                @change="changeEnable(data.item)"
-                v-if="displayEnableButton(data.item)"
-                :checked=" data.item.enable "
-                variant="outline-success"
-                switch
+              @change="changeEnable(data.item)"
+              v-if="displayEnableButton(data.item)"
+              :checked="data.item.enable"
+              variant="outline-success"
+              switch
             ></b-check>
           </div>
-          <opensilex-DetailButton
-              @click="showUsersGroups(data)"
-              label="component.account.details"
-              :detailVisible="data.detailsShowing"
-              :small="true"
-          ></opensilex-DetailButton>
-          <opensilex-EditButton
-              v-if="user.hasCredential(credentials.CREDENTIAL_ACCOUNT_MODIFICATION_ID)"
-              @click="$emit('onEdit', data.item)"
-              label="component.account.update"
-              :small="true"
-          ></opensilex-EditButton>
-          <opensilex-DeleteButton
-              v-if="user.hasCredential(credentials.CREDENTIAL_ACCOUNT_DELETE_ID)"
-              @click="deleteAccount(data.item.uri)"
-              label="component.account.delete"
-              :small="true"
-          >
-          </opensilex-DeleteButton>
+          <DetailButton
+            @click="showUsersGroups(data)"
+            label="component.account.details"
+            :detailVisible="data.detailsShowing"
+            :small="true"
+          ></DetailButton>
+          <EditButton
+            v-if="user.hasCredential(credentials.CREDENTIAL_ACCOUNT_MODIFICATION_ID)"
+            @click="emit('onEdit', data.item)"
+            label="component.account.update"
+            :small="true"
+          ></EditButton>
+          <DeleteButton
+            v-if="user.hasCredential(credentials.CREDENTIAL_ACCOUNT_DELETE_ID)"
+            @click="deleteAccount(data.item.uri)"
+            label="component.account.delete"
+            :small="true"
+          ></DeleteButton>
         </b-button-group>
       </template>
-    </opensilex-TableAsyncView>
+    </TableAsyncView>
   </div>
 </template>
 
-<script lang="ts">
-import {Component, Ref} from "vue-property-decorator";
-import Vue from "vue";
+<script setup lang="ts">
+import {computed, inject, ref, onMounted, useTemplateRef} from "vue";
+import {useStore} from "vuex";
+import {useRoute} from "vue-router";
 import {SecurityService} from "opensilex-security/index";
 import {PersonDTO} from "opensilex-security/model/personDTO";
-import HttpResponse, {OpenSilexResponse} from "../../lib/HttpResponse";
-import {SlotDetails} from "../common/views/TableAsyncView.vue";
 import {NamedResourceDTO} from "opensilex-core/model/namedResourceDTO";
 import {AccountUpdateDTO} from "opensilex-security/model/accountUpdateDTO";
 import {AccountGetDTO} from "opensilex-security/model/accountGetDTO";
-import OpenSilexVuePlugin from "../../models/OpenSilexVuePlugin";
+import HttpResponse, {OpenSilexResponse} from "@/lib/HttpResponse";
+import {SlotDetails} from "@/components/common/views/TableAsyncView.vue";
+import OpenSilexVuePlugin from "@/models/OpenSilexVuePlugin";
+import {OpenSilexStore} from "@/models/Store";
+import StringFilter from "@/components/common/filters/StringFilter.vue";
+import TableAsyncView from "@/components/common/views/TableAsyncView.vue";
+import PersonContact from "@/components/persons/PersonContact.vue";
+import UriLink from "@/components/common/views/UriLink.vue";
+import DetailButton from "@/components/common/buttons/DetailButton.vue";
+import EditButton from "@/components/common/buttons/EditButton.vue";
+import DeleteButton from "@/components/common/buttons/DeleteButton.vue";
+import {useI18n} from "vue-i18n";
 
-@Component
-export default class AccountList extends Vue {
-  $opensilex: OpenSilexVuePlugin;
-  service: SecurityService;
-  $store: any;
-  $route: any;
+const opensilex = inject<OpenSilexVuePlugin>("$opensilex")!;
+const service = opensilex.getService<SecurityService>("opensilex-core.SecurityService");
+const store = useStore() as OpenSilexStore;
+const route = useRoute();
+const {t} = useI18n();
 
-  fields = [
-    {
-      key: "uri",
-      label: "component.common.uri"
-    },
-    {
-      key: "last_name",
-      label: "component.account.linked-person"
-    },
-    {
-      key: "email",
-      label: "component.account.email",
-      sortable: true
-    },
-    {
-      key: "admin",
-      label: "component.account.admin",
-      sortable: true
-    },
-    {
-      label: "component.common.actions",
-      key: "actions",
-      class: "table-actions"
-    }
-  ];
+//#region Refs
+const tableRef = useTemplateRef<InstanceType<typeof TableAsyncView>>('tableRef');
+//#endregion
 
-  personByAccountUri :{
-    [id: string]: PersonDTO;
-  } =  {}
+//#region Data and computed
+const user = computed(() => store.state.user);
+const credentials = computed(() => store.state.credentials);
 
-  groupDetailsByAccountUri :{
-    [id: string]: NamedResourceDTO[];
-  } =  {}
+const filter = ref<string>("");
 
-  get user() {
-    return this.$store.state.user;
+const fields = [
+  {key: "uri", label: "component.common.uri"},
+  {key: "last_name", label: "component.account.linked-person"},
+  {key: "email", label: "component.account.email", sortable: true},
+  {key: "admin", label: "component.account.admin", sortable: true},
+  {label: "component.common.actions", key: "actions", class: "table-actions"}
+];
+
+const personByAccountUri = ref<{[id: string]: PersonDTO | null}>({});
+const groupDetailsByAccountUri = ref<{[id: string]: NamedResourceDTO[]}>({});
+//#endregion
+
+//#region Emits
+const emit = defineEmits<{
+  (e: "onEdit", payload: AccountGetDTO): void
+}>();
+//#endregion
+
+//#region Event handlers and watchers
+onMounted(() => {
+  const query: any = route.query;
+  if (query.filter) {
+    filter.value = decodeURIComponent(query.filter);
   }
+});
 
-  get credentials() {
-    return this.$store.state.credentials;
-  }
-
-  private filter: any = "";
-  @Ref("tableRef") readonly tableRef!: any;
-  currentURI = null;
-  groupDetails = [];
-
-  created() {
-    let query: any = this.$route.query;
-    if (query.filter) {
-      this.filter = decodeURIComponent(query.filter);
-    }
-    this.service = this.$opensilex.getService("opensilex.SecurityService");
-  }
-
-  updateFilter() {
-    this.$opensilex.updateURLParameter("filter", this.filter, "");
-    this.refresh();
-  }
-
-  refresh() {
-    this.tableRef.refresh();
-  }
-
-  async searchAccounts(options) {
-    let accountsResponse = await this.service.searchAccounts(
-        this.filter,
-        options.orderBy,
-        options.currentPage,
-        options.pageSize
-    )
-
-    let key_personUri_value_accountUri : {[id: string]: string} = {}
-
-    accountsResponse.response.result.forEach( account => {
-      this.personByAccountUri[account.uri] = null
-      if (account.linked_person) {
-        key_personUri_value_accountUri[account.linked_person] = account.uri
-      }
-    });
-
-    await this.mapPersonsWithAccount(key_personUri_value_accountUri)
-
-    return accountsResponse
-  }
-
-  deleteAccount(uri: string) {
-    this.$opensilex.showLoader()
-    this.service.deleteAccount(uri)
-        .then( () => this.refresh() )
-        .catch( (error) => this.$opensilex.errorHandler(error))
-        .finally( () => this.$opensilex.hideLoader())
-  }
-
-  async mapPersonsWithAccount(key_personUri_value_accountUri : {[id: string]: string}){
-    if ( Object.keys(key_personUri_value_accountUri).length !== 0 ) {
-      let personsResponse = await this.service.getPersonsByURI(Object.keys(key_personUri_value_accountUri))
-      personsResponse.response.result.forEach(person => {
-        let accountUri = key_personUri_value_accountUri[this.$opensilex.getLongUri(person.uri)]
-        this.personByAccountUri[accountUri] = person
-      })
-    }
-  }
-
-  changeEnable(dto: AccountUpdateDTO) {
-    dto.enable = !dto.enable;
-    this.service
-        .updateAccount(dto)
-        .catch(this.$opensilex.errorHandler);
-  }
-
-  displayEnableButton(accountRow) {
-    let isUserConnected = accountRow.email === this.user.email
-    return this.user.hasCredential(this.credentials.CREDENTIAL_ACCOUNT_MODIFICATION_ID)
-        && !accountRow.admin
-        && !isUserConnected
-  }
-
-  async showUsersGroups(data: SlotDetails<AccountGetDTO>) {
-
-    let accountUri :string = data.item.uri
-
-    if ( !this.groupDetailsByAccountUri[accountUri] ) {
-      let groupResponse :HttpResponse<OpenSilexResponse<Array<NamedResourceDTO>>> = await this.service.getUserGroups(accountUri)
-      let groups :NamedResourceDTO[] = groupResponse.response.result
-      this.groupDetailsByAccountUri[accountUri] = groups
-    }
-
-    data.toggleDetails()
-  }
-
+function updateFilter(): void {
+  opensilex.updateURLParameter("filter", filter.value, "");
+  refresh();
 }
 
+function refresh(): void {
+  tableRef.value?.refresh();
+}
+
+async function searchAccounts(options: any): Promise<any> {
+  const accountsResponse = await service.searchAccounts(
+    filter.value,
+    options.orderBy,
+    options.currentPage,
+    options.pageSize
+  );
+
+  const key_personUri_value_accountUri: {[id: string]: string} = {};
+
+  accountsResponse.response.result.forEach((account: any) => {
+    personByAccountUri.value[account.uri] = null;
+    if (account.linked_person) {
+      key_personUri_value_accountUri[account.linked_person] = account.uri;
+    }
+  });
+
+  await mapPersonsWithAccount(key_personUri_value_accountUri);
+
+  return accountsResponse;
+}
+
+async function mapPersonsWithAccount(key_personUri_value_accountUri: {[id: string]: string}): Promise<void> {
+  if (Object.keys(key_personUri_value_accountUri).length !== 0) {
+    const personsResponse = await service.getPersonsByURI(Object.keys(key_personUri_value_accountUri));
+    personsResponse.response.result.forEach((person: PersonDTO) => {
+      const accountUri = key_personUri_value_accountUri[opensilex.getLongUri(person.uri)];
+      personByAccountUri.value[accountUri] = person;
+    });
+  }
+}
+
+function deleteAccount(uri: string): void {
+  opensilex.showLoader();
+  service.deleteAccount(uri)
+    .then(() => refresh())
+    .catch((error: any) => opensilex.errorHandler(error))
+    .finally(() => opensilex.hideLoader());
+}
+
+function changeEnable(dto: AccountUpdateDTO): void {
+  dto.enable = !dto.enable;
+  service.updateAccount(dto)
+    .catch((error: any) => opensilex.errorHandler(error));
+}
+
+function displayEnableButton(accountRow: any): boolean {
+  const isUserConnected = accountRow.email === user.value.email;
+  return user.value.hasCredential(credentials.value.CREDENTIAL_ACCOUNT_MODIFICATION_ID)
+    && !accountRow.admin
+    && !isUserConnected;
+}
+
+async function showUsersGroups(data: SlotDetails<AccountGetDTO>): Promise<void> {
+  const accountUri: string = data.item.uri;
+
+  if (!groupDetailsByAccountUri.value[accountUri]) {
+    const groupResponse: HttpResponse<OpenSilexResponse<NamedResourceDTO[]>> = await service.getUserGroups(accountUri);
+    const groups: NamedResourceDTO[] = groupResponse.response.result;
+    groupDetailsByAccountUri.value[accountUri] = groups;
+  }
+
+  data.toggleDetails();
+}
+//#endregion
+
+//#region Exposed methods
+defineExpose({
+  refresh
+});
+//#endregion
 </script>
 
 <style scoped lang="scss">

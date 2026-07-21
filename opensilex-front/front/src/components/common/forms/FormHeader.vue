3b19a294f6db3e5832564d9e75fbbd21d6af26e2
ROUX YVAN <yvan.roux@inrae.fr>
2026-07-21 16:26:14 +0200

    vue3(form): all logic in one form, create footer and header

---
+++ opensilex-front/front/src/components/common/forms/FormHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex justify-between items-center">
+    <h4>
+      <slot name="icon">
+        <opensilex-Icon :icon="icon" class="icon-title" v-if="icon" />
+      </slot>
+      {{ title }}
+    </h4>
+    <opensilex-HelpButton
+      v-if="tutorial"
+      label="component.tutorial.name"
+      class="modalFormTutorial"
+      @click="onHelp"
+      :small="true"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  title: { type: String, required: true },
+  icon: String,
+  tutorial: { type: Boolean, default: false },
+  helpAction: Function
+})
+
+function onHelp() {
+  props.helpAction?.()
+}
+</script>
+
+<style scoped lang="scss">
+.modalFormTutorial {
+  position: absolute;
+  top: 19px;
+  right: 50px;
+}
+</style>

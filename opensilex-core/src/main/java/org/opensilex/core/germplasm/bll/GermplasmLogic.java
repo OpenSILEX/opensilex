4e9b7b94d8614721cebb005b2e0281c3adb6c41c
Valentin Rigolle <valentin.rigolle@inrae.fr>
2026-08-12 11:01:55 +0200

    Fixed CSV export by moving the group logic in the business layer

--- opensilex-core/src/main/java/org/opensilex/core/germplasm/bll/GermplasmLogic.java
+++ opensilex-core/src/main/java/org/opensilex/core/germplasm/bll/GermplasmLogic.java
@@ -188,6 +188,10 @@ public class GermplasmLogic {
             boolean fetchMetadata,
             boolean fetchNestedObjects
     ) throws Exception {
+        var user = searchFilter.getUser();
+        if (user != null && !user.isAdmin()) {
+            searchFilter.setGroups(groupDao.getGroupUriList(searchFilter.getUser()));
+        }
         return dao.search(searchFilter, fetchMetadata, fetchNestedObjects);
     }
 

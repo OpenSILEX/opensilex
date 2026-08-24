c48a9ae1a3eb9202ec3ddda634fc9ec90cde9e23
Lasdepic <clementjorda34@gmail.com>
2026-08-24 15:38:30 +0200

    Traduction message-lang.json avec image event

--- opensilex-front/front/src/components/events/EventsView.vue
+++ opensilex-front/front/src/components/events/EventsView.vue
@@ -1,17 +1,12 @@
 <template>
   <div class="container-fluid">
-    <PageContent
-    class="eventsContent">
+    <PageContent class="eventsContent">
       <template v-slot>
-        <EventList
-            ref="eventList"
-            :enableActions="true"
-            :columnsToDisplay="new Set(['type','start','end','description', 'targets'])"
-            :modificationCredentialId="credentials.CREDENTIAL_EVENT_MODIFICATION_ID"
-            :deleteCredentialId="credentials.CREDENTIAL_EVENT_DELETE_ID"
-            :displayTitle="false"
-            :isExperimentalFeature="false"
-        ></EventList>
+        <EventList ref="eventList" :enableActions="true"
+          :columnsToDisplay="new Set(['type', 'start', 'end', 'description', 'targets'])"
+          :modificationCredentialId="credentials.CREDENTIAL_EVENT_MODIFICATION_ID"
+          :deleteCredentialId="credentials.CREDENTIAL_EVENT_DELETE_ID" :displayTitle="false"
+          :isExperimentalFeature="false"></EventList>
       </template>
     </PageContent>
   </div>
@@ -19,26 +14,13 @@
 
 
 <script setup lang="ts">
-    import { computed, inject, Ref, useTemplateRef } from "vue";
-    import Vue from "vue";
-    import EventList from "./list/EventList.vue";
+import { computed } from "vue";
+import EventList from "./list/EventList.vue";
 import { useStore } from "vuex";
-import { useRouter } from "vue-router";
-import { useI18n } from "vue-i18n";
-import OpenSilexVuePlugin from '@/models/OpenSilexVuePlugin';
-import { EventsService } from "opensilex-core";
 import { OpenSilexStore } from '../../models/Store';
-    
-    const store = useStore() as OpenSilexStore;
-    const router = useRouter()
-    const { t } = useI18n()
-    const opensilex: OpenSilexVuePlugin = inject<OpenSilexVuePlugin>('$opensilex')
-    const eventsService = opensilex.getService<EventsService>('opensilex.EventsService')
 
-    const eventList = useTemplateRef<InstanceType<typeof EventList>>('eventList')    
-    
-    const user = computed(() => store.state.user)
-    const credentials = computed(() => store.state.credentials)
+const store = useStore() as OpenSilexStore;
+const credentials = computed(() => store.state.credentials)
 
 </script>
 
@@ -48,13 +30,3 @@ import { OpenSilexStore } from '../../models/Store';
   margin-top: -25px;
 }
 </style>
-
-
-<i18n>
-en:
-  EventsView:
-    description: Manage and configure events
-fr:
-  EventsView:
-    description: Gérer et configurer les événementS
-</i18n>

5bfa0ef2f6d44f92a4a70eae893f46cd77a81b1d
ROUX YVAN <yvan.roux@inrae.fr>
2026-09-08 13:14:25 +0200

    feat(phis): convert phis component to vu3

---
+++ opensilex-phis/front/src/components/layout/PhisHeaderComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <opensilex-DefaultHeaderComponent>
+    <template v-slot:headerLogo>
+        <img
+            v-bind:src="opensilex.getResourceURI('images/logo-phis.svg')"
+            class="header-brand-img"
+            alt="lavalite"
+        />
+    </template>
+  </opensilex-DefaultHeaderComponent>
+  
+</template>
+
+<script setup lang="ts">
+import { inject } from "vue";
+import OpenSilexVuePlugin from "../../../../../opensilex-front/front/src/models/OpenSilexVuePlugin";
+
+const opensilex = inject<OpenSilexVuePlugin>("$opensilex");
+
+
+</script>

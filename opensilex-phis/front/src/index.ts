46c1dfa30ef6a8f3c0d5c2143fb22122844f905e
ROUX YVAN <yvan.roux@inrae.fr>
2026-09-09 14:58:13 +0200

    fix(phis): fix vite build

--- opensilex-phis/front/src/index.ts
+++ opensilex-phis/front/src/index.ts
@@ -1,16 +1,11 @@
 import PhisLoginComponent from "./components/layout/PhisLoginComponent.vue";
 import PhisHeaderComponent from "./components/layout/PhisHeaderComponent.vue";
 
-const components = {
-    "opensilex-phis-PhisLoginComponent": PhisLoginComponent,
-    "opensilex-phis-PhisHeaderComponent": PhisHeaderComponent
-};
-
 export default {
-    install(app, options) {
-        for (const componentName in components) {
-            app.component(componentName, components[componentName]);
-        }
+    install(Vue, options) {
     },
-    components
+    components : {
+        "opensilex-phis-PhisLoginComponent" : PhisLoginComponent,
+        "opensilex-phis-PhisHeaderComponent" : PhisHeaderComponent
+    }
 };

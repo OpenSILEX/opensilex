e2fc161ef578a88114c8dd1f523ac7af8f8331cb
ROUX YVAN <yvan.roux@inrae.fr>
2026-09-21 09:48:17 +0200

    feat(module): BRAPPI better index.ts

--- opensilex-brapi/front/src/index.ts
+++ opensilex-brapi/front/src/index.ts
@@ -1,6 +1,7 @@
+import {OpensilexModulePlugin} from "../../../opensilex-front/front/src/models/OpensilexModulePlugin";
 import { ApiServiceBinder } from './lib';
 export default {
-    install(Vue, options) {
-        ApiServiceBinder.with(Vue.$opensilex.getServiceContainer());
+    install(App, options) {
+        ApiServiceBinder.with(options.opensilex.getServiceContainer());
     }
-};
+} as OpensilexModulePlugin;

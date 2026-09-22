667d094abb45963df300bf5899b32cfc7e3a7046
ROUX YVAN <yvan.roux@inrae.fr>
2026-09-22 15:49:59 +0200

    feat(brapi): remove useless front folder

--- opensilex-brapi/front/src/index.ts
+++
@@ -1,7 +0,0 @@
-import {OpensilexModulePlugin} from "../../../opensilex-front/front/src/models/OpensilexModulePlugin";
-import { ApiServiceBinder } from './lib';
-export default {
-    install(App, options) {
-        ApiServiceBinder.with(options.opensilexInstance.getServiceContainer());
-    }
-} as OpensilexModulePlugin;

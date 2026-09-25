385ee353111aadfb098f7bf32daaaf1321441398
ROUX YVAN <yvan.roux@inrae.fr>
2026-09-25 11:43:10 +0200

    remove __dirname for opensilex-front too

--- opensilex-front/front/vite.config.ts
+++ opensilex-front/front/vite.config.ts
@@ -7,9 +7,9 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 export default defineConfig(({ mode }) => ({
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-      'opensilex-core': resolve(__dirname, '../../opensilex-core/front/src'),
-      'opensilex-security': resolve(__dirname, '../../opensilex-security/front/src')
+      '@': resolve(import.meta.dirname, 'src'),
+      'opensilex-core': resolve(import.meta.dirname, '../../opensilex-core/front/src'),
+      'opensilex-security': resolve(import.meta.dirname, '../../opensilex-security/front/src')
     },
   },
 
